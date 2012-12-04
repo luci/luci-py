b130afe52403299099b0d7586f93271ac1d7d551
csharp@google.com <csharp@google.com@722f4569-f0a1-f026-5e06-c3648ed4afc7>
2012-12-04 20:04:13 +0000

    Add swarm_constants.py as an open sourced file.
    R=mad
    DELTA=1  (1 added, 0 deleted, 0 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5919
    
    
    git-svn-id: https://swarming.googlecode.com/svn/trunk@294 722f4569-f0a1-f026-5e06-c3648ed4afc7

---
+++ src/common/swarm_constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2.7
+#
+# Copyright 2012 Google Inc. All Rights Reserved.
+
+"""Various swarm constants required by the server and the swarm slaves.
+
+This allows the swarm slaves to have this files and the needed variables without
+having to download the whole swarm directory.
+"""
+
+
+
+
+# The exit code to return when the machine should restart.
+RESTART_EXIT_CODE = 99
