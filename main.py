luci/luci-py

commit 096f57ddc2f7a5846a36e102017f857e7b51051f

Convert isolateserver to use ndb.
Add a few performance TODOs along the way.

R=csharp@chromium.org
BUG=

Review URL: https://chromiumcodereview.appspot.com/16358028

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/isolate_server@205527 0039d316-1c4b-4281-b951-d872f2087c98

main.py
@@ -20,7 +20,7 @@ from google.appengine.ext import blobstore
 from google.appengine.api import files
 from google.appengine.api import memcache
 from google.appengine.api import taskqueue
-from google.appengine.ext import db
+from google.appengine.ext import ndb
 from google.appengine.ext.webapp import blobstore_handlers
 # pylint: enable=E0611,F0401
 
@@ -54,7 +54,7 @@ MAX_NAMESPACE_LEN = 29
 #### Models
 
 
-class ContentNamespace(db.Model):
+class ContentNamespace(ndb.Model):
   """Used as an ancestor of ContentEntry to create mutiple content-addressed
   "tables".
 
@@ -74,42 +74,42 @@ class ContentNamespace(db.Model):
   All the tables in the temporary* family must have is_testing==True and the
   others is_testing==False.
   """
-  is_testing = db.BooleanProperty()
-  creation = db.DateTimeProperty(auto_now=True)
+  is_testing = ndb.BooleanProperty()
+  creation = ndb.DateTimeProperty(auto_now=True)
 
 
-class ContentEntry(db.Model):
+class ContentEntry(ndb.Model):
   """Represents the content, keyed by its SHA-1 hash."""
   # The GS filename. blobstore.create_upload_url() doesn't permit specifying a
   # reliable filename, so save the autogenerated filename. Save the file size
   # too
-  filename = db.StringProperty()
+  filename = ndb.StringProperty()
 
   # Cache the file size for statistics purposes.
-  size = db.IntegerProperty()
+  size = ndb.IntegerProperty()
 
   # The content stored inline. This is only valid if the content was smaller
   # than MIN_SIZE_FOR_GS.
-  content = db.ByteStringProperty()
+  content = ndb.BlobProperty()
 
   # The day the content was last accessed. This is used to determine when
   # data is old and should be cleared.
-  last_access = db.DateProperty(auto_now_add=True)
+  last_access = ndb.DateProperty(auto_now_add=True)
 
-  creation = db.DateTimeProperty(auto_now=True)
+  creation = ndb.DateTimeProperty(auto_now=True)
 
   # It is an .isolated file.
-  is_isolated = db.BooleanProperty(default=False)
+  is_isolated = ndb.BooleanProperty(default=False)
 
   # It's content was verified to not be corrupted.
-  is_verified = db.BooleanProperty(default=False)
+  is_verified = ndb.BooleanProperty(default=False)
 
   @property
   def is_compressed(self):
     """Is it the raw data or was it modified in any form, e.g. compressed, so
     that the SHA-1 doesn't match.
     """
-    self.parent_key().name().endswith(('bzip2', 'gzip'))
+    self.key.parent().id().endswith(('bzip2', 'gzip'))
 
   @property
   def gs_filepath(self):
@@ -117,7 +117,7 @@ class ContentEntry(db.Model):
     if self.content:
       return None
     # namespace/hash_key.
-    return u'%s/%s' % (self.parent_key().name(), self.filename)
+    return u'%s/%s' % (self.key.parent().id(), self.filename)
 
 
 def get_content_namespace_key(namespace):
@@ -126,8 +126,9 @@ def get_content_namespace_key(namespace):
   Makes sure the entity exists in the datastore.
   memcache.
   """
+  # TODO(maruel): Use get_or_insert_async().
   return ContentNamespace.get_or_insert(
-      namespace, is_testing=namespace.startswith('temporary')).key()
+      namespace, is_testing=namespace.startswith('temporary')).key
 
 
 def get_content_by_hash(namespace, hash_key):
@@ -142,9 +143,8 @@ def get_content_by_hash(namespace, hash_key):
 
   try:
     namespace_model_key = get_content_namespace_key(namespace)
-    key = db.Key.from_path('ContentEntry', hash_key, parent=namespace_model_key)
-    return ContentEntry.get(key)
-  except (db.BadKeyError, db.KindError):
+    return ContentEntry.get_by_id(hash_key, namespace_model_key)
+  except (ndb.BadKeyError, ndb.KindError):
     pass
 
   return None
@@ -164,10 +164,13 @@ def create_entry(namespace, hash_key):
     logging.error('Given an invalid key, %s', hash_key)
     return None
 
-  namespace_model_key = get_content_namespace_key(namespace)
-  key = db.Key.from_path('ContentEntry', hash_key, parent=namespace_model_key)
+  get_content_namespace_key(namespace)
+  key = ndb.Key(ContentNamespace, namespace, ContentEntry, hash_key)
 
-  if ContentEntry.all(keys_only=True).filter('__key__ =', key).get():
+  # It's already present.
+  # TODO(maruel): Profile to see if it is faster to fetch the whole entity so
+  # the cache is used.
+  if ContentEntry.query(ContentEntry.key == key).get(keys_only=True):
     return None
   return ContentEntry(key=key)
 
@@ -177,7 +180,7 @@ def delete_entry_and_gs_entry(to_delete):
   # Note all the files to delete.
   gs_files_to_delete = filter(None, (i.gs_filepath for i in to_delete))
 
-  entities_future = db.delete_async(to_delete)
+  entities_future = ndb.delete_multi_async(to_delete)
 
   # Do this one last because it is synchronous.
   # TODO(maruel): This could leak a broken BlobInfo object in the blobstore
@@ -298,7 +301,7 @@ class RestrictedCleanupOldEntriesWorkerHandler(webapp2.RequestHandler):
         days=DATASTORE_TIME_TO_LIVE_IN_DAYS)
 
     incremental_delete(
-        ContentEntry.all().filter('last_access <', old_cutoff),
+        ContentEntry.query(ContentEntry.last_access < old_cutoff),
         delete=delete_entry_and_gs_entry)
     logging.info('Done deleting old entries')
 
@@ -316,23 +319,24 @@ class RestrictedCleanupTestingEntriesWorkerHandler(webapp2.RequestHandler):
     logging.info('Deleting testing entries')
     old_cutoff_testing = datetime.datetime.today() - datetime.timedelta(days=1)
     # For each testing namespace.
-    namespace_query = ContentNamespace.all(keys_only=True).filter(
-        'is_testing =', True)
+    namespace_query = ContentNamespace.query(
+        ContentNamespace.is_testing == True)
     orphaned_namespaces = []
-    for namespace in namespace_query:
+    # TODO(maruel): These could be run in parallel with @ndb.synctasklet.
+    for namespace in namespace_query.iter(keys_only=True):
       logging.debug('Namespace %s', namespace.name())
       found = incremental_delete(
-          ContentEntry.all(keys_only=True).ancestor(
-              namespace).filter(
-              'last_access <', old_cutoff_testing),
-          delete=db.delete_async)
+          ContentEntry.query(
+              ContentEntry.last_access < old_cutoff_testing,
+              ancestor=namespace).iter(keys_only=True),
+          delete=ndb.delete_multi_async)
       if not found:
         orphaned_namespaces.append(namespace)
     if orphaned_namespaces:
       # Since delete_async() is used, the stale ContentNamespace will
       # likely stay for another full day, so keep it an extra day.
       logging.info('Deleting %s testing namespaces', len(orphaned_namespaces))
-      db.delete_async(orphaned_namespaces)
+      ndb.delete_multi_async(orphaned_namespaces)
     logging.info('Done deleting testing namespaces')
 
 
@@ -343,13 +347,13 @@ class RestrictedObliterateWorkerHandler(webapp2.RequestHandler):
       self.abort(405, detail='Only internal task queue tasks can do this')
     logging.info('Deleting ContentEntry')
     incremental_delete(
-        ContentEntry.all(keys_only=True).order('creation'),
-        db.delete_async)
+        ContentEntry.query().order('creation').iter(keys_only=True),
+        ndb.delete_multi_async)
 
     logging.info('Deleting Namespaces')
     incremental_delete(
-        ContentNamespace.all(keys_only=True).order('creation'),
-        db.delete_async)
+        ContentNamespace.query().order('creation').iter(keys_only=True),
+        ndb.delete_multi_async)
 
     logging.info('Deleting blobs')
     incremental_delete(
@@ -395,12 +399,12 @@ class RestrictedTagWorkerHandler(webapp2.RequestHandler):
     to_save = []
     for raw_hash_digest in raw_hash_digests:
       hash_digest = binascii.hexlify(raw_hash_digest)
-      key = db.Key.from_path('ContentEntry', hash_digest, parent=parent_key)
-      item = ContentEntry.get(key)
+      # TODO(maruel): Use get_by_id_async()
+      item = ContentEntry.get_by_id(hash_digest, parent=parent_key)
       if item and item.last_access != today:
         item.last_access = today
         to_save.append(item)
-    db.put(to_save)
+    ndb.put_multi(to_save)
     logging.info('Done timestamping %d entries', len(to_save))
 
 
@@ -476,8 +480,8 @@ class ContainsHashHandler(acl.ACLRequestHandler):
 
     # Convert to entity keys.
     keys = (
-        db.Key.from_path(
-            'ContentEntry',
+        ndb.Key(
+            ContentEntry,
             binascii.hexlify(raw_hash_digest),
             parent=namespace_model_key)
         for raw_hash_digest in raw_hash_digests
@@ -485,27 +489,18 @@ class ContainsHashHandler(acl.ACLRequestHandler):
 
     # Start the queries in parallel. It must be a list so the calls are executed
     # right away.
+    # TODO(maruel): Queries are not cached so in practice it could be faster to
+    # use get_by_id_async() or count_async instead even if this means pulling
+    # all the data in. This needs to be profiled. Another option is to do the
+    # caching ourself (?).
     queries = [
-        ContentEntry.all().filter('__key__ =', key).run(
-            read_policy=db.EVENTUAL_CONSISTENCY,
-            limit=1,
-            batch_size=1,
-            keys_only=True)
+        ContentEntry.query(ContentEntry.key == key).get_async(keys_only=True)
         for key in keys
     ]
 
-    # Convert to True/False. It's a bit annoying because run() returns a
-    # ResultsIterator.
-    def IteratorToBool(itr):
-      # TODO(maruel): Return if the entity is verified or not. Or add a filter
-      # to the query above?
-      for _ in itr:
-        return True
-      return False
-
-    # Convert to byte, chr(0) if not present, chr(1) if it is.
-
-    contains = [IteratorToBool(q) for q in queries]
+    # Convert the Future to True/False, then to byte, chr(0) if not present,
+    # chr(1) if it is.
+    contains = [bool(q.get_result()) for q in queries]
     self.response.out.write(bytearray(contains))
     self.response.headers['Content-Type'] = 'application/octet-stream'
     found = sum(contains, 0)

commit 7a757a8887616af79c00a5be7cc807278b1e23af

Reduce the number of native indexes.
This will help with performance by reducing the number of indexes that need to
be updated on each new entry.

Fixes a regression introduced in r205527 since order() takes a Property instance
and not a string.

R=csharp@chromium.org
BUG=

Review URL: https://chromiumcodereview.appspot.com/15851012

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/isolate_server@205538 0039d316-1c4b-4281-b951-d872f2087c98

main.py
@@ -75,7 +75,7 @@ class ContentNamespace(ndb.Model):
   others is_testing==False.
   """
   is_testing = ndb.BooleanProperty()
-  creation = ndb.DateTimeProperty(auto_now=True)
+  creation = ndb.DateTimeProperty(indexed=False, auto_now=True)
 
 
 class ContentEntry(ndb.Model):
@@ -83,7 +83,7 @@ class ContentEntry(ndb.Model):
   # The GS filename. blobstore.create_upload_url() doesn't permit specifying a
   # reliable filename, so save the autogenerated filename. Save the file size
   # too
-  filename = ndb.StringProperty()
+  filename = ndb.StringProperty(indexed=False)
 
   # Cache the file size for statistics purposes.
   size = ndb.IntegerProperty()
@@ -96,13 +96,13 @@ class ContentEntry(ndb.Model):
   # data is old and should be cleared.
   last_access = ndb.DateProperty(auto_now_add=True)
 
-  creation = ndb.DateTimeProperty(auto_now=True)
+  creation = ndb.DateTimeProperty(indexed=False, auto_now=True)
 
   # It is an .isolated file.
   is_isolated = ndb.BooleanProperty(default=False)
 
   # It's content was verified to not be corrupted.
-  is_verified = ndb.BooleanProperty(default=False)
+  is_verified = ndb.BooleanProperty(indexed=False, default=False)
 
   @property
   def is_compressed(self):
@@ -347,18 +347,14 @@ class RestrictedObliterateWorkerHandler(webapp2.RequestHandler):
       self.abort(405, detail='Only internal task queue tasks can do this')
     logging.info('Deleting ContentEntry')
     incremental_delete(
-        ContentEntry.query().order('creation').iter(keys_only=True),
-        ndb.delete_multi_async)
+        ContentEntry.query().iter(keys_only=True), ndb.delete_multi_async)
 
     logging.info('Deleting Namespaces')
     incremental_delete(
-        ContentNamespace.query().order('creation').iter(keys_only=True),
-        ndb.delete_multi_async)
+        ContentNamespace.query().iter(keys_only=True), ndb.delete_multi_async)
 
     logging.info('Deleting blobs')
-    incremental_delete(
-        blobstore.BlobInfo.all().order('creation'),
-        delete_blobinfo_async)
+    incremental_delete(blobstore.BlobInfo.all(), delete_blobinfo_async)
 
     logging.info('Deleting GS bucket %s', GS_BUCKET)
     incremental_delete(