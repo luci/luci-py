d36eb8bd60c6215e417a615c244d9772135b411c
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 15:47:51 +0000

    Make appstats disabled by default and possible to enable with a config setting.
    Enabling it is not free cost-wise so make it possible to enable it from a
    setting and defaults it to False. The main drawback is that the instances have
    to be restarted for it to take effect.
    
    R=csharp@chromium.org
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/16747008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/isolate_server@205798 0039d316-1c4b-4281-b951-d872f2087c98

--- appengine_config.py
+++ appengine_config.py
@@ -2,12 +2,28 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# The app engine headers are located locally, so don't worry about not finding
-# them.
-# pylint: disable=E0611,F0401
-from google.appengine.ext.appstats import recording
-# pylint: enable=E0611,F0401
+"""Configures appstats if enabled in the config.
+
+See config.py for more information.
+
+https://developers.google.com/appengine/docs/python/tools/appengineconfig
+"""
+
+_SETTINGS = __import__('config').settings()
+
+
+appstats_CALC_RPC_COSTS = (
+    _SETTINGS.enable_appstats and _SETTINGS.enable_appstats_cost)
 
 
 def webapp_add_wsgi_middleware(app):
-  return recording.appstats_wsgi_middleware(app)
+  """Overrides the wsgi application with appstats if enabled.
+
+  https://developers.google.com/appengine/docs/python/tools/appstats
+  """
+  if _SETTINGS.enable_appstats:
+    # pylint: disable=E0611,F0401
+    from google.appengine.ext.appstats import recording
+    return recording.appstats_wsgi_middleware(app)
+
+  return app
