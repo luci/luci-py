d6cc1582fd2893f6a20ca44e60d45b502661fc73
Marc-Antoine Ruel <maruel@chromium.org>
2013-08-13 17:02:39 -0400

    Change update.py to use the pseudo revision of mergebase, not HEAD.
    Otherwise, the intermediary commits increase the pseudo-revision, which doesn't
    make sense.
    
    Refactor the code to be a bit more readable.
    
    R=vadimsh@chromium.org
    BUG=
    
    Review URL: https://codereview.appspot.com/12875045

--- tools/update.py
+++ tools/update.py
@@ -3,7 +3,9 @@
 # Use of this source code is governed by the Apache v2.0 license that can be
 # found in the LICENSE file.
 
-"""Updates an isolateserver instance."""
+"""Updates an AppEngine instance with the version derived from the current git
+checkout state.
+"""
 
 import logging
 import optparse
@@ -20,30 +22,72 @@ def git(cmd):
   return subprocess.check_output(['git'] + cmd, cwd=APP_DIR)
 
 
-def calculate_version(tag):
-  """Returns a tag for a git checkout."""
-  describe = git(['describe', 'HEAD']).rstrip()
-  if not describe.startswith('baserev-'):
+def get_pseudo_revision(remote, baserev_tag_name):
+  """Returns the pseudo revision number and commit hash describing
+  the base upstream commit this branch is based on.
+
+  The base upstream commit hash is determined by 'git merge-base'. See the man
+  page for more information.
+
+  The pseudo revision is calculated by the number of commits separating the base
+  upstream commit from a predetermined tag created with 'git tag'. The proper
+  setup is to create a git tag of the earliest commit (the root commit) with:
+
+    git tag -a baserev <earliest commit>
+
+  where 'baserev' matches |baserev_tag_name|. Do not forget to push the tag
+  upstream! The earliest commit should be a root commit, e.g. a commit with no
+  parent. A git tree can have multiple root commits when git repositories are
+  merged together so the developer should select the one that makes the most
+  sense. The list of all root commits can be retrieved with:
+
+    git rev-list --parents HEAD | egrep "^[a-f0-9]{40}$"
+
+  Then this function uses 'git describe' which will implicitly use the tag
+  created above to count the number of commits separating the tag from the merge
+  base commit. This is the pseudo revision number.
+
+  Returns:
+    tuple of:
+    - pseudo revision number as a int
+    - upstream commit hash this branch is based of.
+  """
+  mergebase = git(['merge-base', 'HEAD', remote]).rstrip()
+  describe = git(['describe', mergebase]).rstrip()
+  if not describe.startswith(baserev_tag_name + '-'):
     print >> sys.stderr, 'Make sure to run git fetch --tags'
     sys.exit(1)
 
   # describe has a format similar to 'baserev-124-g33c3dca'.
   _, pseudo_revision, commit = describe.split('-', 2)
-  commit = commit[1:]
+  assert commit.startswith('g'), commit
+  pseudo_revision = int(pseudo_revision)
+  assert mergebase.startswith(commit[1:]), (mergebase, commit)
+  return pseudo_revision, mergebase
 
-  remote = 'origin/master'
-  mergebase = git(['merge-base', 'HEAD', remote]).rstrip()
-  logging.info('commit: %s, mergebase: %s', commit, mergebase)
-  if not mergebase.startswith(commit):
+
+def calculate_version(tag):
+  """Returns a tag for a git checkout.
+
+  Uses the pseudo revision number from the upstream commit this branch is based
+  on, the abbreviated commit hash. Adds -tainted if the code is not
+  pristine and optionally adds a tag to further describe it.
+  """
+  pseudo_revision, mergebase = get_pseudo_revision('origin/master', 'baserev')
+  head = git(['rev-parse', 'HEAD']).rstrip()
+
+  logging.info('head: %s, mergebase: %s', head, mergebase)
+  if head != mergebase:
     pristine = False
-    # Using a local commit hash is not useful, use the real base commit instead.
-    # Trim it to 7 characters like 'git describe' does.
-    commit = mergebase[:7]
   else:
-    pristine = not git(['diff', mergebase])
+    # Look for local uncommitted diff.
+    pristine = not (
+        git(['diff', mergebase]) or git(['diff', '--cached', mergebase]))
     logging.info('was diff clear? %s', pristine)
 
-  version = '%s-%s' % (pseudo_revision, commit)
+  # Trim it to 7 characters like 'git describe' does. 40 characters is
+  # overwhelming!
+  version = '%s-%s' % (pseudo_revision, mergebase[:7])
   if not pristine:
     version += '-tainted'
   if tag:
