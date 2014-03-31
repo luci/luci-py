luci/luci-py

commit c5f797b8a2f8b9c2e20d4a659ea2afca100d9a1e

Fix stats tendency to over count requests. Enforce strictness. Fix aliasing.
- Stop using default value in ndb.LocalStructuredProperty. IT'S A TRAP. It
  causes hard-to-find aliasing problems. While it's possible to get it right,
  it's just too easy to get it wrong afterward when updating the code. So better
  be safe than sorry and stop using it completely. Enforces this restriction in
  accumulate().
- Fix bug where a StatsDay could be created with an invalid key, the parent
  wouldn't be set to self.root_key when there's one unsealed StatsDay but
  StatsRoot.timestamp is not set.
- Fix generate_stats_data() which would return more data than requested.
- Add functions to abstract logservice usage in stats_framework.py. logservice
  is not easily usable in tests.
- Isolate server: remove filtering on frontend modules for statistics gathering.
  This will result in inconsistent stats for a while.
- Add stats_test.py which ensures correct isolate server statistics gathering.

R=vadimsh@chromium.org
BUG=

Review URL: https://codereview.appspot.com/77890044

services/components/stats_framework.py
@@ -12,9 +12,11 @@ measurements saved and presentations must be supplied by the user.
 """
 
 import calendar
+import collections
 import datetime
 import logging
 
+from google.appengine.api import logservice
 from google.appengine.ext import ndb
 from google.appengine.runtime import DeadlineExceededError
 
@@ -34,6 +36,54 @@ TOO_RECENT = 5 if not utils.is_local_dev_server() else 1
 def utcnow():
   """To be mocked in tests."""
   return datetime.datetime.utcnow()
+
+
+def add_entry(message):
+  """Adds an entry for the current request.
+
+  Meant to be mocked in tests.
+  """
+  logging.debug(PREFIX + message)
+
+
+# One handled HTTP request and the associated statistics if any.
+StatsEntry = collections.namedtuple('StatsEntry', ('request', 'entries'))
+
+
+def _yield_logs(start_time, end_time):
+  """Yields logservice.RequestLogs for the requested time interval.
+
+  Meant to be mocked in tests.
+  """
+  for request in logservice.fetch(
+      start_time=start_time - 1 if start_time else start_time,
+      end_time=end_time + 1 if end_time else end_time,
+      include_app_logs=True):
+    yield request
+
+
+def yield_entries(start_time, end_time):
+  """Yields StatsEntry in this time interval.
+
+  Look at requests that *ended* between [start_time, end_time[. Ignore the start
+  time of the request. This is because the parameters start_time and end_time of
+  logserver.fetch() filters on the completion time of the request.
+  """
+  offset = len(PREFIX)
+  for request in _yield_logs(start_time, end_time):
+    if not request.finished or not request.end_time:
+      continue
+    if start_time and request.end_time < start_time:
+      continue
+    if end_time and request.end_time >= end_time:
+      continue
+
+    # Gathers all the entries added via add_entry().
+    entries = [
+      l.message[offset:] for l in request.app_logs
+      if l.level <= logservice.LOG_LEVEL_INFO and l.message.startswith(PREFIX)
+    ]
+    yield StatsEntry(request, entries)
 
 
 class StatisticsFramework(object):
@@ -182,7 +232,7 @@ class StatisticsFramework(object):
       modified = ndb.DateTimeProperty(indexed=False, auto_now_add=True)
 
       # Statistics for the day.
-      values = ndb.LocalStructuredProperty(snapshot_cls, default=snapshot_cls())
+      values = ndb.LocalStructuredProperty(snapshot_cls)
 
       # Hours that have been summed. A complete day will be set to (1<<24)-1,
       # e.g.  0xFFFFFF, e.g. 24 bits or 6x4 bits.
@@ -219,7 +269,8 @@ class StatisticsFramework(object):
       generated under a transaction, so ~1 transaction per minute.
       """
       created = ndb.DateTimeProperty(indexed=False, auto_now=True)
-      values = ndb.LocalStructuredProperty(snapshot_cls, default=snapshot_cls())
+      # Statistics for the hour.
+      values = ndb.LocalStructuredProperty(snapshot_cls)
 
       # Minutes that have been summed. A complete hour will be set to (1<<60)-1,
       # e.g. 0xFFFFFFFFFFFFFFF, e.g. 60 bits or 15x4 bits.
@@ -252,7 +303,8 @@ class StatisticsFramework(object):
       definition.
       """
       created = ndb.DateTimeProperty(indexed=False, auto_now=True)
-      values = ndb.LocalStructuredProperty(snapshot_cls, default=snapshot_cls())
+      # Statistics for one minute.
+      values = ndb.LocalStructuredProperty(snapshot_cls)
 
       def to_dict(self):
         """Adds the key and remove cruft not necessary for the user."""
@@ -266,7 +318,10 @@ class StatisticsFramework(object):
     return StatsMinute
 
   def _set_last_processed_time(self, moment):
-    """Saves the last minute processed."""
+    """Saves the last minute processed.
+
+    Occasionally used in tests to bound the search for statistics.
+    """
     assert moment.second == 0, moment
     assert moment.microsecond == 0, moment
     root = self.root_key.get()
@@ -296,13 +351,15 @@ class StatisticsFramework(object):
       if minute_after.date() != timestamp.date():
         # That was 23:59. Make sure day for 00:00 exists.
         self.stats_day_cls.get_or_insert(
-            str(minute_after.date()), parent=self.root_key)
+            str(minute_after.date()), parent=self.root_key,
+            values=self.snapshot_cls())
 
       if minute_after.hour != timestamp.hour:
         # That was NN:59.
         self.stats_hour_cls.get_or_insert(
             '%02d' % minute_after.hour,
-            parent=self.day_key(minute_after.date()))
+            parent=self.day_key(minute_after.date()),
+            values=self.snapshot_cls())
       return minute_after
     return self._guess_earlier_minute_to_process(now)
 
@@ -327,21 +384,23 @@ class StatisticsFramework(object):
         # midnight to regenerate stats. It will be resource intensive.
         today = now.date()
         key_id = str(today - datetime.timedelta(days=self.MAX_BACKTRACK))
-        day = self.stats_day_cls.get_or_insert(key_id, parent=self.root_key)
+        day = self.stats_day_cls.get_or_insert(
+            key_id, parent=self.root_key, values=self.snapshot_cls())
     else:
       # Take the next unsealed day. Note that if there's a non-sealed, sealed,
       # sealed sequence of self.stats_day_cls, the non-sealed entity will be
       # skipped.
       # TODO(maruel): Fix this.
       key_id = str(last_sealed_day.to_date() + datetime.timedelta(days=1))
-      day = self.stats_day_cls.get_or_insert(key_id)
+      day = self.stats_day_cls.get_or_insert(
+          key_id, parent=self.root_key, values=self.snapshot_cls())
 
     # TODO(maruel): Should we trust it all the time or do an explicit query? For
     # now, trust the bitmap.
     hour_bit = _lowest_missing_bit(day.hours_bitmap)
     assert hour_bit < 24, hour_bit
     hour = self.stats_hour_cls.get_or_insert(
-        '%02d' % hour_bit, parent=day.key)
+        '%02d' % hour_bit, parent=day.key, values=self.snapshot_cls())
     minute_bit = _lowest_missing_bit(hour.minutes_bitmap)
     assert minute_bit < 60, minute_bit
     date = day.to_date()
@@ -369,9 +428,12 @@ class StatisticsFramework(object):
     # cache.
     opts = ndb.ContextOptions(use_memcache=False)
     future_day = self.stats_day_cls.get_or_insert_async(
-        str(moment.date()), parent=self.root_key, context_options=opts)
+        str(moment.date()), parent=self.root_key,
+        values=self.snapshot_cls(),
+        context_options=opts)
     future_hour = self.stats_hour_cls.get_or_insert_async(
         '%02d' % moment.hour, parent=self.day_key(moment.date()),
+        values=self.snapshot_cls(),
         context_options=opts)
     future_minute = self.stats_minute_cls.get_by_id_async(
         minute_key_id, parent=self.hour_key(moment), use_memcache=False)
@@ -424,10 +486,14 @@ class StatisticsFramework(object):
 def accumulate(lhs, rhs):
   """Adds the values from rhs into lhs.
 
-  Both must be an ndb.Model.
+  Both must be an ndb.Model. lhs is modified. rhs is not.
 
   rhs._properties not in lhs._properties are lost.
   lhs._properties not in rhs._properties are untouched.
+
+  This function has specific handling for ndb.LocalStructuredProperty, it
+  refuses any instance with a default value. THIS IS A TRAP. The default object
+  instance will be aliased one way or another. It's just not worth the risk.
   """
   assert isinstance(lhs, ndb.Model), lhs
   assert isinstance(rhs, ndb.Model), rhs
@@ -440,14 +506,16 @@ def accumulate(lhs, rhs):
       lhs_value = getattr(lhs, key, default)
       rhs_value = getattr(rhs, key, default)
       if hasattr(lhs_value, 'accumulate'):
-        assert callable(lhs_value.accumulate), lhs_value
+        # Do not use ndb.LocalStructuredProperty(MyClass, default=MyClass())
+        # since any object created without specifying a object for this property
+        # will get the exact instance provided as the default argument, it's
+        # dangerous aliasing. See the unit test for a way to set a default value
+        # that is safe.
+        assert default is None, key
+        assert callable(lhs_value.accumulate), key
         lhs_value.accumulate(rhs_value)
-        # Enforce setting back the property, so it works even if lhs_value ==
-        # default.
-        total = lhs_value
       else:
-        total = lhs_value + rhs_value
-      setattr(lhs, key, total)
+        setattr(lhs, key, lhs_value + rhs_value)
 
 
 def generate_stats_data(days, hours, minutes, now, stats_handler):
@@ -466,21 +534,21 @@ def generate_stats_data(days, hours, minutes, now, stats_handler):
   if days:
     gen = (
         stats_handler.day_key(today - datetime.timedelta(days=i))
-        for i in range(days + 1))
+        for i in xrange(days))
     future_days = ndb.get_multi_async(gen, use_cache=False, use_memcache=False)
 
   future_hours = []
   if hours:
     gen = (
         stats_handler.hour_key(now - datetime.timedelta(hours=i))
-        for i in range(hours + 1))
+        for i in xrange(hours))
     future_hours = ndb.get_multi_async(gen, use_cache=False, use_memcache=False)
 
   future_minutes = []
   if minutes:
     gen = (
         stats_handler.minute_key(now - datetime.timedelta(minutes=i))
-        for i in range(minutes + 1))
+        for i in xrange(minutes))
     future_minutes = ndb.get_multi_async(
         gen, use_cache=False, use_memcache=False)
 

services/components/tests/stats_framework_test.py
@@ -6,16 +6,24 @@
 import calendar
 import datetime
 import sys
+import time
 import unittest
 
 import test_env
 test_env.setup_test_env()
 
+import webapp2
 from google.appengine.ext import ndb
-from google.appengine.ext import testbed
 
 from components import stats_framework
-from depot_tools import auto_stub
+from components import stats_framework_mock
+
+# From tools/third_party/
+import webtest
+
+# For TestCase.
+import test_case
+
 
 # pylint: disable=W0212
 
@@ -29,11 +37,21 @@ class InnerSnapshot(ndb.Model):
 
 class Snapshot(ndb.Model):
   """Fake statistics."""
-  a = ndb.IntegerProperty(default=0, indexed=False)
+  requests = ndb.IntegerProperty(default=0, indexed=False)
   b = ndb.FloatProperty(default=0, indexed=False)
-  inner = ndb.LocalStructuredProperty(InnerSnapshot, default=InnerSnapshot())
+  inner = ndb.LocalStructuredProperty(InnerSnapshot)
+
+  def __init__(self, **kwargs):
+    # This is the recommended way to use ndb.LocalStructuredProperty inside a
+    # snapshot.
+    #
+    # Warning: The only reason it works is because Snapshot is itself inside a
+    # ndb.LocalStructuredProperty.
+    kwargs.setdefault('inner', InnerSnapshot())
+    super(Snapshot, self).__init__(**kwargs)
 
   def accumulate(self, rhs):
+    # accumulate() specifically handles where default value is a class instance.
     return stats_framework.accumulate(self, rhs)
 
 
@@ -51,27 +69,10 @@ def strip_seconds(timestamp):
   return datetime.datetime(*timestamp.timetuple()[:5], second=0)
 
 
-class StatsFrameworkTest(auto_stub.TestCase):
-  def setUp(self):
-    super(StatsFrameworkTest, self).setUp()
-    self.testbed = testbed.Testbed()
-    self.testbed.activate()
-    self.testbed.init_datastore_v3_stub()
-    self.testbed.init_memcache_stub()
-
-  def tearDown(self):
-    self.testbed.deactivate()
-    super(StatsFrameworkTest, self).tearDown()
-
-  def mock_now(self, now, seconds):
-    now = now + datetime.timedelta(seconds=seconds)
-    self.mock(stats_framework, 'utcnow', lambda: now)
-    self.mock(ndb.DateTimeProperty, '_now', lambda _: now)
-    self.mock(ndb.DateProperty, '_now', lambda _: now.date())
-
+class StatsFrameworkTest(test_case.TestCase, stats_framework_mock.MockMixIn):
   def test_empty(self):
     handler = stats_framework.StatisticsFramework(
-        'global_stats', Snapshot, lambda *_: self.fail())
+        'test_framework', Snapshot, self.fail)
 
     self.assertEqual(0, handler.stats_root_cls.query().count())
     self.assertEqual(0, handler.stats_day_cls.query().count())
@@ -106,7 +107,8 @@ class StatsFrameworkTest(auto_stub.TestCase):
       """Returns fake statistics."""
       self.assertEqual(start + 60, end)
       called.append(start)
-      return Snapshot(a=1, b=1, inner=InnerSnapshot(c='%d,' % len(called)))
+      return Snapshot(
+          requests=1, b=1, inner=InnerSnapshot(c='%d,' % len(called)))
 
     handler = stats_framework.StatisticsFramework(
         'test_framework', Snapshot, gen_data)
@@ -145,7 +147,7 @@ class StatsFrameworkTest(auto_stub.TestCase):
       {
         'key': midnight.strftime('%Y-%m-%d'),
         'values': {
-          'a': limit,
+          'requests': limit,
           'b': float(limit),
           'inner': {
             'c': u''.join('%d,' % i for i in xrange(1, limit + 1)),
@@ -166,7 +168,7 @@ class StatsFrameworkTest(auto_stub.TestCase):
         'key': (midnight + datetime.timedelta(seconds=i*60*60)).strftime(
             '%Y-%m-%-d %H:%M'),
         'values': {
-          'a': 60,
+          'requests': 60,
           'b': 60.,
           'inner': {
             'c': u''.join(
@@ -189,7 +191,7 @@ class StatsFrameworkTest(auto_stub.TestCase):
         'key': (midnight + datetime.timedelta(seconds=i*60)).strftime(
             '%Y-%m-%-d %H:%M'),
         'values': {
-          'a': 1,
+          'requests': 1,
           'b': 1.,
           'inner': {
             'c': u'%d,' % (i + 1),
@@ -210,7 +212,8 @@ class StatsFrameworkTest(auto_stub.TestCase):
       """Returns fake statistics."""
       self.assertEqual(start + 60, end)
       called.append(start)
-      return Snapshot(a=1, b=1, inner=InnerSnapshot(c='%d,' % len(called)))
+      return Snapshot(
+          requests=1, b=1, inner=InnerSnapshot(c='%d,' % len(called)))
 
     handler = stats_framework.StatisticsFramework(
         'test_framework', Snapshot, gen_data)
@@ -243,25 +246,25 @@ class StatsFrameworkTest(auto_stub.TestCase):
       'days': [
         {
           'key': now.strftime('%Y-%m-%d'),
-          'values': {'a': 0, 'b': 0.0, 'inner': {'c': u''}},
+          'values': {'requests': 0, 'b': 0.0, 'inner': {'c': u''}},
         },
       ],
       'hours': [
         {
           'key': now.strftime('%Y-%m-%d %H:00'),
-          'values': {'a': 2, 'b': 2.0, 'inner': {'c': u'1,2,'}},
+          'values': {'requests': 2, 'b': 2.0, 'inner': {'c': u'1,2,'}},
         },
       ],
       'minutes': [
         {
           'key': (now - datetime.timedelta(seconds=60)).strftime(
               '%Y-%m-%d %H:%M'),
-          'values': {'a': 1, 'b': 1.0, 'inner': {'c': u'2,'}},
+          'values': {'requests': 1, 'b': 1.0, 'inner': {'c': u'2,'}},
         },
         {
           'key': (now - datetime.timedelta(seconds=120)).strftime(
               '%Y-%m-%d %H:%M'),
-          'values': {'a': 1, 'b': 1.0, 'inner': {'c': u'1,'}},
+          'values': {'requests': 1, 'b': 1.0, 'inner': {'c': u'1,'}},
         },
       ],
       'now': '2010-01-02 03:04:05',
@@ -269,7 +272,8 @@ class StatsFrameworkTest(auto_stub.TestCase):
     self.assertEqual(expected, out)
 
     # Limit the number of items returned.
-    out = stats_framework.generate_stats_data(0, 0, 1, now, handler)
+    out = stats_framework.generate_stats_data(
+        0, 0, 1, now - datetime.timedelta(seconds=60), handler)
     for key in ('days', 'hours', 'minutes'):
       out[key] = [i.to_dict() for i in out[key]]
     expected = {
@@ -279,16 +283,16 @@ class StatsFrameworkTest(auto_stub.TestCase):
         {
           'key': (now - datetime.timedelta(seconds=60)).strftime(
               '%Y-%m-%d %H:%M'),
-          'values': {'a': 1, 'b': 1.0, 'inner': {'c': u'2,'}},
+          'values': {'requests': 1, 'b': 1.0, 'inner': {'c': u'2,'}},
         },
       ],
-      'now': '2010-01-02 03:04:05',
+      'now': '2010-01-02 03:03:05',
     }
     self.assertEqual(expected, out)
 
   def test_keys(self):
     handler = stats_framework.StatisticsFramework(
-        'test_framework', Snapshot, None)
+        'test_framework', Snapshot, self.fail)
     date = datetime.datetime(2010, 1, 2)
     self.assertEqual(
         ndb.Key('StatsRoot', 'test_framework', 'StatsDay', '2010-01-02'),
@@ -307,6 +311,205 @@ class StatsFrameworkTest(auto_stub.TestCase):
           'StatsHour', '00',
           'StatsMinute', '00'),
         handler.minute_key(date))
+
+  def test_yield_empty(self):
+    self.assertEqual(
+        0, len(list(stats_framework.yield_entries(None, None))))
+
+
+def generate_snapshot(start_time, end_time):
+  values = Snapshot()
+  for entry in stats_framework.yield_entries(start_time, end_time):
+    values.requests += 1
+    for l in entry.entries:
+      values.inner.c += l
+  return values
+
+
+class StatsFrameworkLogTest(test_case.TestCase, stats_framework_mock.MockMixIn):
+  def setUp(self):
+    super(StatsFrameworkLogTest, self).setUp()
+    stats_framework_mock.configure(self)
+    self.h = stats_framework.StatisticsFramework(
+        'test_framework', Snapshot, generate_snapshot)
+
+    # pylint: disable=E0213
+    class GenerateHandler(webapp2.RequestHandler):
+      def get(self2):
+        stats_framework.add_entry('Hello')
+        self2.response.write('Yay')
+
+    class JsonHandler(webapp2.RequestHandler):
+      def get(self2):
+        self2.response.headers['Content-Type'] = (
+            'application/json; charset=utf-8')
+        data = stats_framework.generate_stats_data_from_request(
+            self2.request, self.h)
+        self2.response.write(stats_framework.utils.encode_to_json(data))
+
+    routes = [
+        ('/generate', GenerateHandler),
+        ('/json', JsonHandler),
+    ]
+    real_app = webapp2.WSGIApplication(routes, debug=True)
+    self.app = webtest.TestApp(
+        real_app, extra_environ={'REMOTE_ADDR': 'fake-ip'})
+    self.now = datetime.datetime(2010, 1, 2, 3, 4, 5, 6)
+    self.mock_now(self.now, 0)
+
+  def test_yield_entries(self):
+    stats_framework_mock.reset_timestamp(self.h, self.now)
+
+    self.assertEqual(
+        0, len(list(stats_framework.yield_entries(None, None))))
+    self.assertEqual(
+        0, len(list(stats_framework.yield_entries(1, time.time()))))
+
+    self.assertEqual('Yay', self.app.get('/generate').body)
+
+    self.assertEqual(
+        1, len(list(stats_framework.yield_entries(None, None))))
+    self.assertEqual(
+        1, len(list(stats_framework.yield_entries(1, time.time()))))
+    self.assertEqual(
+        0, len(list(stats_framework.yield_entries(
+          None, stats_framework_mock.now_epoch()))))
+
+  def test_json_empty(self):
+    stats_framework_mock.reset_timestamp(self.h, self.now)
+    expected = {
+      u'days': [],
+      u'hours': [],
+      u'minutes': [],
+      u'now': u'2010-01-02 03:04:05',
+    }
+    self.assertEqual(expected, self.app.get('/json').json)
+
+  def test_json_empty_processed(self):
+    stats_framework_mock.reset_timestamp(self.h, self.now)
+    self.h.process_next_chunk(0)
+    expected = {
+      u'days': [
+        {
+          u'key': u'2010-01-02',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+      ],
+      u'hours': [
+        {
+          u'key': u'2010-01-02 03:00',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+        {
+          u'key': u'2010-01-02 02:00',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+      ],
+      u'minutes': [
+        {
+          u'key': u'2010-01-02 03:04',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+        {
+          u'key': u'2010-01-02 03:03',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+        {
+          u'key': u'2010-01-02 03:02',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+        {
+          u'key': u'2010-01-02 03:01',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+        {
+          u'key': u'2010-01-02 03:00',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+        {
+          u'key': u'2010-01-02 02:59',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+        {
+          u'key': u'2010-01-02 02:58',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+        {
+          u'key': u'2010-01-02 02:57',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+        {
+          u'key': u'2010-01-02 02:56',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+        {
+          u'key': u'2010-01-02 02:55',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+      ],
+      u'now': u'2010-01-02 03:04:05',
+    }
+    self.assertEqual(expected, self.app.get('/json').json)
+
+  def test_json_empty_processed_limit(self):
+    stats_framework_mock.reset_timestamp(self.h, self.now)
+    self.h.process_next_chunk(0)
+    expected = {
+      u'days': [
+      ],
+      u'hours': [
+        {
+          u'key': u'2010-01-02 03:00',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+      ],
+      u'minutes': [
+        {
+          u'key': u'2010-01-02 03:04',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+      ],
+      u'now': u'2010-01-02 03:04:05',
+    }
+    self.assertEqual(
+        expected, self.app.get('/json?days=0&hours=1&minutes=1').json)
+
+  def test_json_two(self):
+    stats_framework_mock.reset_timestamp(self.h, self.now)
+    self.assertEqual('Yay', self.app.get('/generate').body)
+    self.assertEqual('Yay', self.app.get('/generate').body)
+    self.h.process_next_chunk(0)
+    expected = {
+      u'days': [
+        {
+          u'key': u'2010-01-02',
+          u'values': {u'b': 0.0, u'inner': {u'c': u''}, u'requests': 0},
+        },
+      ],
+      u'hours': [
+        {
+          u'key': u'2010-01-02 03:00',
+          u'values': {
+            u'b': 0.0,
+            u'inner': {u'c': u'HelloHello'},
+            u'requests': 2,
+          },
+        },
+      ],
+      u'minutes': [
+        {
+          u'key': u'2010-01-02 03:04',
+          u'values': {
+            u'b': 0.0,
+            u'inner': {u'c': u'HelloHello'},
+            u'requests': 2,
+          },
+        },
+      ],
+      u'now': u'2010-01-02 03:04:05',
+    }
+    self.assertEqual(
+        expected, self.app.get('/json?days=1&hours=1&minutes=1').json)
 
 
 if __name__ == '__main__':

commit 1393db0796c893d49fa5514326b5e0fb85f9b847

Fix regression introduced in c5f797b8a2f8b9c2e20d4a659ea2afca100d9a1e.
This caused under counting by only counting stats in the current version in the
current module.

Fix StatisticsFramework._guess_earlier_minute_to_process() since it was
completely broken in presence of sealed days.

R=vadimsh@chromium.org
BUG=

Review URL: https://codereview.appspot.com/81500045

services/components/stats_framework.py
@@ -55,10 +55,14 @@ def _yield_logs(start_time, end_time):
 
   Meant to be mocked in tests.
   """
+  # If module_versions is not specified, it will default to the current version
+  # on current module, which is not what we want.
+  module_versions = utils.get_module_version_list(None, True)
   for request in logservice.fetch(
       start_time=start_time - 1 if start_time else start_time,
       end_time=end_time + 1 if end_time else end_time,
-      include_app_logs=True):
+      include_app_logs=True,
+      module_versions=module_versions):
     yield request
 
 
@@ -376,28 +380,43 @@ class StatisticsFramework(object):
 
   def _guess_earlier_minute_to_process(self, now):
     """Searches backward in time for the last sealed day."""
-    logging.info('Guessing for earliest day')
+    logging.info(
+        '%s.timestamp is missing or invalid; searching for last sealed day',
+        self.root_key)
+    # Use a loop to find it. In practice it would require an index but it's
+    # better to not have the index and make this code slower. We do it by
+    # counting all the entities and fetching the last one.
     q = self.stats_day_cls.query(ancestor=self.root_key)
     # TODO(maruel): It should search for recent unsealed day, that is still
     # reachable from the logs, e.g. the logs for that day hasn't been expurged
     # yet.
     q.filter(
         self.stats_day_cls.hours_bitmap == self.stats_day_cls.SEALED_BITMAP)
-    q.order(-self.stats_day_cls.key)
-    last_sealed_day = q.get()
-    if not last_sealed_day:
+    # Can't use .order(-self.stats_day_cls.key) without an index. So use a cheap
+    # tricky with offset=count-1.
+    # TODO(maruel): that this won't work on very large history, likely after 3
+    # years worth of history. Fix this accordingly by 2016.
+    count = q.count()
+    if not count:
+      logging.info('Failed to find sealed day')
       # Maybe there's an non-sealed day. Get the latest one.
       q = self.stats_day_cls.query(ancestor=self.root_key)
       q.order(-self.stats_day_cls.key)
       day = q.get()
       if not day:
+        logging.info('Failed to find a day at all')
         # We are bootstrapping as there is no entity at all. Use ~5 days ago at
         # midnight to regenerate stats. It will be resource intensive.
         today = now.date()
         key_id = str(today - datetime.timedelta(days=self.MAX_BACKTRACK))
         day = self.stats_day_cls.get_or_insert(
             key_id, parent=self.root_key, values=self.snapshot_cls())
+        logging.info('Selected/created: %s', key_id)
+      else:
+        logging.info('Selected: %s', day.key)
     else:
+      last_sealed_day = q.get(offset=count-1)
+      assert last_sealed_day
       # Take the next unsealed day. Note that if there's a non-sealed, sealed,
       # sealed sequence of self.stats_day_cls, the non-sealed entity will be
       # skipped.
@@ -405,18 +424,22 @@ class StatisticsFramework(object):
       key_id = str(last_sealed_day.to_date() + datetime.timedelta(days=1))
       day = self.stats_day_cls.get_or_insert(
           key_id, parent=self.root_key, values=self.snapshot_cls())
+      logging.info('Selected: %s', key_id)
 
     # TODO(maruel): Should we trust it all the time or do an explicit query? For
     # now, trust the bitmap.
     hour_bit = _lowest_missing_bit(day.hours_bitmap)
-    assert hour_bit < 24, hour_bit
+    assert hour_bit < 24, (hour_bit, day.to_date())
+
     hour = self.stats_hour_cls.get_or_insert(
         '%02d' % hour_bit, parent=day.key, values=self.snapshot_cls())
     minute_bit = _lowest_missing_bit(hour.minutes_bitmap)
     assert minute_bit < 60, minute_bit
     date = day.to_date()
-    return datetime.datetime(
+    result = datetime.datetime(
         date.year, date.month, date.day, hour_bit, minute_bit)
+    logging.info('Using: %s', result)
+    return result
 
   def _process_one_minute(self, moment):
     """Generates exactly one self.stats_minute_cls.
@@ -475,7 +498,7 @@ class StatisticsFramework(object):
       futures.append(hour.put_async(use_memcache=False))
       if hour.minutes_bitmap == self.stats_hour_cls.SEALED_BITMAP:
         logging.info(
-            '%s Hour is sealed: %s-%s',
+            '%s Hour is sealed: %s %s:00',
             self.root_key_id, day.key.id(), hour.key.id())
 
     # Adds data for the past hour back into day.

services/components/tests/stats_framework_test.py
@@ -311,6 +311,7 @@ class StatsFrameworkTest(test_case.TestCase, stats_framework_mock.MockMixIn):
         handler.minute_key(date))
 
   def test_yield_empty(self):
+    self.testbed.init_modules_stub()
     self.assertEqual(
         0, len(list(stats_framework.yield_entries(None, None))))
 