425cadfec01882f3a034ac026d96b4b183bf8dad
Marc-Antoine Ruel <maruel@chromium.org>
2014-06-06 14:37:47 -0400

    Revert 25ebd5249f "Remove appstats from isolate server as an experimentation."
    Let's see if appstats was the culprit of the failures described in the initial
    commit. The failures didn't occur since 25ebd5249f was made live.
    
    TBR=vadimsh@chromium.org
    BUG=
    
    Review URL: https://codereview.appspot.com/108730048

--- services/isolate/appengine_config.py
+++ services/isolate/appengine_config.py
@@ -2,7 +2,9 @@
 # Use of this source code is governed by the Apache v2.0 license that can be
 # found in the LICENSE file.
 
-"""AppEngine global instance configuration.
+"""Configures appstats.
 
 https://developers.google.com/appengine/docs/python/tools/appengineconfig
 """
+
+appstats_CALC_RPC_COSTS = False
--- services/isolate/main_backend.py
+++ services/isolate/main_backend.py
@@ -7,18 +7,30 @@
 import os
 import sys
 
+from google.appengine.ext.appstats import recording
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(BASE_DIR, 'third_party'))
 sys.path.insert(0, os.path.join(BASE_DIR, 'components', 'third_party'))
 
 from components import ereporter2
+from components import utils
 import handlers_backend
 
 
 def create_application():
   """Bootstraps the app and creates the url router."""
   ereporter2.register_formatter()
-  return handlers_backend.create_application()
+  a = handlers_backend.create_application()
+  # In theory we'd want to take the output of app_identity.get_application_id().
+  # Sadly, this function does an RPC call and may contribute to cause time out
+  # on the initial load.
+  # Doing it here instead of appengine_config.py reduce the scope of appstats
+  # recording. To clarify, this means mapreduces started with map_reduce_jobs.py
+  # won't be instrumented, which is actually what we want in practice.
+  if utils.is_canary():
+    a = recording.appstats_wsgi_middleware(a)
+  return a
 
 
 app = create_application()
