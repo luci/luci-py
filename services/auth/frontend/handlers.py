507417af3ef526b464d800fc8c3d48be3192fcf0
Vadim Shtayura <vadimsh@chromium.org>
2014-06-19 16:27:23 -0700

    Enable 'auth' and 'auth_ui' components on Auth service.
    At some point auth_ui will probably be refactored to be a part of
    auth service itself, but for now it's easier to keep them separate.
    
    R=maruel@chromium.org
    BUG=
    
    Review URL: https://codereview.appspot.com/107210043

--- services/auth/frontend/handlers.py
+++ services/auth/frontend/handlers.py
@@ -4,30 +4,61 @@
 
 """This module defines Auth Server frontend url handlers."""
 
+import re
 import webapp2
 
-
-class RootHandler(webapp2.RequestHandler):
-  def get(self):
-    self.response.headers['Content-Type'] = 'text/plain; charset=utf-8'
-    self.response.write('Hello, world')
+from components import auth
+from components import auth_ui
+from components import utils
 
 
 class WarmupHandler(webapp2.RequestHandler):
   def get(self):
+    auth.warmup()
     self.response.headers['Content-Type'] = 'text/plain; charset=utf-8'
     self.response.write('ok')
 
 
 def get_routes():
-  return [
-    # Public URLs.
-    webapp2.Route(r'/', RootHandler),
-
-    # AppEngine-specific URLs.
-    webapp2.Route(r'/_ah/warmup', WarmupHandler),
-  ]
+  routes = []
+  routes.extend(auth_ui.get_rest_api_routes())
+  routes.extend(auth_ui.get_ui_routes())
+  routes.extend([
+    webapp2.Route(
+        r'/', webapp2.RedirectHandler, defaults={'_uri': '/auth/groups'}),
+    webapp2.Route(
+        r'/_ah/warmup', WarmupHandler),
+  ])
+  return routes
 
 
 def create_application(debug=False):
+  # Supported authentication mechanisms.
+  auth.configure([
+    auth.oauth_authentication,
+    auth.cookie_authentication,
+    auth.service_to_service_authentication,
+  ])
+
+  # URL of a commit that corresponds to currently running version.
+  rev = re.match(r'\d+-([a-f0-9]+)$', utils.get_app_version())
+  if rev:
+    app_revision_url = (
+        'https://code.google.com/p/swarming/source/detail?r=%s' % rev)
+  else:
+    app_revision_url = None
+
+  # Customize auth UI to show that it's running on Auth Service.
+  auth_ui.configure_ui(
+      app_name='Auth Service',
+      app_version=utils.get_app_version(),
+      app_revision_url=app_revision_url)
+
+  # Add a fake admin for local dev server.
+  if utils.is_local_dev_server():
+    auth.bootstrap_group(
+        auth.ADMIN_GROUP,
+        auth.Identity(auth.IDENTITY_USER, 'test@example.com'),
+        'Users that can manage groups')
+
   return webapp2.WSGIApplication(get_routes(), debug=debug)
