luci/luci-py

commit 741bdc61429e6ad22e57590cbcc7a0405bf32997

Change bot properties to use 'id' instead of 'tag'.
Change the id to be the base host name unqualified, e.g. keep 'foo' from host
'foo.bar.com'. The IDs do not need the FQDN.

Fix slave_machine.py to stop overriding 'id' consistently. The issue about the
network being up in time will be fixed later with a CL coming shortly, which
refactors this code significantly.

This change will rename all the bots with much shorter bot names, making it more
readable. So it is better to do this sooner than later. This also means
host.bar.com will conflict with host.foo.com. If this happens in practice, it is
up to the infrastructure people managing these hosts to change the id manually
in their start_slave.py.

The id can still be overriden, which is necessary for cases where multiple bots
are running on a single host, for example android testing.

R=csharp@chromium.org
BUG=

Review URL: https://codereview.appspot.com/109420043

services/swarming/swarm_bot/os_utilities.py
@@ -572,16 +572,16 @@ def get_dimensions():
   return dimensions
 
 
-def get_attributes(tag):
+def get_attributes(id_tag):
   """Returns the default Swarming dictionary of attributes for this bot.
 
-  'tag' is used to uniquely identify the bot.
+  'id' is used to uniquely identify the bot.
   'dimensions' is used for task selection.
   """
   return {
     'dimensions': get_dimensions(),
+    'id': id_tag if id_tag else get_hostname.split('.', 1)[0],
     'ip': get_ip(),
-    'tag': tag,
   }
 
 
@@ -696,8 +696,7 @@ def restart_and_return():
 def main():
   """Prints out the output of get_attributes()."""
   # Pass an empty tag, so pop it up since it has no significance.
-  attribs = get_attributes(tag='')
-  attribs.pop('tag')
+  attribs = get_attributes(None)
   pprint.pprint(attribs)
   return 0
 

services/swarming/swarm_bot/os_utilities_test.py
@@ -105,7 +105,7 @@ class TestOsUtilities(auto_stub.TestCase):
 
   def test_get_attributes(self):
     actual = os_utilities.get_attributes('id')
-    expected = set(['dimensions', 'ip', 'tag'])
+    expected = set(['dimensions', 'id', 'ip'])
     self.assertEqual(expected, set(actual))
 
     expected_dimensions = set(

services/swarming/server/bot_management.py
@@ -224,7 +224,13 @@ def validate_and_fix_attributes(attributes):
         raise test_request_message.Error(
             'Invalid value for %s: %s\n%s' % (key, value, attributes))
 
-    elif key in ('id', 'ip', 'tag', 'version'):
+    elif key == 'tag':
+      # TODO(maruel): 'tag' is now ignored but do not abort if present so
+      # upgrading can be done seamlessly. Delete this condition once all the
+      # servers and bots are upgraded.
+      pass
+
+    elif key in ('id', 'ip', 'version'):
       if not isinstance(value, basestring):
         raise test_request_message.Error(
             'Invalid value for %s: %s\n%s' % (key, value, attributes))

services/swarming/swarm_bot/slave_machine.py
@@ -15,7 +15,6 @@ import logging
 import logging.handlers
 import optparse
 import os
-import socket
 import subprocess
 import sys
 import time
@@ -199,11 +198,6 @@ class SlaveMachine(object):
       logging.error('Unable to make initial connection to the swarm server. '
                     'Aborting.')
       return
-
-    # The fully qualified domain name will uniquely identify this machine
-    # to the server, so we can use it to give a deterministic id for this slave.
-    # Also store as lower case, since it is already case-insensitive.
-    self._attributes['id'] = socket.getfqdn().lower()
 
     url = self._url + '/poll_for_test'
 
@@ -462,8 +456,8 @@ def get_attributes():
     }
     return {
       'dimensions': dimensions,
+      'id': os_utilities.get_hostname().split('.', 1)[0],
       'ip': os_utilities.get_ip(),
-      'tag': os_utilities.get_hostname(),
     }
 
 

services/swarming/swarm_bot/slave_machine_test.py
@@ -6,7 +6,6 @@
 import json
 import logging
 import os
-import socket
 import subprocess
 import sys
 import time
@@ -112,7 +111,6 @@ class TestSlaveMachine(auto_stub.TestCase):
 
   def _CreateValidAttribs(self, try_count=0):
     attributes = self.attributes.copy()
-    attributes['id'] = socket.getfqdn().lower()
     attributes['try_count'] = try_count
     return {'attributes': json.dumps(attributes)}
 

commit ff2a7dc9685a3e6e44659ab817aceae10c79a0b8

Fix a typo in os_utilities.py.
The bug was introduced in 741bdc61429e6ad22e57590cbcc7a0405bf32997. Add a small
regression test that would have caught it.

TBR=csharp@chromium.org
BUG=

Review URL: https://codereview.appspot.com/107460043

services/swarming/swarm_bot/os_utilities.py
@@ -580,7 +580,7 @@ def get_attributes(id_tag):
   """
   return {
     'dimensions': get_dimensions(),
-    'id': id_tag if id_tag else get_hostname.split('.', 1)[0],
+    'id': id_tag if id_tag else get_hostname().split('.', 1)[0],
     'ip': get_ip(),
   }
 

services/swarming/swarm_bot/os_utilities_test.py
@@ -116,6 +116,11 @@ class TestOsUtilities(auto_stub.TestCase):
       expected_dimensions.add('integrity')
     self.assertEqual(expected_dimensions, set(actual['dimensions']))
 
+  def test_get_attributes_none(self):
+    actual = os_utilities.get_attributes(None)
+    expected = set(['dimensions', 'id', 'ip'])
+    self.assertEqual(expected, set(actual))
+
   def test_setup_auto_startup_win(self):
     # TODO(maruel): Figure out a way to test properly.
     pass