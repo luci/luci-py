7b0f35abbb622179318d9e61c461245d29ae8f24
Marc-Antoine Ruel <maruel@chromium.org>
2014-09-02 21:37:05 -0400

    Remove the experimental golang implementation.
    It won't be used short-term, so remove it in the meantime to not keep dead code
    in the tree.
    
    R=vadimsh@chromium.org
    BUG=
    
    Review URL: https://codereview.appspot.com/139090043

--- services-go/pkg/aedmztest/doc.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2013 The Swarming Authors. All rights reserved.
-// Use of this source code is governed by the Apache v2.0 license that can be
-// found in the LICENSE file.
-
-// Package aedmztest is an AppEngine abstraction layer for unit testing.
-//
-// Coupled with the package aedmz, it enables unit testing that properly
-// simulate a running (local or AppEngine) environment.
-package aedmztest
