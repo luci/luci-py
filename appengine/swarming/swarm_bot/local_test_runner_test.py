luci/luci-py

commit 0f772be54d9ec7a018d322ba3ea50672653816d6

Fix bad regression, bots stopped auto-restarting.
This was a bug introduced with the new bot code. Added a unit test to ensure
that the bot restarts when and only when the child process returns non-zero.

Both local_test_runner and slave_machine were incorrect, fixed both.

R=vadimsh@chromium.org
BUG=

Review URL: https://codereview.appspot.com/146790043

appengine/swarming/swarm_bot/local_test_runner.py
@@ -47,6 +47,15 @@ MAX_PACKET_INTERVAL = 30
 MIN_PACKET_INTERNAL = 10
 
 
+# Exit code used to restart the host. Keep in sync with slave_machine.py. The
+# reason for its existance is that if an exception occurs, local_test_runner's
+# exit code will be 1. If the process is killed, it'll likely be -9. In these
+# cases, we want the task to be marked as error. But if local_test_runner wants
+# to reboot without marking the task as an internal failure, a special code must
+# be used.
+RESTART_CODE = 89
+
+
 def download_data(root_dir, files):
   """Downloads and expands the zip files enumerated in the test run data."""
   for data_url, _ in files:
@@ -93,6 +102,9 @@ def load_and_run(filename, swarming_server):
   This may throw all sorts of exceptions in case of failure. It's up to the
   caller to trap them. These shall be considered 'internal_failure' instead of
   'failure' from a TaskRunResult standpoint.
+
+  Return:
+    True on success, False if the task failed.
   """
   # The work directory is guaranteed to exist since it was created by
   # slave_machine.py and contains the manifest. Temporary files will be
@@ -108,9 +120,11 @@ def load_and_run(filename, swarming_server):
   # Download the script to run in the temporary directory.
   download_data(root_dir, task_details.data)
 
+  out = True
   for index in xrange(len(task_details.commands)):
-    run_command(swarming_server, index, task_details, root_dir)
-  return 0
+    out = out and not bool(
+        run_command(swarming_server, index, task_details, root_dir))
+  return out
 
 
 def post_update(swarming_server, params, exit_code, stdout, output_chunk_start):
@@ -170,7 +184,7 @@ def run_command(swarming_server, index, task_details, root_dir):
   server can ensure they are processed in order.
 
   Returns:
-    Number of stdout bytes streamed to the server.
+    Child process exit code.
   """
   # Signal the command is about to be started.
   params = {
@@ -248,8 +262,9 @@ def run_command(swarming_server, index, task_details, root_dir):
     post_update(swarming_server, params, exit_code, stdout, output_chunk_start)
     output_chunk_start += len(stdout)
     stdout = ''
+
   assert not stdout
-  return output_chunk_start
+  return exit_code
 
 
 def main(args):
@@ -271,7 +286,10 @@ def main(args):
   on_error.report_on_exception_exit(options.swarming_server)
 
   remote = url_helper.XsrfRemote(options.swarming_server)
-  return load_and_run(options.request_file_name, remote)
+  if not load_and_run(options.request_file_name, remote):
+    # This means it's time for the bot to reboot but it's not task_error worthy.
+    return RESTART_CODE
+  return 0
 
 
 if __name__ == '__main__':

appengine/swarming/swarm_bot/local_test_runner_test.py
@@ -108,6 +108,7 @@ class TestLocalTestRunner(net_utils.TestCase):
       self.assertEqual(self.work_dir, work_dir)
       self.assertTrue(isinstance(task_details, local_test_runner.TaskDetails))
       runs.append(index)
+      return 23
     self.mock(local_test_runner, 'run_command', run_command)
 
     manifest = os.path.join(self.root_dir, 'manifest')
@@ -123,7 +124,8 @@ class TestLocalTestRunner(net_utils.TestCase):
       }
       json.dump(data, f)
 
-    self.assertEqual(0, local_test_runner.load_and_run(manifest, server))
+    # run_command() returned 23 so load_and_run() returns False.
+    self.assertEqual(False, local_test_runner.load_and_run(manifest, server))
 
   def test_run_command(self):
     def check_final(kwargs):
@@ -179,7 +181,65 @@ class TestLocalTestRunner(net_utils.TestCase):
         })
     # This runs the command for real.
     self.assertEqual(
-        3, local_test_runner.run_command(server, 0, task_details, '.'))
+        0, local_test_runner.run_command(server, 0, task_details, '.'))
+
+  def test_run_command_fail(self):
+    def check_final(kwargs):
+      self.assertLess(0, kwargs['data'].pop('duration'))
+      self.assertEqual(
+          {
+            'data': {
+              'command_index': 0,
+              'exit_code': 1,
+              'hard_timeout': False,
+              'id': 'localhost',
+              'io_timeout': False,
+              'output': 'hi\n',
+              'output_chunk_start': 0,
+              'task_id': 23,
+            },
+            'headers': {'X-XSRF-Token': 'token'},
+          },
+          kwargs)
+
+    requests = [
+      (
+        'https://localhost:1/auth/api/v1/accounts/self/xsrf_token',
+        {'data': {}, 'headers': {'X-XSRF-Token-Request': '1'}},
+        {'xsrf_token': 'token'},
+      ),
+      (
+        'https://localhost:1/swarming/api/v1/bot/task_update/23',
+        {
+          'data': {'command_index': 0, 'id': 'localhost', 'task_id': 23},
+          'headers': {'X-XSRF-Token': 'token'},
+        },
+        {},
+      ),
+      (
+        'https://localhost:1/swarming/api/v1/bot/task_update/23',
+        check_final,
+        {},
+      ),
+    ]
+    self.expected_requests(requests)
+    server = url_helper.XsrfRemote('https://localhost:1/')
+
+    task_details = local_test_runner.TaskDetails(
+        {
+          'bot_id': 'localhost',
+          'commands': [
+            [sys.executable, '-c', 'import sys; print(\'hi\'); sys.exit(1)'],
+          ],
+          'data': [],
+          'env': {},
+          'hard_timeout': 6,
+          'io_timeout': 6,
+          'task_id': 23,
+        })
+    # This runs the command for real.
+    self.assertEqual(
+        1, local_test_runner.run_command(server, 0, task_details, '.'))
 
   def test_run_command_hard_timeout(self):
     # This runs the command for real.
@@ -240,7 +300,7 @@ class TestLocalTestRunner(net_utils.TestCase):
           'task_id': 23,
         })
     self.assertEqual(
-        3, local_test_runner.run_command(server, 0, task_details, '.'))
+        -9, local_test_runner.run_command(server, 0, task_details, '.'))
 
   def test_run_command_io_timeout(self):
     # This runs the command for real.
@@ -301,7 +361,7 @@ class TestLocalTestRunner(net_utils.TestCase):
           'task_id': 23,
         })
     self.assertEqual(
-        3, local_test_runner.run_command(server, 0, task_details, '.'))
+        -9, local_test_runner.run_command(server, 0, task_details, '.'))
 
   def test_run_command_large(self):
     # Method should have "self" as first argument - pylint: disable=E0213
@@ -402,7 +462,7 @@ class TestLocalTestRunner(net_utils.TestCase):
           'task_id': 23,
         })
     self.assertEqual(
-        400012, local_test_runner.run_command(server, 0, task_details, './'))
+        0, local_test_runner.run_command(server, 0, task_details, './'))
 
   def test_main(self):
     def load_and_run(manifest, swarming_server):
@@ -412,7 +472,8 @@ class TestLocalTestRunner(net_utils.TestCase):
 
     self.mock(local_test_runner, 'load_and_run', load_and_run)
     self.assertEqual(
-        0, local_test_runner.main(['-S', 'http://localhost', '-f', 'foo']))
+        local_test_runner.RESTART_CODE,
+        local_test_runner.main(['-S', 'http://localhost', '-f', 'foo']))
 
 
 if __name__ == '__main__':

appengine/swarming/swarm_bot/slave_machine.py
@@ -36,6 +36,10 @@ THIS_FILE = os.path.abspath(zipped_archive.get_main_script_path())
 ROOT_DIR = os.path.dirname(THIS_FILE)
 
 
+# See local_test_runner.py for documentation.
+RESTART_CODE = 89
+
+
 def get_attributes():
   """Returns start_slave.py's get_attributes() dict."""
   # Importing this administrator provided script could have side-effects on
@@ -235,6 +239,7 @@ def run_manifest(remote, attributes, manifest):
   # extracted before any I/O is done, like writting the manifest to disk.
   task_id = manifest['task_id']
 
+  restart_msg = None
   try:
     # We currently do not clean up the 'work' directory now is it compartmented.
     # TODO(maruel): Compartmentation should be done via tag. It is important to
@@ -257,16 +262,25 @@ def run_manifest(remote, attributes, manifest):
         command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=ROOT_DIR)
     out = proc.communicate()[0]
 
-    if proc.returncode:
+    # TODO(maruel): Ask start_slave.py before rebooting.
+    # https://code.google.com/p/swarming/issues/detail?id=159
+    if proc.returncode == RESTART_CODE:
+      restart_msg = 'Restarting due to task failure'
+    elif proc.returncode:
       # TODO(maruel): manifest/task number.
-      raise ValueError('Execution failed, internal error:\n%s' % out)
-
-    # At this point the script called by subprocess has handled any further
-    # communication with the swarming server.
-    logging.debug('done!')
+      restart_msg = 'Execution failed, internal error:\n%s' % out
+      post_error_task(remote, attributes, restart_msg, task_id)
+    else:
+      # At this point the script called by subprocess has handled any further
+      # communication with the swarming server.
+      logging.debug('done!')
   except Exception as e:
     # Cancel task_id with internal_failure.
-    post_error_task(remote, attributes, str(e), task_id)
+    restart_msg = 'Internal exception occured: %s' % str(e)
+    post_error_task(remote, attributes, restart_msg, task_id)
+  finally:
+    if restart_msg:
+      restart_bot(remote, attributes, restart_msg)
 
 
 def update_bot(remote, attributes, version):

appengine/swarming/swarm_bot/slave_machine_test.py
@@ -265,9 +265,7 @@ class TestSlaveMachine(net_utils.TestCase):
     self.assertTrue(slave_machine.poll_server(server, {'a': 1}, {'s': 1}))
     self.assertEqual([(server, {'a': 1}, 'Please die now')], restart)
 
-  def test_run_manifest(self):
-    self.mock(slave_machine, 'post_error', self.fail)
-
+  def _mock_popen(self, returncode):
     # Method should have "self" as first argument - pylint: disable=E0213
     class Popen(object):
       def __init__(self2, cmd, cwd, stdout, stderr):
@@ -282,13 +280,36 @@ class TestSlaveMachine(net_utils.TestCase):
         self.assertEqual(subprocess.STDOUT, stderr)
 
       def communicate(self2):
-        self2.returncode = 0
+        self2.returncode = returncode
         return 'foo', None
     self.mock(subprocess, 'Popen', Popen)
+
+  def test_run_manifest(self):
+    self.mock(slave_machine, 'post_error', self.fail)
+    self.mock(slave_machine, 'post_error_task', self.fail)
+    self.mock(slave_machine, 'restart_bot', self.fail)
+    self._mock_popen(0)
 
     server = url_helper.XsrfRemote('https://localhost:1/')
     manifest = {'task_id': 24}
     slave_machine.run_manifest(server, {}, manifest)
+
+  def test_run_manifest_restart(self):
+    self.mock(slave_machine, 'post_error', self.fail)
+    posted = []
+    self.mock(
+        slave_machine, 'post_error_task', lambda *args: posted.append(args))
+    restarted = []
+    self.mock(slave_machine, 'restart_bot', lambda *_: restarted.append(True))
+
+    self._mock_popen(1)
+    server = url_helper.XsrfRemote('https://localhost:1/')
+    server.token = 'foo'
+    manifest = {'task_id': 24}
+    slave_machine.run_manifest(server, {}, manifest)
+    self.assertEqual(
+        [(server, {}, 'Execution failed, internal error:\nfoo', 24)], posted)
+    self.assertEqual([True], restarted)
 
   def test_update_bot_linux(self):
     self.mock(sys, 'platform', 'linux2')

commit 7fa51abf4b335f0be8794b392e790a592192e0f4

Fix obvious bug introduced in 0f772be54d.
When a command would return non-zero, the following commands would be skip. This
would cause the server to wait for the results and mark the task as BOT_DIED.

Add regression unit test.

R=vadimsh@chromium.org
BUG=chromium:418122

Review URL: https://codereview.appspot.com/146340045

appengine/swarming/swarm_bot/local_test_runner.py
@@ -122,8 +122,8 @@ def load_and_run(filename, swarming_server):
 
   out = True
   for index in xrange(len(task_details.commands)):
-    out = out and not bool(
-        run_command(swarming_server, index, task_details, root_dir))
+    exit_code = run_command(swarming_server, index, task_details, root_dir)
+    out = out and not bool(exit_code)
   return out
 
 

appengine/swarming/swarm_bot/local_test_runner_test.py
@@ -108,7 +108,7 @@ class TestLocalTestRunner(net_utils.TestCase):
       self.assertEqual(self.work_dir, work_dir)
       self.assertTrue(isinstance(task_details, local_test_runner.TaskDetails))
       runs.append(index)
-      return 23
+      return 0
     self.mock(local_test_runner, 'run_command', run_command)
 
     manifest = os.path.join(self.root_dir, 'manifest')
@@ -124,8 +124,46 @@ class TestLocalTestRunner(net_utils.TestCase):
       }
       json.dump(data, f)
 
-    # run_command() returned 23 so load_and_run() returns False.
+    self.assertEqual(True, local_test_runner.load_and_run(manifest, server))
+    self.assertEqual([0, 1], runs)
+
+  def test_load_and_run_fail(self):
+    requests = [
+      (
+        'https://localhost:1/f',
+        {},
+        compress_to_zip({'file3': 'content3'}),
+        None,
+      ),
+    ]
+    self.expected_requests(requests)
+    server = url_helper.XsrfRemote('https://localhost:1/')
+
+    runs = []
+    def run_command(swarming_server, index, task_details, work_dir):
+      self.assertEqual(server, swarming_server)
+      self.assertEqual(self.work_dir, work_dir)
+      self.assertTrue(isinstance(task_details, local_test_runner.TaskDetails))
+      runs.append(index)
+      # Fails the first, pass the second.
+      return 1 if len(runs) == 1 else 0
+    self.mock(local_test_runner, 'run_command', run_command)
+
+    manifest = os.path.join(self.root_dir, 'manifest')
+    with open(manifest, 'wb') as f:
+      data = {
+        'bot_id': 'localhost',
+        'commands': [['a'], ['b', 'c']],
+        'env': {'d': 'e'},
+        'data': [('https://localhost:1/f', 'foo.zip')],
+        'hard_timeout': 10,
+        'io_timeout': 11,
+        'task_id': 23,
+      }
+      json.dump(data, f)
+
     self.assertEqual(False, local_test_runner.load_and_run(manifest, server))
+    self.assertEqual([0, 1], runs)
 
   def test_run_command(self):
     def check_final(kwargs):