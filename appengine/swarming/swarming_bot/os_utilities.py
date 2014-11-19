d4c6a453900e0be6124808ed54ac21495a5a6921
Sergiy Byelozyorov <sergiyb@google.com>
2014-11-19 19:06:28 +0100

    On non-Retina Macs, there is no spdisplays_retina key in spdiplays_ndrvs list entries
    I've tested modified function in an interactive Python session on both Retina and non-Retina Macs
    
    BUG=426897
    R=maruel@chromium.org, phajdan.jr@chromium.org, *maruel@chromium.org, kbr@chromium.org
    
    Review URL: https://codereview.appspot.com/175330043

--- appengine/swarming/swarming_bot/os_utilities.py
+++ appengine/swarming/swarming_bot/os_utilities.py
@@ -247,7 +247,7 @@ def _get_gpu_osx():
 def _get_monitor_hidpi_osx():
   """Returns True if the monitor is hidpi."""
   return any(
-    any(m['spdisplays_retina'] == 'spdisplays_yes'
+    any(m.get('spdisplays_retina') == 'spdisplays_yes'
         for m in card['spdisplays_ndrvs'])
     for card in _get_SPDisplaysDataType_osx()
     if 'spdisplays_ndrvs' in card)
