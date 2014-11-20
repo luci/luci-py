luci/luci-py

commit 2233ff98e26c11fc6ec8130cf85a38ed39df21bc

Update cron_handle_bot_died() to use proper transaction.
- Switch the query to be keys_only=True, and simplify the DB operations to be
  more efficient.
- Add better unit test coverage.
- Fix inconsistency in TaskResultSummary content when a *retry* expires.

R=vadimsh@chromium.org
BUG=swarming:62

Review URL: https://codereview.appspot.com/180730044

appengine/swarming/server/task_scheduler.py
@@ -35,7 +35,7 @@ def _secs_to_ms(value):
   return int(round(value * 1000.))
 
 
-def _expire_task(to_run_key):
+def _expire_task(to_run_key, request):
   """Expires a TaskResultSummary and unschedules the TaskToRun.
 
   Returns:
@@ -48,9 +48,8 @@ def _expire_task(to_run_key):
     logging.info('Not reapable anymore')
     return None
 
-  request_key = task_to_run.task_to_run_key_to_request_key(to_run_key)
   result_summary_key = task_result.request_key_to_result_summary_key(
-      request_key)
+      request.key)
 
   def run():
     # Fetch the two entities concurrently.
@@ -66,7 +65,8 @@ def _expire_task(to_run_key):
     if result_summary.try_number:
       # It's a retry that is being expired. Keep the old state. That requires an
       # additional pipelined GET but that shouldn't be the common case.
-      result_summary.state = result_summary.run_result_key.get().state
+      run_result = result_summary.run_result_key.get()
+      result_summary.set_from_run_result(run_result, request)
     else:
       result_summary.state = task_result.State.EXPIRED
     result_summary.abandoned_ts = utils.utcnow()
@@ -177,25 +177,94 @@ def _update_stats(run_result, bot_id, request, completed):
         dimensions=request.properties.dimensions)
 
 
-@ndb.transactional
-def _retry_task(request, result_summary, run_result, now):
-  """Registers a TaskToRun as to be tried again.
+def _handle_dead_bot(run_result_key):
+  """Handles TaskRunResult where its bot has stopped showing sign of life.
 
-  Transactionally sets the TaskRunResult as BOT_DIED. Set the TaskRunResult as
-  dead but do NOT update TaskResultSummary. This is important so the client is
-  not confused about the state.
+  Transactionally updates the entities depending on the state of this task. The
+  task may be retried automatically, canceled or left alone.
 
-  Returns True on success.
+  Returns:
+    True if the task was retried, False if the task was killed, None if no
+    action was done.
   """
-  to_run = task_to_run.retry(request, now)
-  if not to_run:
-    return False
-  run_result.state = task_result.State.BOT_DIED
-  run_result.internal_failure = True
-  run_result.abandoned_ts = now
-  result_summary.state = task_result.State.PENDING
-  ndb.put_multi((to_run, result_summary, run_result))
-  return True
+  result_summary_key = task_result.run_result_key_to_result_summary_key(
+      run_result_key)
+  request_key = task_result.result_summary_key_to_request_key(
+      result_summary_key)
+  request_future = request_key.get_async()
+  now = utils.utcnow()
+  server_version = utils.get_app_version()
+  packed = task_result.pack_run_result_key(run_result_key)
+  logging.info('Handling dead bot task %s', packed)
+  request = request_future.get_result()
+  to_run_key = task_to_run.request_to_task_to_run_key(request)
+
+  def run():
+    """Returns tuple(Result, bot_id)."""
+    # Do one GET, one PUT at the end.
+    run_result, result_summary, to_run = ndb.get_multi(
+        (run_result_key, result_summary_key, to_run_key))
+    if run_result.state != task_result.State.RUNNING:
+      # It was updated already or not updating last. Likely DB index was stale.
+      return None, run_result.bot_id
+
+    if (not run_result.server_versions or
+        run_result.server_versions[-1] != server_version):
+      run_result.server_versions.append(server_version)
+
+    if result_summary.try_number != run_result.try_number:
+      # Not updating correct run_result, cancel it without touching
+      # result_summary.
+      to_put = (run_result,)
+      run_result.state = task_result.State.BOT_DIED
+      run_result.internal_failure = True
+      run_result.abandoned_ts = now
+      result = False
+    elif result_summary.try_number == 1:
+      # Retry it.
+      to_put = (run_result, result_summary, to_run)
+      to_run.queue_number = task_to_run.gen_queue_number_key(request)
+      run_result.state = task_result.State.BOT_DIED
+      run_result.internal_failure = True
+      run_result.abandoned_ts = now
+      # Do not sync data from run_result to result_summary, since the task is
+      # being retried.
+      result_summary.reset_to_pending()
+      result = True
+    else:
+      # Cancel it, there was more than one try.
+      to_put = (run_result, result_summary)
+      run_result.state = task_result.State.BOT_DIED
+      run_result.internal_failure = True
+      run_result.abandoned_ts = now
+      result_summary.set_from_run_result(run_result, request)
+      result = False
+    ndb.put_multi(to_put)
+    return result, run_result.bot_id
+
+  try:
+    success, bot_id = ndb.transaction(run)
+    if success is not None:
+      task_to_run.set_lookup_cache(to_run_key, success)
+      if not success:
+        stats.add_run_entry(
+            'run_bot_died', run_result_key,
+            bot_id=bot_id[0],
+            dimensions=request.properties.dimensions,
+            user=request.user)
+      else:
+        logging.info('Retried')
+    else:
+      logging.info('Ignored')
+    return success
+  except (
+      apiproxy_errors.CancelledError,
+      datastore_errors.BadRequestError,
+      datastore_errors.Timeout,
+      datastore_errors.TransactionFailedError,
+      RuntimeError) as e:
+    logging.info('Failed retrying task\n%s\n%s', packed, e)
+    return None
 
 
 def _copy_entity(src, dst, skip_list):
@@ -487,10 +556,9 @@ def cron_abort_expired_task_to_run():
   skipped = 0
   try:
     for to_run in task_to_run.yield_expired_task_to_run():
-      request_future = to_run.request_key.get_async()
-      if _expire_task(to_run.key):
+      request = to_run.request_key.get()
+      if _expire_task(to_run.key, request):
         killed += 1
-        request = request_future.get_result()
         stats.add_task_entry(
             'task_request_expired',
             task_result.request_key_to_result_summary_key(
@@ -500,7 +568,6 @@ def cron_abort_expired_task_to_run():
       else:
         # It's not a big deal, the bot will continue running.
         skipped += 1
-        request_future.wait()
   finally:
     # TODO(maruel): Use stats_framework.
     logging.info('Killed %d task, skipped %d', killed, skipped)
@@ -513,25 +580,19 @@ def cron_handle_bot_died():
   If the task was at its first try, it'll be retried. Otherwise the task will be
   canceled.
   """
-  retried = 0
+  ignored = 0
   killed = 0
+  retried = 0
   try:
-    for run_result in task_result.yield_run_results_with_dead_bot():
-      if run_result.try_number == 1:
-        packed = task_result.pack_run_result_key(run_result.key)
-        request, result_summary = ndb.get_multi(
-            (run_result.request_key, run_result.result_summary_key))
-        if result_summary.try_number == 1:
-          if _retry_task(request, result_summary, run_result, utils.utcnow()):
-            task_to_run.set_lookup_cache(
-                task_to_run.request_to_task_to_run_key(request), True)
-            retried += 1
-            logging.info('Retried task %s', packed)
-            continue
-          logging.info('Failed retrying task %s', packed)
-      bot_kill_task(run_result)
-      killed += 1
+    for run_result_key in task_result.yield_run_result_keys_with_dead_bot():
+      result = _handle_dead_bot(run_result_key)
+      if result is True:
+        retried += 1
+      elif result is False:
+        killed += 1
+      else:
+        ignored += 1
   finally:
     # TODO(maruel): Use stats_framework.
-    logging.info('Killed %d tasks; retried %d tasks', killed, retried)
-  return killed, retried
+    logging.info('Killed %d; retried %d; ignored: %d', killed, retried, ignored)
+  return killed, retried, ignored

appengine/swarming/server/task_to_run.py
@@ -196,6 +196,11 @@ def task_to_run_key_to_request_key(task_key):
   return task_key.parent()
 
 
+def gen_queue_number_key(request):
+  """Returns the value to use for TaskToRun.queue_number based on request."""
+  return _gen_queue_number_key(request.created_ts, request.priority)
+
+
 def new_task_to_run(request):
   """Returns a fresh new TaskToRun for the task ready to be scheduled.
 
@@ -204,7 +209,7 @@ def new_task_to_run(request):
   """
   return TaskToRun(
       key=request_to_task_to_run_key(request),
-      queue_number=_gen_queue_number_key(request.created_ts, request.priority),
+      queue_number=gen_queue_number_key(request),
       expiration_ts=request.expiration_ts)
 
 
@@ -472,6 +477,5 @@ def retry(request, now):
     return None
 
   # The original queue_number is used but expiration_ts is unchanged.
-  to_run.queue_number = _gen_queue_number_key(
-      request.created_ts, request.priority)
+  to_run.queue_number = gen_queue_number_key(request)
   return to_run

appengine/swarming/server/task_scheduler_test.py
@@ -599,12 +599,11 @@ class TaskSchedulerApiTest(test_case.TestCase):
     }
     _request, run_result = task_scheduler.bot_reap_task(
         bot_dimensions, 'localhost', 'abc')
-    run_result.state = task_result.State.BOT_DIED
-    run_result.put()
+    now_1 = self.mock_now(self.now + task_result.BOT_PING_TOLERANCE, 1)
+    self.assertEqual((0, 1, 0), task_scheduler.cron_handle_bot_died())
+    self.assertEqual(task_result.State.BOT_DIED, run_result.key.get().state)
     self.assertEqual(
-        True,
-        task_scheduler._retry_task(
-            request, result_summary, run_result, utils.utcnow()))
+        task_result.State.PENDING, run_result.result_summary_key.get().state)
 
     # BOT_DIED is kept instead of EXPIRED.
     abandoned_ts = self.mock_now(self.now, data['scheduling_expiration_secs']+1)
@@ -621,7 +620,7 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'exit_codes': [],
       'failure': False,
       'id': '14350e868888800',
-      'internal_failure': False,
+      'internal_failure': True,
       'modified_ts': abandoned_ts,
       'name': u'Request name',
       'properties_hash': None,
@@ -634,7 +633,7 @@ class TaskSchedulerApiTest(test_case.TestCase):
     self.assertEqual(expected, result_summary.key.get().to_dict())
 
   def test_cron_handle_bot_died(self):
-    # TODO(maruel): Test expired tasks.
+    # Test first retry, then success.
     self.mock(random, 'getrandbits', lambda _: 0x88)
     data = _gen_request_data(
         properties=dict(dimensions={u'OS': u'Windows-3.1.1'}),
@@ -648,13 +647,13 @@ class TaskSchedulerApiTest(test_case.TestCase):
     _request, run_result = task_scheduler.bot_reap_task(
         bot_dimensions, 'localhost', 'abc')
     self.assertEqual(1, run_result.try_number)
-    self.mock_now(self.now + task_result.BOT_PING_TOLERANCE, 1)
-    self.assertEqual((0, 1), task_scheduler.cron_handle_bot_died())
+    self.assertEqual(task_result.State.RUNNING, run_result.state)
+    now_1 = self.mock_now(self.now + task_result.BOT_PING_TOLERANCE, 1)
+    self.assertEqual((0, 1, 0), task_scheduler.cron_handle_bot_died())
 
     # Refresh and compare:
-    run_result = run_result.key.get()
     expected = {
-      'abandoned_ts': datetime.datetime(2014, 1, 2, 3, 9, 6, 6),
+      'abandoned_ts': now_1,
       'bot_id': u'localhost',
       'bot_version': u'abc',
       'completed_ts': None,
@@ -663,39 +662,38 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'failure': False,
       'id': '14350e868888801',
       'internal_failure': True,
-      'modified_ts': datetime.datetime(2014, 1, 2, 3, 9, 6, 6),
+      'modified_ts': now_1,
       'server_versions': [u'default-version'],
-      'started_ts': datetime.datetime(2014, 1, 2, 3, 4, 5, 6),
+      'started_ts': self.now,
       'state': task_result.State.BOT_DIED,
       'try_number': 1,
     }
-    self.assertEqual(expected, run_result.to_dict())
-    result_summary = run_result.result_summary_key.get()
+    self.assertEqual(expected, run_result.key.get().to_dict())
     expected = {
       'abandoned_ts': None,
       'bot_id': u'localhost',
       'bot_version': u'abc',
       'completed_ts': None,
-      'created_ts': datetime.datetime(2014, 1, 2, 3, 4, 5, 6),
+      'created_ts': self.now,
       'deduped_from': None,
       'durations': [],
       'exit_codes': [],
       'failure': False,
       'id': '14350e868888800',
       'internal_failure': False,
-      'modified_ts': datetime.datetime(2014, 1, 2, 3, 9, 6, 6),
+      'modified_ts': now_1,
       'name': u'Request name',
       'properties_hash': None,
       'server_versions': [u'default-version'],
-      'started_ts': datetime.datetime(2014, 1, 2, 3, 4, 5, 6),
+      'started_ts': None,
       'state': task_result.State.PENDING,
       'try_number': 1,
       'user': u'Jesus',
     }
-    self.assertEqual(expected, result_summary.to_dict())
+    self.assertEqual(expected, run_result.result_summary_key.get().to_dict())
 
     # Task was retried.
-    self.mock_now(self.now + task_result.BOT_PING_TOLERANCE, 2)
+    now_2 = self.mock_now(self.now + task_result.BOT_PING_TOLERANCE, 2)
     _request, run_result = task_scheduler.bot_reap_task(
         bot_dimensions, 'localhost', 'abc')
     logging.info('%s', [t.to_dict() for t in task_to_run.TaskToRun.query()])
@@ -703,29 +701,100 @@ class TaskSchedulerApiTest(test_case.TestCase):
     with task_scheduler.bot_update_task(
         run_result.key, 'localhost', 0, 'Foo1', 0, 0, 0.1) as (entities, _):
       ndb.put_multi(entities)
-    result_summary = run_result.result_summary_key.get()
     expected = {
       'abandoned_ts': None,
       'bot_id': u'localhost',
       'bot_version': u'abc',
-      'completed_ts': datetime.datetime(2014, 1, 2, 3, 9, 7, 6),
-      'created_ts': datetime.datetime(2014, 1, 2, 3, 4, 5, 6),
+      'completed_ts': now_2,
+      'created_ts': self.now,
       'deduped_from': None,
       'durations': [0.1],
       'exit_codes': [0],
       'failure': False,
       'id': '14350e868888800',
       'internal_failure': False,
-      'modified_ts': datetime.datetime(2014, 1, 2, 3, 9, 7, 6),
+      'modified_ts': now_2,
       'name': u'Request name',
       'properties_hash': None,
       'server_versions': [u'default-version'],
-      'started_ts': datetime.datetime(2014, 1, 2, 3, 9, 7, 6),
-      'state': 112,
+      'started_ts': now_2,
+      'state': task_result.State.COMPLETED,
       'try_number': 2,
       'user': u'Jesus',
     }
-    self.assertEqual(expected, result_summary.to_dict())
+    self.assertEqual(expected, run_result.result_summary_key.get().to_dict())
+
+  def test_cron_handle_bot_died_second(self):
+    # Test two retries leading to a BOT_DIED status.
+    self.mock(random, 'getrandbits', lambda _: 0x88)
+    data = _gen_request_data(
+        properties=dict(dimensions={u'OS': u'Windows-3.1.1'}),
+        scheduling_expiration_secs=600)
+    task_scheduler.make_request(data)
+    bot_dimensions = {
+      u'OS': [u'Windows', u'Windows-3.1.1'],
+      u'hostname': u'localhost',
+      u'foo': u'bar',
+    }
+    _request, run_result = task_scheduler.bot_reap_task(
+        bot_dimensions, 'localhost', 'abc')
+    self.assertEqual(1, run_result.try_number)
+    self.assertEqual(task_result.State.RUNNING, run_result.state)
+    self.mock_now(self.now + task_result.BOT_PING_TOLERANCE, 1)
+    self.assertEqual((0, 1, 0), task_scheduler.cron_handle_bot_died())
+    now_1 = self.mock_now(self.now + task_result.BOT_PING_TOLERANCE, 2)
+    _request, run_result = task_scheduler.bot_reap_task(
+        bot_dimensions, 'localhost', 'abc')
+    now_2 = self.mock_now(self.now + 2 * task_result.BOT_PING_TOLERANCE, 3)
+    self.assertEqual((1, 0, 0), task_scheduler.cron_handle_bot_died())
+    self.assertEqual((0, 0, 0), task_scheduler.cron_handle_bot_died())
+    expected = {
+      'abandoned_ts': now_2,
+      'bot_id': u'localhost',
+      'bot_version': u'abc',
+      'completed_ts': None,
+      'created_ts': self.now,
+      'deduped_from': None,
+      'durations': [],
+      'exit_codes': [],
+      'failure': False,
+      'id': '14350e868888800',
+      'internal_failure': True,
+      'modified_ts': now_2,
+      'name': u'Request name',
+      'properties_hash': None,
+      'server_versions': [u'default-version'],
+      'started_ts': now_1,
+      'state': task_result.State.BOT_DIED,
+      'try_number': 2,
+      'user': u'Jesus',
+    }
+    self.assertEqual(expected, run_result.result_summary_key.get().to_dict())
+
+  def test_cron_handle_bot_died_ignored(self):
+    # Test two retries.
+    self.mock(random, 'getrandbits', lambda _: 0x88)
+    data = _gen_request_data(
+        properties=dict(dimensions={u'OS': u'Windows-3.1.1'}),
+        scheduling_expiration_secs=600)
+    task_scheduler.make_request(data)
+    bot_dimensions = {
+      u'OS': [u'Windows', u'Windows-3.1.1'],
+      u'hostname': u'localhost',
+      u'foo': u'bar',
+    }
+    _request, run_result = task_scheduler.bot_reap_task(
+        bot_dimensions, 'localhost', 'abc')
+    self.assertEqual(1, run_result.try_number)
+    self.assertEqual(task_result.State.RUNNING, run_result.state)
+    self.mock_now(self.now + task_result.BOT_PING_TOLERANCE, 1)
+    self.assertEqual((0, 1, 0), task_scheduler.cron_handle_bot_died())
+    self.mock_now(self.now + task_result.BOT_PING_TOLERANCE, 2)
+    _request, run_result = task_scheduler.bot_reap_task(
+        bot_dimensions, 'localhost', 'abc')
+    self.mock_now(self.now + 2 * task_result.BOT_PING_TOLERANCE, 3)
+    self.assertEqual((1, 0, 0), task_scheduler.cron_handle_bot_died())
+    self.assertEqual((0, 0, 0), task_scheduler.cron_handle_bot_died())
 
   def test_search_by_name(self):
     data = _gen_request_data(

appengine/swarming/server/task_to_run_test.py
@@ -79,31 +79,6 @@ def _hash_dimensions(dimensions):
 
 
 class TaskToRunPrivateTest(test_case.TestCase):
-  def test_gen_queue_number_key(self):
-    # tuples of (input, expected).
-    # 2**47 / 365 / 24 / 60 / 60 / 1000. = 4462.756
-    data = [
-      (('1970-01-01 00:00:00.000',   0), '0x0000000000000000'),
-      (('1970-01-01 00:00:00.000', 255), '0x7f80000000000000'),
-      (('1970-01-01 00:00:00.040',   0), '0x0000000000002800'),
-      (('1970-01-01 00:00:00.050',   0), '0x0000000000003200'),
-      (('1970-01-01 00:00:00.100',   0), '0x0000000000006400'),
-      (('1970-01-01 00:00:00.900',   0), '0x0000000000038400'),
-      (('1970-01-01 00:00:01.000',   0), '0x000000000003e800'),
-      (('1970-01-01 00:00:00.000',   1), '0x0080000000000000'),
-      (('1970-01-01 00:00:00.000',   2), '0x0100000000000000'),
-      (('2010-01-02 03:04:05.060',   0), '0x000125ecfd5cc400'),
-      (('2010-01-02 03:04:05.060',   1), '0x008125ecfd5cc400'),
-      # It's the end of the world as we know it...
-      (('6429-10-17 02:45:55.327',   0), '0x007fffffffffff00'),
-      (('6429-10-17 02:45:55.327', 255), '0x7fffffffffffff00'),
-      (('6429-10-17 02:45:55.327', 255), '0x7fffffffffffff00'),
-    ]
-    for (timestamp, priority), expected in data:
-      d = datetime.datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
-      actual = '0x%016x' % task_to_run._gen_queue_number_key(d, priority)
-      self.assertEquals(expected, actual)
-
   def test_powerset(self):
     # tuples of (input, expected).
     # TODO(maruel): We'd want the code to deterministically try 'Windows-6.1'
@@ -303,6 +278,31 @@ class TaskToRunApiTest(test_case.TestCase):
     task_to_run.validate_to_run_key(task_key)
     with self.assertRaises(ValueError):
       task_to_run.validate_to_run_key(ndb.Key('TaskRequest', 1, 'TaskToRun', 1))
+
+  def test_gen_queue_number_key(self):
+    # tuples of (input, expected).
+    # 2**47 / 365 / 24 / 60 / 60 / 1000. = 4462.756
+    data = [
+      (('1970-01-01 00:00:00.000',   0), '0x0000000000000000'),
+      (('1970-01-01 00:00:00.000', 255), '0x7f80000000000000'),
+      (('1970-01-01 00:00:00.040',   0), '0x0000000000002800'),
+      (('1970-01-01 00:00:00.050',   0), '0x0000000000003200'),
+      (('1970-01-01 00:00:00.100',   0), '0x0000000000006400'),
+      (('1970-01-01 00:00:00.900',   0), '0x0000000000038400'),
+      (('1970-01-01 00:00:01.000',   0), '0x000000000003e800'),
+      (('1970-01-01 00:00:00.000',   1), '0x0080000000000000'),
+      (('1970-01-01 00:00:00.000',   2), '0x0100000000000000'),
+      (('2010-01-02 03:04:05.060',   0), '0x000125ecfd5cc400'),
+      (('2010-01-02 03:04:05.060',   1), '0x008125ecfd5cc400'),
+      # It's the end of the world as we know it...
+      (('6429-10-17 02:45:55.327',   0), '0x007fffffffffff00'),
+      (('6429-10-17 02:45:55.327', 255), '0x7fffffffffffff00'),
+      (('6429-10-17 02:45:55.327', 255), '0x7fffffffffffff00'),
+    ]
+    for (timestamp, priority), expected in data:
+      d = datetime.datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
+      actual = '0x%016x' % task_to_run._gen_queue_number_key(d, priority)
+      self.assertEquals(expected, actual)
 
   def test_new_task_to_run(self):
     self.mock(random, 'getrandbits', lambda _: 0x12)

appengine/swarming/server/task_result.py
@@ -556,6 +556,14 @@ class TaskResultSummary(_TaskResultCommon):
       return None
     return result_summary_key_to_run_result_key(self.key, self.try_number)
 
+  def reset_to_pending(self):
+    """Resets this entity to pending state."""
+    self.durations = []
+    self.exit_codes = []
+    self.internal_failure = False
+    self.state = State.PENDING
+    self.started_ts = None
+
   def set_from_run_result(self, run_result, request):
     """Copies all the relevant properties from a TaskRunResult into this
     TaskResultSummary.
@@ -870,15 +878,15 @@ def new_run_result(request, try_number, bot_id, bot_version):
       server_versions=[utils.get_app_version()])
 
 
-def yield_run_results_with_dead_bot():
-  """Yields all the TaskRunResult where the bot died recently.
+def yield_run_result_keys_with_dead_bot():
+  """Yields all the TaskRunResult ndb.Key where the bot died recently.
 
-  In practice it is returning a ndb.Query but this is equivalent.
+  In practice it is returning a ndb.QueryIterator but this is equivalent.
   """
   # If a bot didn't ping recently, it is considered dead.
   deadline = utils.utcnow() - BOT_PING_TOLERANCE
   q = TaskRunResult.query().filter(TaskRunResult.modified_ts < deadline)
-  return q.filter(TaskRunResult.state == State.RUNNING)
+  return q.filter(TaskRunResult.state == State.RUNNING).iter(keys_only=True)
 
 
 def prepare_put_run_result(run_result, request):

appengine/swarming/server/task_result_test.py
@@ -403,7 +403,7 @@ class TaskResultApiTest(test_case.TestCase):
         run_result.key_string)
     self.assertEqual(complete_ts, run_result.ended_ts)
 
-  def test_yield_run_results_with_dead_bot(self):
+  def test_yield_run_result_keys_with_dead_bot(self):
     request = task_request.make_request(_gen_request_data())
     result_summary = task_result.new_result_summary(request)
     result_summary.put()
@@ -412,11 +412,13 @@ class TaskResultApiTest(test_case.TestCase):
     ndb.put_multi(task_result.prepare_put_run_result(run_result, request))
 
     self.mock_now(self.now + task_result.BOT_PING_TOLERANCE)
-    self.assertEqual([], list(task_result.yield_run_results_with_dead_bot()))
+    self.assertEqual(
+        [], list(task_result.yield_run_result_keys_with_dead_bot()))
 
     self.mock_now(self.now + task_result.BOT_PING_TOLERANCE, 1)
     self.assertEqual(
-        [run_result], list(task_result.yield_run_results_with_dead_bot()))
+        [run_result.key],
+        list(task_result.yield_run_result_keys_with_dead_bot()))
 
   def test_prepare_put_run_result(self):
     request = task_request.make_request(_gen_request_data())

commit 1f149fae1ca2a18c900603b7ae205edf21bd2192

Fix retries that could happen even when the task expired
When the request expired, the task must not be retried even in case of BOT_DIED.
This was a small regression in 2233ff98e26c11fc6ec8130cf85a38ed39df21bc.

R=vadimsh@chromium.org
BUG=swarming:62

Review URL: https://codereview.appspot.com/181760043

appengine/swarming/server/task_scheduler.py
@@ -202,7 +202,7 @@ def _handle_dead_bot(run_result_key):
       run_result.internal_failure = True
       run_result.abandoned_ts = now
       result = False
-    elif result_summary.try_number == 1:
+    elif result_summary.try_number == 1 and now < request.expiration_ts:
       # Retry it.
       to_put = (run_result, result_summary, to_run)
       to_run.queue_number = task_to_run.gen_queue_number_key(request)
@@ -214,7 +214,8 @@ def _handle_dead_bot(run_result_key):
       result_summary.reset_to_pending()
       result = True
     else:
-      # Cancel it, there was more than one try.
+      # Cancel it, there was more than one try or the task expired in the
+      # meantime.
       to_put = (run_result, result_summary)
       run_result.state = task_result.State.BOT_DIED
       run_result.internal_failure = True

appengine/swarming/server/task_to_run.py
@@ -436,22 +436,3 @@ def abort_task_to_run(task):
   task.put()
   # Add it to the negative cache.
   set_lookup_cache(task.key, False)
-
-
-def retry(request, now):
-  """Retries a task at its original priority.
-
-  If the task expired, it will not be retried.
-
-  Returns the TaskToRun entity to save in the DB on success.
-  """
-  assert ndb.in_transaction()
-  if now > request.expiration_ts:
-    return None
-  to_run = request_to_task_to_run_key(request).get()
-  if to_run.queue_number:
-    return None
-
-  # The original queue_number is used but expiration_ts is unchanged.
-  to_run.queue_number = gen_queue_number_key(request)
-  return to_run

appengine/swarming/server/task_scheduler_test.py
@@ -590,7 +590,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
   def test_cron_abort_expired_task_to_run_retry(self):
     self.mock(random, 'getrandbits', lambda _: 0x88)
     data = _gen_request_data(
-        properties=dict(dimensions={u'OS': u'Windows-3.1.1'}))
+        properties=dict(dimensions={u'OS': u'Windows-3.1.1'}),
+        scheduling_expiration_secs=600)
     request, result_summary = task_scheduler.make_request(data)
 
     # Fake first try bot died.
@@ -728,7 +729,7 @@ class TaskSchedulerApiTest(test_case.TestCase):
     self.assertEqual(expected, run_result.result_summary_key.get().to_dict())
 
   def test_cron_handle_bot_died_second(self):
-    # Test two retries leading to a BOT_DIED status.
+    # Test two tries internal_failure's leading to a BOT_DIED status.
     self.mock(random, 'getrandbits', lambda _: 0x88)
     data = _gen_request_data(
         properties=dict(dimensions={u'OS': u'Windows-3.1.1'}),
@@ -774,8 +775,7 @@ class TaskSchedulerApiTest(test_case.TestCase):
     }
     self.assertEqual(expected, run_result.result_summary_key.get().to_dict())
 
-  def test_cron_handle_bot_died_ignored(self):
-    # Test two retries.
+  def test_cron_handle_bot_died_ignored_expired(self):
     self.mock(random, 'getrandbits', lambda _: 0x88)
     data = _gen_request_data(
         properties=dict(dimensions={u'OS': u'Windows-3.1.1'}),
@@ -790,14 +790,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
         bot_dimensions, 'localhost', 'abc')
     self.assertEqual(1, run_result.try_number)
     self.assertEqual(task_result.State.RUNNING, run_result.state)
-    self.mock_now(self.now + task_result.BOT_PING_TOLERANCE, 1)
-    self.assertEqual((0, 1, 0), task_scheduler.cron_handle_bot_died())
-    self.mock_now(self.now + task_result.BOT_PING_TOLERANCE, 2)
-    _request, run_result = task_scheduler.bot_reap_task(
-        bot_dimensions, 'localhost', 'abc')
-    self.mock_now(self.now + 2 * task_result.BOT_PING_TOLERANCE, 3)
+    self.mock_now(self.now + task_result.BOT_PING_TOLERANCE, 601)
     self.assertEqual((1, 0, 0), task_scheduler.cron_handle_bot_died())
-    self.assertEqual((0, 0, 0), task_scheduler.cron_handle_bot_died())
 
   def test_search_by_name(self):
     data = _gen_request_data(

appengine/swarming/server/task_to_run_test.py
@@ -624,44 +624,6 @@ class TaskToRunApiTest(test_case.TestCase):
     self.assertEqual(
         1, len(_yield_next_available_task_to_dispatch(bot_dimensions)))
 
-  def test_retry(self):
-    to_run = _gen_new_task_to_run(
-        properties=dict(dimensions={u'OS': u'Windows-3.1.1'}))
-    bot_dimensions = {u'OS': u'Windows-3.1.1', u'hostname': u'localhost'}
-    self.assertEqual(
-        1, len(_yield_next_available_task_to_dispatch(bot_dimensions)))
-    to_run.queue_number = None
-    to_run.put()
-    self.assertEqual(
-        0, len(_yield_next_available_task_to_dispatch(bot_dimensions)))
-
-    request = to_run.request_key.get()
-    self.mock_now(self.now, 60)
-    def tx():
-      task_to_run.retry(request, utils.utcnow()).put()
-    ndb.transaction(tx)
-    self.assertEqual(
-        1, len(_yield_next_available_task_to_dispatch(bot_dimensions)))
-
-  def test_retry_expired(self):
-    to_run = _gen_new_task_to_run(
-        properties=dict(dimensions={u'OS': u'Windows-3.1.1'}))
-    bot_dimensions = {u'OS': u'Windows-3.1.1', u'hostname': u'localhost'}
-    self.assertEqual(
-        1, len(_yield_next_available_task_to_dispatch(bot_dimensions)))
-    to_run.queue_number = None
-    to_run.put()
-    self.assertEqual(
-        0, len(_yield_next_available_task_to_dispatch(bot_dimensions)))
-
-    self.mock_now(self.now, 61)
-    def tx():
-      self.assertEqual(
-          None, task_to_run.retry(to_run.request_key.get(), utils.utcnow()))
-    ndb.transaction(tx)
-    self.assertEqual(
-        0, len(_yield_next_available_task_to_dispatch(bot_dimensions)))
-
 
 if __name__ == '__main__':
   if '-v' in sys.argv: