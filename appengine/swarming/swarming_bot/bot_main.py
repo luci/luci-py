luci/luci-py

commit 4ccc27857a25e28d6f673af52dff7793cbee5e9e

Add bot_config.on_before_task().
It is meant for eventual external monitoring of the bot behavior.

R=vadimsh@chromium.org
BUG=swarming:189

Review URL: https://codereview.appspot.com/200120043

appengine/swarming/swarming_bot/bot_main.py
@@ -117,6 +117,18 @@ def get_state(sleep_streak):
   return state
 
 
+def on_before_task(botobj):
+  """Hook function called just before a task is going to be scheduled."""
+  try:
+    if _in_load_test_mode():
+      return
+
+    import bot_config
+    return bot_config.on_before_task(botobj)
+  except Exception as e:
+    botobj.post_error('Failed to call hook on_after_task()' % e)
+
+
 def on_after_task(botobj, failure, internal_failure):
   """Hook function called after a task."""
   try:
@@ -126,7 +138,7 @@ def on_after_task(botobj, failure, internal_failure):
     import bot_config
     return bot_config.on_after_task(botobj, failure, internal_failure)
   except Exception as e:
-    logging.exception('Failed to call hook on_after_task(): %s', e)
+    botobj.post_error('Failed to call hook on_after_task()' % e)
 
 
 def setup_bot(skip_reboot):
@@ -339,6 +351,7 @@ def run_manifest(botobj, manifest, start):
     path = os.path.join(work_dir, 'test_run.json')
     with open(path, 'wb') as f:
       f.write(json.dumps(manifest))
+    on_before_task(botobj)
     command = [
       sys.executable, THIS_FILE, 'task_runner',
       '--swarming-server', url,

appengine/swarming/swarming_bot/bot_config.py
@@ -56,6 +56,18 @@ def get_state():
 ### Hooks
 
 
+def on_before_task(bot):
+  """Hook function to be called before running a task.
+
+  It shouldn't do much, since it can't cancel the task so it shouldn't do
+  anything too fancy.
+
+  Arguments:
+  - bot: bot.Bot instance.
+  """
+  pass
+
+
 def on_after_task(bot, failure, internal_failure):
   """Hook function to be called after running a task.
 

commit fe5c2222c950dd75856cdc1cffea21ab36d482af

Fix dual bugs introduced in 4ccc27857a2.
Obvious string formatting error plus copy-paste error.

TBR=vadimsh@chromium.org
BUG=

Review URL: https://codereview.appspot.com/201790043

appengine/swarming/swarming_bot/bot_main.py
@@ -126,7 +126,7 @@ def on_before_task(botobj):
     import bot_config
     return bot_config.on_before_task(botobj)
   except Exception as e:
-    botobj.post_error('Failed to call hook on_after_task()' % e)
+    botobj.post_error('Failed to call hook on_before_task(): %s' % e)
 
 
 def on_after_task(botobj, failure, internal_failure):
@@ -138,7 +138,7 @@ def on_after_task(botobj, failure, internal_failure):
     import bot_config
     return bot_config.on_after_task(botobj, failure, internal_failure)
   except Exception as e:
-    botobj.post_error('Failed to call hook on_after_task()' % e)
+    botobj.post_error('Failed to call hook on_after_task(): %s' % e)
 
 
 def setup_bot(skip_reboot):