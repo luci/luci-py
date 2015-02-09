luci/luci-py

commit 610048179c644ddf2152d3eecd16b8f737f6b6b6

Add parent task id support and set SWARMING_TASK_ID.
SWARMING_TASK_ID will be used by swarming.py client tool to set parent_task_id.

This will be used to expose when reentrant tasks are used.

R=vadimsh@chromium.org
BUG=swarming:199

Review URL: https://codereview.appspot.com/197040043

appengine/swarming/handlers_frontend.py
@@ -538,12 +538,24 @@ class TaskHandler(auth.AuthenticatingHandler):
         self.abort(404, 'Invalid key format.')
 
     # 'result' can be either a TaskRunResult or TaskResultSummary.
-    result, request = ndb.get_multi([key, request_key])
+    result_future = key.get_async()
+    request_future = request_key.get_async()
+    result = result_future.get_result()
     if not result:
       self.abort(404, 'Invalid key.')
 
     if not acl.is_privileged_user():
       self.abort(403, 'Implement access control based on the user')
+
+    request = request_future.get_result()
+    parent_task_future = None
+    if request.parent_task_id:
+      parent_key = task_result.unpack_run_result_key(request.parent_task_id)
+      parent_task_future = parent_key.get_async()
+    children_tasks_futures = [
+      task_result.unpack_result_summary_key(c).get_async()
+      for c in result.children_task_ids
+    ]
 
     bot_id = result.bot_id
     following_task_future = None
@@ -566,34 +578,32 @@ class TaskHandler(auth.AuthenticatingHandler):
     bot_future = (
         bot_management.get_info_key(bot_id).get_async() if bot_id else None)
 
-    following_task_id = None
-    following_task_name = None
-    previous_task_id = None
-    previous_task_name = None
+    following_task = None
     if following_task_future:
       following_task = following_task_future.get_result()
-      if following_task:
-        following_task_id = task_pack.pack_run_result_key(following_task.key)
-        following_task_name = following_task.name
+
+    previous_task = None
     if previous_task_future:
       previous_task = previous_task_future.get_result()
-      if previous_task:
-        previous_task_id = task_pack.pack_run_result_key(previous_task.key)
-        previous_task_name = previous_task.name
+
+    parent_task = None
+    if parent_task_future:
+      parent_task = parent_task_future.get_result()
+    children_tasks = [c.get_result() for c in children_tasks_futures]
 
     params = {
       'bot': bot_future.get_result() if bot_future else None,
+      'children_tasks': children_tasks,
       'is_admin': acl.is_admin(),
       'is_gae_admin': users.is_current_user_admin(),
       'is_privileged_user': acl.is_privileged_user(),
-      'following_task_id': following_task_id,
-      'following_task_name': following_task_name,
+      'following_task': following_task,
       'full_appid': os.environ['APPLICATION_ID'],
       'host_url': self.request.host_url,
       'is_running': result.state == task_result.State.RUNNING,
       'now': utils.utcnow(),
-      'previous_task_id': previous_task_id,
-      'previous_task_name': previous_task_name,
+      'parent_task': parent_task,
+      'previous_task': previous_task,
       'request': request,
       'task': result,
       'xsrf_token': self.generate_xsrf_token(),

appengine/swarming/handlers_api_test.py
@@ -181,6 +181,7 @@ class ClientApiTest(test_env_handlers.AppTestBase):
             (now + datetime.timedelta(seconds=30)).strftime(
                 utils.DATETIME_FORMAT)),
         u'name': u'job1',
+        u'parent_task_id': None,
         u'priority': 200,
         u'properties': {
           u'commands': [[u'rm', u'-rf', u'/']],
@@ -228,6 +229,7 @@ class ClientApiTest(test_env_handlers.AppTestBase):
       u'abandoned_ts': str_now,
       u'bot_id': None,
       u'bot_version': None,
+      u'children_task_ids': [],
       u'completed_ts': None,
       u'costs_usd': [],
       u'cost_saved_usd': None,
@@ -266,6 +268,7 @@ class ClientApiTest(test_env_handlers.AppTestBase):
       u'abandoned_ts': None,
       u'bot_id': None,
       u'bot_version': None,
+      u'children_task_ids': [],
       u'completed_ts': None,
       u'costs_usd': [],
       u'cost_saved_usd': None,
@@ -304,6 +307,7 @@ class ClientApiTest(test_env_handlers.AppTestBase):
       u'abandoned_ts': None,
       u'bot_id': u'bot1',
       u'bot_version': self.bot_version,
+      u'children_task_ids': [],
       u'completed_ts': None,
       u'cost_usd': 0.,
       u'durations': [],
@@ -458,6 +462,7 @@ class ClientApiTest(test_env_handlers.AppTestBase):
       u'expiration_ts': unicode(
           (now + datetime.timedelta(days=1)).strftime(utils.DATETIME_FORMAT)),
       u'name': u'hi',
+      u'parent_task_id': None,
       u'priority': 100,
       u'properties': {
         u'commands': [[u'python', u'run_test.py']],
@@ -501,6 +506,7 @@ class ClientApiTest(test_env_handlers.AppTestBase):
       u'abandoned_ts': None,
       u'bot_id': u'bot1',
       u'bot_version': self.bot_version,
+      u'children_task_ids': [],
       u'completed_ts': now_str,
       u'costs_usd': [0.1],
       u'cost_saved_usd': None,
@@ -524,6 +530,7 @@ class ClientApiTest(test_env_handlers.AppTestBase):
       u'abandoned_ts': None,
       u'bot_id': u'bot1',
       u'bot_version': self.bot_version,
+      u'children_task_ids': [],
       u'completed_ts': now_str,
       u'costs_usd': [],
       u'cost_saved_usd': 0.1,
@@ -757,6 +764,7 @@ class ClientApiTest(test_env_handlers.AppTestBase):
           u'abandoned_ts': None,
           u'bot_id': u'bot1',
           u'bot_version': self.bot_version,
+          u'children_task_ids': [],
           u'completed_ts': now_1_str,
           u'cost_usd': 0.1,
           u'durations': [0.1],
@@ -786,6 +794,7 @@ class ClientApiTest(test_env_handlers.AppTestBase):
           u'abandoned_ts': None,
           u'bot_id': u'bot1',
           u'bot_version': self.bot_version,
+          u'children_task_ids': [],
           u'completed_ts': now_str,
           u'cost_usd': 0.1,
           u'durations': [0.1],

appengine/swarming/handlers_bot_test.py
@@ -250,6 +250,7 @@ class BotApiTest(test_env_handlers.AppTestBase):
       u'abandoned_ts': None,
       u'bot_id': u'bot1',
       u'bot_version': self.bot_version,
+      u'children_task_ids': [],
       u'completed_ts': None,
       u'cost_usd': 0.,
       u'durations': [],
@@ -375,6 +376,7 @@ class BotApiTest(test_env_handlers.AppTestBase):
         u'abandoned_ts': None,
         u'bot_id': u'bot1',
         u'bot_version': self.bot_version,
+        u'children_task_ids': [],
         u'completed_ts': None,
         u'cost_usd': 0.1,
         u'durations': [],
@@ -526,6 +528,7 @@ class BotApiTest(test_env_handlers.AppTestBase):
       u'abandoned_ts': None,
       u'bot_id': u'bot1',
       u'bot_version': self.bot_version,
+      u'children_task_ids': [],
       u'completed_ts': str_now,
       u'cost_usd': 0.1,
       u'durations': [0.1],
@@ -573,6 +576,7 @@ class BotApiTest(test_env_handlers.AppTestBase):
       u'abandoned_ts': str_now,
       u'bot_id': u'bot1',
       u'bot_version': self.bot_version,
+      u'children_task_ids': [],
       u'completed_ts': None,
       u'cost_usd': 0.,
       u'durations': [],

appengine/swarming/local_smoke_test.py
@@ -131,6 +131,7 @@ def gen_expected(**kwargs):
   expected = {
     u'abandoned_ts': None,
     u'bot_id': unicode(socket.getfqdn().split('.', 1)[0]),
+    u'children_task_ids': [],
     u'cost_saved_usd': None,
     u'deduped_from': None,
     u'exit_codes': [0],

appengine/swarming/remote_smoke_test.py
@@ -6,6 +6,8 @@
 """Integration test for the Swarming server."""
 
 import Queue
+import json
+import logging
 import optparse
 import os
 import shutil
@@ -18,25 +20,45 @@ import time
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
 CHECKOUT_DIR = os.path.dirname(os.path.dirname(APP_DIR))
 CLIENT_DIR = os.path.join(CHECKOUT_DIR, 'client')
-ISOLATE_SCRIPT = os.path.join(CLIENT_DIR, 'isolate.py')
 SWARMING_SCRIPT = os.path.join(CLIENT_DIR, 'swarming.py')
 
 
-def gen_isolate(isolate, script):
+def gen_isolated(isolate, script, includes=None):
   """Archives a script to `isolate` server."""
   tmp = tempfile.mkdtemp(prefix='swarming_smoke')
+  data = {
+    'variables': {
+      'command': ['python', '-u', 'script.py'],
+      'files': ['script.py'],
+    },
+  }
   try:
     with open(os.path.join(tmp, 'script.py'), 'wb') as f:
       f.write(script)
     path = os.path.join(tmp, 'script.isolate')
     with open(path, 'wb') as f:
-      f.write(
-          "{'variables': {\n"
-          "'command': ['python', 'script.py'], 'files': ['script.py']\n"
-          "}}")
+      # This file is actually python but it's #closeenough.
+      json.dump(data, f, sort_keys=True, separators=(',', ':'))
     isolated = os.path.join(tmp, 'script.isolated')
-    out = subprocess.check_output(
-        [ISOLATE_SCRIPT, 'archive', '-I', isolate, '-i', path, '-s', isolated])
+    cmd = [
+      os.path.join(CLIENT_DIR, 'isolate.py'), 'archive',
+      '-I', isolate, '-i', path, '-s', isolated,
+    ]
+    out = subprocess.check_output(cmd)
+    if includes:
+      # Mangle the .isolated to include another one. A bit hacky but works well.
+      # In practice, we'd need to add a --include flag to isolate.py archive or
+      # something.
+      with open(isolated, 'rb') as f:
+        data = json.load(f)
+      data['includes'] = includes
+      with open(isolated, 'wb') as f:
+        json.dump(data, f, sort_keys=True, separators=(',', ':'))
+      cmd = [
+        os.path.join(CLIENT_DIR, 'isolateserver.py'), 'archive',
+        '-I', isolate, '--namespace', 'default-gzip', isolated,
+      ]
+      out = subprocess.check_output(cmd)
     return out.split(' ', 1)[0]
   finally:
     shutil.rmtree(tmp)
@@ -52,7 +74,7 @@ def capture(cmd, **kwargs):
 
 def test_normal(swarming, isolate, extra_flags):
   """Runs a normal task that succeeds."""
-  h = gen_isolate(isolate, 'print(\'SUCCESS\')')
+  h = gen_isolated(isolate, 'print(\'SUCCESS\')')
   subprocess.check_output(
       [SWARMING_SCRIPT, 'run', '-S', swarming, '-I', isolate, h] + extra_flags)
   return 'SUCCESS'
@@ -60,7 +82,7 @@ def test_normal(swarming, isolate, extra_flags):
 
 def test_expiration(swarming, isolate, extra_flags):
   """Schedule a task that cannot be scheduled and expire."""
-  h = gen_isolate(isolate, 'print(\'SUCCESS\')')
+  h = gen_isolated(isolate, 'print(\'SUCCESS\')')
   start = time.time()
   out, exitcode = capture(
       [
@@ -78,7 +100,7 @@ def test_expiration(swarming, isolate, extra_flags):
 
 def test_io_timeout(swarming, isolate, extra_flags):
   """Runs a task that triggers IO timeout."""
-  h = gen_isolate(
+  h = gen_isolated(
       isolate,
       'import time\n'
       'print(\'SUCCESS\')\n'
@@ -100,7 +122,7 @@ def test_io_timeout(swarming, isolate, extra_flags):
 
 def test_hard_timeout(swarming, isolate, extra_flags):
   """Runs a task that triggers hard timeout."""
-  h = gen_isolate(
+  h = gen_isolated(
       isolate,
       'import time\n'
       'for i in xrange(6):'
@@ -120,6 +142,42 @@ def test_hard_timeout(swarming, isolate, extra_flags):
   return 'SUCCESS'
 
 
+def test_reentrant(swarming, isolate, extra_flags):
+  """Runs a task that triggers a child task.
+
+  To be able to do so, it archives all of ../../client/.
+
+  Because the parent task blocks on the child task, it requires at least 2 bots
+  alive.
+  """
+  # First isolate the whole client directory.
+  cmd = [
+    os.path.join(CLIENT_DIR, 'isolateserver.py'), 'archive',
+    '-I', isolate, '--namespace', 'default-gzip', '--blacklist', 'tests',
+    CLIENT_DIR,
+  ]
+  client_isolated = subprocess.check_output(cmd).split()[0]
+  logging.info('- %s', client_isolated)
+
+  script = '\n'.join((
+      'import os',
+      'import subprocess',
+      'import sys',
+      'print("Before\\n")',
+      'print("SWARMING_TASK_ID=%s\\n" % os.environ["SWARMING_TASK_ID"])',
+      'subprocess.check_call(',
+      '  [sys.executable, "-u", "example/3_swarming_run_auto_upload.py",',
+      '    "-S", "%s",' % swarming,
+      '    "-I", "%s",' % isolate,
+      '    "--verbose",',
+      '  ])',
+      'print("After\\n")'))
+  h = gen_isolated(isolate, script, [client_isolated])
+  subprocess.check_output(
+      [SWARMING_SCRIPT, 'run', '-S', swarming, '-I', isolate, h] + extra_flags)
+  return 'SUCCESS'
+
+
 def get_all_tests():
   m = sys.modules[__name__]
   return {k[5:]: getattr(m, k) for k in dir(m) if k.startswith('test_')}
@@ -135,10 +193,14 @@ def run_test(results, swarming, isolate, extra_flags, name, test_case):
 
 
 def main():
+  # It's necessary for relative paths in .isolate.
+  os.chdir(APP_DIR)
+
   parser = optparse.OptionParser()
   parser.add_option('-S', '--swarming', help='Swarming server')
   parser.add_option('-I', '--isolate-server', help='Isolate server')
   parser.add_option('-d', '--dimensions', nargs=2, default=[], action='append')
+  parser.add_option('-v', '--verbose', action='store_true', help='Logs more')
   options, args = parser.parse_args()
 
   if args:
@@ -147,20 +209,20 @@ def main():
     parser.error('--swarming required')
   if not options.isolate_server:
     parser.error('--isolate-server required')
-
   if not os.path.isfile(SWARMING_SCRIPT):
     parser.error('Invalid checkout, %s does not exist' % SWARMING_SCRIPT)
 
+  logging.basicConfig(level=logging.DEBUG if options.verbose else logging.ERROR)
   extra_flags = ['--priority', '5', '--tags', 'smoke_test:1']
   for k, v in options.dimensions or [('os', 'Linux')]:
     extra_flags.extend(('-d', k, v))
 
   # Run all the tests in parallel.
   tests = get_all_tests()
-
   results = Queue.Queue(maxsize=len(tests))
 
   for name, fn in sorted(tests.iteritems()):
+    logging.info('%s', name)
     t = threading.Thread(
         target=run_test, name=name,
         args=(results, options.swarming, options.isolate_server, extra_flags,

appengine/swarming/server/task_request.py
@@ -75,16 +75,15 @@ _MIN_TIMEOUT_SECS = 1 if utils.is_local_dev_server() else 30
 _BEGINING_OF_THE_WORLD = datetime.datetime(2010, 1, 1, 0, 0, 0, 0)
 
 
-# Mask to TaskRequest key ids so they become decreasing numbers.
-_TASK_REQUEST_KEY_ID_MASK = int(2L**63-1)
-
-
 # Parameters for make_request().
 # The content of the 'data' parameter. This relates to the context of the
 # request, e.g. who wants to run a task.
-_DATA_KEYS = frozenset(
+_REQUIRED_DATA_KEYS = frozenset(
     ['name', 'priority', 'properties', 'scheduling_expiration_secs', 'tags',
      'user'])
+_EXPECTED_DATA_KEYS = frozenset(
+    ['name', 'parent_task_id', 'priority', 'properties',
+      'scheduling_expiration_secs', 'tags', 'user'])
 # The content of 'properties' inside the 'data' parameter. This relates to the
 # task itself, e.g. what to run.
 _REQUIRED_PROPERTIES_KEYS = frozenset(
@@ -134,29 +133,6 @@ def _validate_dict_of_strings(prop, value):
     raise TypeError('%s must be a dict of strings' % prop._name)
 
 
-def _validate_grace(prop, value):
-  """Validates grace_period_secs in TaskProperties."""
-  if not (0 <= value <= _ONE_DAY_SECS):
-    # pylint: disable=W0212
-    raise datastore_errors.BadValueError(
-        '%s (%ds) must be between %ds and one day' % (prop._name, value, 0))
-
-
-def _validate_timeout(prop, value):
-  """Validates timeouts in seconds in TaskProperties."""
-  if not (_MIN_TIMEOUT_SECS <= value <= _ONE_DAY_SECS):
-    # pylint: disable=W0212
-    raise datastore_errors.BadValueError(
-        '%s (%ds) must be between %ds and one day' %
-            (prop._name, value, _MIN_TIMEOUT_SECS))
-
-
-def _validate_priority(_prop, value):
-  """Validates TaskRequest.priority."""
-  validate_priority(value)
-  return value
-
-
 def _validate_expiration(prop, value):
   """Validates TaskRequest.expiration_ts."""
   now = utils.utcnow()
@@ -169,8 +145,39 @@ def _validate_expiration(prop, value):
         (prop._name, value, offset, _MIN_TIMEOUT_SECS, now))
 
 
+def _validate_grace(prop, value):
+  """Validates grace_period_secs in TaskProperties."""
+  if not (0 <= value <= _ONE_DAY_SECS):
+    # pylint: disable=W0212
+    raise datastore_errors.BadValueError(
+        '%s (%ds) must be between %ds and one day' % (prop._name, value, 0))
+
+
+def _validate_priority(_prop, value):
+  """Validates TaskRequest.priority."""
+  validate_priority(value)
+  return value
+
+
+def _validate_task_run_id(_prop, value):
+  """Validates a task_id looks valid without fetching the entity."""
+  if not value:
+    return None
+  task_pack.unpack_run_result_key(value)
+  return value
+
+
+def _validate_timeout(prop, value):
+  """Validates timeouts in seconds in TaskProperties."""
+  if not (_MIN_TIMEOUT_SECS <= value <= _ONE_DAY_SECS):
+    # pylint: disable=W0212
+    raise datastore_errors.BadValueError(
+        '%s (%ds) must be between %ds and one day' %
+            (prop._name, value, _MIN_TIMEOUT_SECS))
+
+
 def _validate_tags(prop, value):
-  """Validates and sort TaskRequest.tags."""
+  """Validates and sorts TaskRequest.tags."""
   if not ':' in value:
     # pylint: disable=W0212
     raise ValueError('%s must be key:value form, not %s' % (prop._name, value))
@@ -291,6 +298,10 @@ class TaskRequest(ndb.Model):
   # Tags that specify the category of the task.
   tags = ndb.StringProperty(repeated=True, validator=_validate_tags)
 
+  # Set when a task (the parent) reentrantly create swarming tasks. Must be set
+  # to a valid task_id pointing to a TaskRunResult or be None.
+  parent_task_id = ndb.StringProperty(validator=_validate_task_run_id)
+
   @property
   def scheduling_expiration_secs(self):
     """Reconstructs this value from expiration_ts and created_ts."""
@@ -336,8 +347,8 @@ def _new_request_key():
     way that affects the packing and unpacking of task ids, this value should be
     bumped.
 
-  The key id is this value XORed with _TASK_REQUEST_KEY_ID_MASK. The reason is
-  that increasing key id values are in decreasing timestamp order.
+  The key id is this value XORed with task_pack.TASK_REQUEST_KEY_ID_MASK. The
+  reason is that increasing key id values are in decreasing timestamp order.
   """
   utcnow = utils.utcnow()
   if utcnow < _BEGINING_OF_THE_WORLD:
@@ -348,7 +359,7 @@ def _new_request_key():
   # TODO(maruel): Use real randomness.
   suffix = random.getrandbits(16)
   task_id = int((now << 20) | (suffix << 4) | 0x1)
-  return ndb.Key(TaskRequest, task_id ^ _TASK_REQUEST_KEY_ID_MASK)
+  return ndb.Key(TaskRequest, task_id ^ task_pack.TASK_REQUEST_KEY_ID_MASK)
 
 
 def _put_request(request):
@@ -417,7 +428,7 @@ def make_request(data):
   Argument:
   - data: dict with:
     - name
-    - user
+    - parent_task_id*
     - properties
       - commands
       - data
@@ -430,18 +441,38 @@ def make_request(data):
     - priority
     - scheduling_expiration_secs
     - tags
+    - user
 
   * are optional.
+
+  If parent_task_id is set, properties for the parent are used:
+  - priority: defaults to parent.priority - 1
+  - user: overriden by parent.user
 
   Returns:
     The newly created TaskRequest.
   """
   # Save ourself headaches with typos and refuses unexpected values.
-  _assert_keys(_DATA_KEYS, _DATA_KEYS, data, 'request keys')
+  _assert_keys(_EXPECTED_DATA_KEYS, _REQUIRED_DATA_KEYS, data, 'request keys')
   data_properties = data['properties']
   _assert_keys(
       _EXPECTED_PROPERTIES_KEYS, _REQUIRED_PROPERTIES_KEYS, data_properties,
       'request properties keys')
+
+  parent_task_id = data.get('parent_task_id') or None
+  if parent_task_id:
+    data = data.copy()
+    run_result_key = task_pack.unpack_run_result_key(parent_task_id)
+    result_summary_key = task_pack.run_result_key_to_result_summary_key(
+        run_result_key)
+    request_key = task_pack.result_summary_key_to_request_key(
+        result_summary_key)
+    parent = request_key.get()
+    if not parent:
+      raise ValueError('parent_task_id is invalid')
+    data['priority'] = max(min(data['priority'], parent.priority - 1), 0)
+    # Drop the previous user.
+    data['user'] = parent.user
 
   # Class TaskProperties takes care of making everything deterministic.
   properties = TaskProperties(
@@ -457,15 +488,17 @@ def make_request(data):
   now = utils.utcnow()
   expiration_ts = now + datetime.timedelta(
       seconds=data['scheduling_expiration_secs'])
+
   request = TaskRequest(
-      created_ts=now,
       authenticated=auth.get_current_identity(),
-      name=data['name'],
-      user=data['user'] or '',
-      properties=properties,
-      priority=data['priority'],
+      created_ts=now,
       expiration_ts=expiration_ts,
-      tags=data['tags'])
+      name=data['name'],
+      parent_task_id=parent_task_id,
+      priority=data['priority'],
+      properties=properties,
+      tags=data['tags'],
+      user=data['user'] or '')
   _put_request(request)
   return request
 

appengine/swarming/server/task_request_test.py
@@ -71,7 +71,7 @@ class TaskRequestPrivateTest(TestCase):
       now = int(round(delta.total_seconds() * 1000.))
       key = task_request._new_request_key()
       # Remove the XOR.
-      key_id = key.integer_id() ^ task_request._TASK_REQUEST_KEY_ID_MASK
+      key_id = key.integer_id() ^ task_pack.TASK_REQUEST_KEY_ID_MASK
       timestamp = key_id >> 20
       randomness = (key_id >> 4) & 0xFFFF
       version = key_id & 0xF
@@ -90,7 +90,7 @@ class TaskRequestPrivateTest(TestCase):
     self.mock_now(task_request._BEGINING_OF_THE_WORLD)
     key = task_request._new_request_key()
     # Remove the XOR.
-    key_id = key.integer_id() ^ task_request._TASK_REQUEST_KEY_ID_MASK
+    key_id = key.integer_id() ^ task_pack.TASK_REQUEST_KEY_ID_MASK
     #   00000000000 7766 1
     #     ^          ^   ^
     #     |          |   |
@@ -116,7 +116,7 @@ class TaskRequestPrivateTest(TestCase):
     self.mock_now(now)
     key = task_request._new_request_key()
     # Remove the XOR.
-    key_id = key.integer_id() ^ task_request._TASK_REQUEST_KEY_ID_MASK
+    key_id = key.integer_id() ^ task_pack.TASK_REQUEST_KEY_ID_MASK
     #   7ffffffffff 7766 1
     #     ^          ^   ^
     #     |          |   |
@@ -124,6 +124,16 @@ class TaskRequestPrivateTest(TestCase):
     #                |
     #               rand
     self.assertEqual('0x7ffffffffff77661', '0x%016x' % key_id)
+
+  def test_validate_task_run_id(self):
+    class P(object):
+      _name = 'foo'
+    self.assertEqual(
+        '1d69b9f088008811',
+        task_request._validate_task_run_id(P(), '1d69b9f088008811'))
+    self.assertEqual(None, task_request._validate_task_run_id(P(), ''))
+    with self.assertRaises(ValueError):
+      task_request._validate_task_run_id(P(), '1')
 
 
 class TaskRequestApiTest(TestCase):
@@ -175,6 +185,7 @@ class TaskRequestApiTest(TestCase):
     expected_request = {
       'authenticated': auth_testing.DEFAULT_MOCKED_IDENTITY,
       'name': u'Request name',
+      'parent_task_id': None,
       'priority': 50,
       'properties': expected_properties,
       'properties_hash': None,
@@ -195,6 +206,20 @@ class TaskRequestApiTest(TestCase):
         int(round((expiration - created).total_seconds())), deadline_to_run)
     self.assertEqual(expected_request, actual)
     self.assertEqual(31., request.scheduling_expiration_secs)
+
+  def test_make_request_parent(self):
+    parent = task_request.make_request(_gen_request_data())
+    # Hack: Would need to know about TaskResultSummary.
+    parent_id = task_pack.pack_request_key(parent.key) + '1'
+    child = task_request.make_request(
+        _gen_request_data(parent_task_id=parent_id))
+    self.assertEqual(parent_id, child.parent_task_id)
+
+  def test_make_request_invalid_parent_id(self):
+    # Must ends with '1' or '2', not '0'
+    data = _gen_request_data(parent_task_id='1d69b9f088008810')
+    with self.assertRaises(ValueError):
+      task_request.make_request(data)
 
   def test_make_request_idempotent(self):
     request = task_request.make_request(

appengine/swarming/server/task_result.py
@@ -119,6 +119,14 @@ def _validate_not_pending(prop, value):
     raise datastore_errors.BadValueError('%s cannot be PENDING' % prop._name)
 
 
+def _validate_task_summary_id(_prop, value):
+  """Validates a task_id looks valid without fetching the entity."""
+  if not value:
+    return None
+  task_pack.unpack_result_summary_key(value)
+  return value
+
+
 def _calculate_failure(result_common):
   # When a command times out, there may not be any exit code, it is still a user
   # process failure mode, not an infrastructure failure mode.
@@ -268,6 +276,12 @@ class _TaskResultCommon(ndb.Model):
   completed_ts = ndb.DateTimeProperty()
   abandoned_ts = ndb.DateTimeProperty()
 
+  # Children tasks that were triggered by this task. This is set when the task
+  # reentrantly creates other Swarming tasks. Note that the task_id is to a
+  # TaskResultSummary.
+  children_task_ids = ndb.StringProperty(
+      validator=_validate_task_summary_id, repeated=True)
+
   @property
   def can_be_canceled(self):
     """Returns True if the task is in a state that can be canceled."""
@@ -411,6 +425,9 @@ class _TaskResultCommon(ndb.Model):
 
     if self.state == State.TIMED_OUT and not self.failure:
       raise datastore_errors.BadValueError('Timeout implies task failure')
+
+    self.children_task_ids = sorted(
+        set(self.children_task_ids), key=lambda x: int(x, 16))
 
 
 class TaskRunResult(_TaskResultCommon):

commit 1e2f285b72a584e91acf72a503f7858520429d23

Fix bug introduced in 610048179c64.
It resulted in a HTTP 500 in task that had a parent.

R=vadimsh@chromium.org
BUG=

Review URL: https://codereview.appspot.com/196650043

appengine/swarming/handlers_frontend.py
@@ -552,10 +552,10 @@ class TaskHandler(auth.AuthenticatingHandler):
     request = request_future.get_result()
     parent_task_future = None
     if request.parent_task_id:
-      parent_key = task_result.unpack_run_result_key(request.parent_task_id)
+      parent_key = task_pack.unpack_run_result_key(request.parent_task_id)
       parent_task_future = parent_key.get_async()
     children_tasks_futures = [
-      task_result.unpack_result_summary_key(c).get_async()
+      task_pack.unpack_result_summary_key(c).get_async()
       for c in result.children_task_ids
     ]
 