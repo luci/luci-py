9082f953988e79ebf15f843db7c702327f6ff4c9
Vadim Shtayura <vadimsh@chromium.org>
2015-04-29 14:33:37 -0700

    Metrics reusable component to simplify talking to Cloud Monitoring API.
    It implements metric autoregistration (and update), batching and asynchronous
    sending.
    
    R=nodir@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1117623004

---
+++ appengine/components/components/metrics/__init__.py
@@ -0,0 +1,11 @@
+# Copyright 2015 The Swarming Authors. All rights reserved.
+# Use of this source code is governed by the Apache v2.0 license that can be
+# found in the LICENSE file.
+
+"""Wrapper around Google Cloud Monitoring API.
+
+See metrics.py for usage example.
+"""
+
+# Wildcard import - pylint: disable=W0401
+from .metrics import *
