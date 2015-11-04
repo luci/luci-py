7534e9b2af5e30b2cce4e5e710600ebeb4f61e9a
maruel <maruel@chromium.org>
2015-11-04 12:53:29 -0800

    Add kill_adb() and increase python-adb logging to WARNING.
    It gives a master switch that can easily be temporarily increased to INFO or
    even DEBUG when needed by simply pushing a new tainted swarming server version.
    This helps quickly debugging issues. On the other hand, even INFO level is quite
    verbose so keep it at WARNING by default.
    
    Adding kill_adb() is useful, since it's better to forcibly kill adb upfront in
    on_after_task() than try to kill it after trying USB enumeration then killing
    adb then enumerating again.
    
    R=stip@chromium.org
    BUG=chromium:525873
    
    Review URL: https://codereview.chromium.org/1430073003

--- appengine/swarming/swarming_bot/api/platforms/android.py
+++ appengine/swarming/swarming_bot/api/platforms/android.py
@@ -14,7 +14,20 @@ import os
 
 
 from adb import adb_commands_safe
+from adb import adb_protocol
+from adb import common
 from adb import high
+
+
+# Master switch that can easily be temporarily increased to INFO or even DEBUG
+# when needed by simply pushing a new tainted swarming server version. This
+# helps quickly debugging issues. On the other hand, even INFO level is quite
+# verbose so keep it at WARNING by default.
+LEVEL = logging.WARNING
+adb_commands_safe._LOG.setLevel(LEVEL)
+adb_protocol._LOG.setLevel(LEVEL)
+common._LOG.setLevel(LEVEL)
+high._LOG.setLevel(LEVEL)
 
 
 def initialize(pub_key, priv_key):
@@ -29,3 +42,7 @@ def get_devices(bot):
 
 def close_devices(devices):
   return high.CloseDevices(devices)
+
+
+def kill_adb():
+  return adb_commands_safe.KillADB()
