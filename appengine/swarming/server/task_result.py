luci/luci-py

commit cbec56be8c05b8bd3d7ff2f081797e3ec3f041bf

Update _TaskResultCommon to flatten exit_codes and durations.
- Rename durations to duration, other members will be added after to include
  duration as measured by task_runner and isolated operations overhead.
- Abuse the fact that a ndb.IntegerProperty(repeated=True) can safely be loaded
  as a ndb.IntegerProperty() when there's only one item.
- Rename duration_total to duration_as_seen_by_server to disambiguate with
  duration.
- Enforce more internal consistency on task results when storing in the DB. Had
  to update a few test cases. This simplifies bot_update_task() a bit.

This CL is part paying technical debt, part renamed 'durations' to 'duration'
so more durations (pun intended) can be added without looking out of place, as
'durations' was a list.

There is no externally visible functionality change.

R=vadimsh@chromium.org
BUG=

Review URL: https://codereview.chromium.org/1848943002

appengine/swarming/server/task_result.py
@@ -244,8 +244,6 @@ class _TaskResultCommon(ndb.Model):
   It is not meant to be instantiated on its own.
 
   TODO(maruel): Overhaul this entity:
-  - Convert exit_codes to exit_code, a single value.
-  - Convert durations to duration, a single value.
   - Get rid of TaskOutput as it is not needed anymore (?)
   """
   # Bot that ran this task.
@@ -278,11 +276,12 @@ class _TaskResultCommon(ndb.Model):
   # Number of TaskOutputChunk entities for the output.
   stdout_chunks = ndb.IntegerProperty(indexed=False)
 
-  # Aggregated exit codes. Ordered by command.
-  exit_codes = ndb.IntegerProperty(repeated=True, indexed=False)
+  # Process exit code.
+  exit_code = ndb.IntegerProperty(indexed=False, name='exit_codes')
 
-  # Aggregated durations in seconds. Ordered by command.
-  durations = ndb.FloatProperty(repeated=True, indexed=False)
+  # Task duration in seconds as seen by the task_runner, including isolation
+  # overhead.
+  duration = ndb.FloatProperty(indexed=False, name='durations')
 
   # Time when a bot reaped this task.
   started_ts = ndb.DateTimeProperty()
@@ -320,8 +319,11 @@ class _TaskResultCommon(ndb.Model):
     return self.state == State.PENDING
 
   @property
-  def duration_total(self):
-    """Returns the timedelta the task spent executing, including overhead.
+  def duration_as_seen_by_server(self):
+    """Returns the timedelta the task spent executing, including server<->bot
+    communication overhead.
+
+    This is the task duration as seen by the server, not by the bot.
 
     Task abandoned or not yet completed are not applicable and return None.
     """
@@ -330,10 +332,9 @@ class _TaskResultCommon(ndb.Model):
     return self.completed_ts - self.started_ts
 
   def duration_now(self, now):
-    """Returns the timedelta the task spent executing as of now, including
-    overhead.
+    """Returns the timedelta the task spent executing as of now.
 
-    Similar to .duration_total except that its return value is not
+    Similar to .duration_as_seen_by_server except that its return value is not
     deterministic. Task abandoned is not applicable and return None.
     """
     if not self.started_ts or self.abandoned_ts:
@@ -343,10 +344,6 @@ class _TaskResultCommon(ndb.Model):
   @property
   def ended_ts(self):
     return self.completed_ts or self.abandoned_ts
-
-  @property
-  def exit_code(self):
-    return self.exit_codes[0] if self.exit_codes else None
 
   @property
   def is_exceptional(self):
@@ -434,10 +431,6 @@ class _TaskResultCommon(ndb.Model):
   def _pre_put_hook(self):
     """Use extra validation that cannot be validated throught 'validator'."""
     super(_TaskResultCommon, self)._pre_put_hook()
-    # TODO(vadimsh): Map reduce jobs use non-transactional put for old entities
-    # for performance reasons.
-    # assert ndb.in_transaction(), (
-    #     'Saving %s outside of transaction' % self.__class__.__name__)
     if self.state == State.EXPIRED:
       if self.failure or self.exit_code is not None:
         raise datastore_errors.BadValueError(
@@ -449,8 +442,32 @@ class _TaskResultCommon(ndb.Model):
     if not self.modified_ts:
       raise datastore_errors.BadValueError('Must update .modified_ts')
 
+    if (self.duration is None) != (self.exit_code is None):
+        raise datastore_errors.BadValueError(
+            'duration and exit_code must both be None or not None')
+    if self.state in State.STATES_DONE:
+      if self.duration is None:
+        raise datastore_errors.BadValueError(
+            'duration and exit_code must be set with state %s' %
+            State.to_string(self.state))
+    else:
+      if self.duration is not None:
+        raise datastore_errors.BadValueError(
+            'duration and exit_code must not be set with state %s' %
+            State.to_string(self.state))
+
     self.children_task_ids = sorted(
         set(self.children_task_ids), key=lambda x: int(x, 16))
+
+  @classmethod
+  def _properties_fixed(cls):
+    """Returns all properties with their member name, excluding computed
+    properties.
+    """
+    return [
+      prop._code_name for prop in cls._properties.itervalues()
+      if not isinstance(prop, ndb.ComputedProperty)
+    ]
 
 
 class TaskRunResult(_TaskResultCommon):
@@ -618,8 +635,8 @@ class TaskResultSummary(_TaskResultCommon):
 
   def reset_to_pending(self):
     """Resets this entity to pending state."""
-    self.durations = []
-    self.exit_codes = []
+    self.duration = None
+    self.exit_code = None
     self.internal_failure = False
     self.state = State.PENDING
     self.started_ts = None
@@ -632,10 +649,7 @@ class TaskResultSummary(_TaskResultCommon):
     set.
     """
     assert isinstance(run_result, TaskRunResult), run_result
-    for property_name in _TaskResultCommon._properties:
-      if isinstance(
-          getattr(_TaskResultCommon, property_name), ndb.ComputedProperty):
-        continue
+    for property_name in _TaskResultCommon._properties_fixed():
       setattr(self, property_name, getattr(run_result, property_name))
     # Include explicit support for 'state' and 'try_number'. TaskRunResult.state
     # is a ComputedProperty so it can't be copied as-is, and try_number is a
@@ -669,10 +683,7 @@ class TaskResultSummary(_TaskResultCommon):
     if self.try_number and self.try_number > run_result.try_number:
       return False
 
-    for property_name in _TaskResultCommon._properties:
-      if isinstance(
-          getattr(_TaskResultCommon, property_name), ndb.ComputedProperty):
-        continue
+    for property_name in _TaskResultCommon._properties_fixed():
       if getattr(self, property_name) != getattr(run_result, property_name):
         return True
     # Include explicit support for 'state' and 'try_number'. TaskRunResult.state

appengine/swarming/handlers_frontend.py
@@ -311,7 +311,7 @@ class BotHandler(auth.AuthenticatingHandler):
               run_results[index-1].started_ts - run_results[index].ended_ts)
           # We are taking the whole time the bot was doing work, not just the
           # duration associated with the task.
-          duration = run_results[index].duration_total
+          duration = run_results[index].duration_as_seen_by_server
           if duration:
             run_time += duration
 
@@ -515,10 +515,11 @@ class TasksHandler(auth.AuthenticatingHandler):
         t.cost_saved_usd for t in tasks if t.cost_saved_usd)
     # Include the overhead in the total amount of time saved, since it's
     # overhead saved.
-    # In theory, t.duration_total should always be set when t.deduped_from is
-    # set but there has some broken entities in the datastore.
+    # In theory, t.duration_as_seen_by_server should always be set when
+    # t.deduped_from is set but there has some broken entities in the datastore.
     total_saved = safe_sum(
-        t.duration_total for t in tasks if t.deduped_from and t.duration_total)
+        t.duration_as_seen_by_server for t in tasks
+        if t.deduped_from and t.duration_as_seen_by_server)
     duration_sum = safe_sum(durations)
     total_saved_percent = (
         (100. * total_saved.total_seconds() / duration_sum.total_seconds())

appengine/swarming/message_conversion.py
@@ -158,8 +158,6 @@ def task_result_to_rpc(entity):
       if entity.outputs_ref else None)
   kwargs = {
     'bot_dimensions': _string_list_pairs_from_dict(entity.bot_dimensions or {}),
-    'duration': (entity.durations or [None])[0],
-    'exit_code': entity.exit_code,
     'outputs_ref': outputs_ref,
     'state': swarming_rpcs.StateField(entity.state),
   }

appengine/swarming/server/task_result_test.py
@@ -146,8 +146,8 @@ class TaskResultApiTest(TestCase):
       'cost_saved_usd': None,
       'created_ts': self.now,
       'deduped_from': None,
-      'durations': [],
-      'exit_codes': [],
+      'duration': None,
+      'exit_code': None,
       'failure': False,
       'id': '1d69b9f088008810',
       'internal_failure': False,
@@ -194,8 +194,8 @@ class TaskResultApiTest(TestCase):
       'children_task_ids': [],
       'completed_ts': None,
       'cost_usd': 0.,
-      'durations': [],
-      'exit_codes': [],
+      'duration': None,
+      'exit_code': None,
       'failure': False,
       'id': '1d69b9f088008811',
       'internal_failure': False,
@@ -230,8 +230,8 @@ class TaskResultApiTest(TestCase):
       'cost_saved_usd': None,
       'created_ts': self.now,
       'deduped_from': None,
-      'durations': [],
-      'exit_codes': [],
+      'duration': None,
+      'exit_code': None,
       'failure': False,
       'id': '1d69b9f088008810',
       'internal_failure': False,
@@ -277,8 +277,8 @@ class TaskResultApiTest(TestCase):
       'cost_saved_usd': None,
       'created_ts': self.now,
       'deduped_from': None,
-      'durations': [],
-      'exit_codes': [],
+      'duration': None,
+      'exit_code': None,
       'failure': False,
       'id': '1d69b9f088008810',
       'internal_failure': False,
@@ -305,7 +305,8 @@ class TaskResultApiTest(TestCase):
     complete_ts = self.now + datetime.timedelta(seconds=6)
     self.mock_now(complete_ts)
     run_result.completed_ts = complete_ts
-    run_result.exit_codes.append(0)
+    run_result.duration = 0.1
+    run_result.exit_code = 0
     run_result.state = task_result.State.COMPLETED
     run_result.modified_ts = utils.utcnow()
     ndb.transaction(lambda: ndb.put_multi(run_result.append_output('foo', 0)))
@@ -322,8 +323,8 @@ class TaskResultApiTest(TestCase):
       'cost_saved_usd': None,
       'created_ts': self.now,
       'deduped_from': None,
-      'durations': [],
-      'exit_codes': [0],
+      'duration': 0.1,
+      'exit_code': 0,
       'failure': False,
       'id': '1d69b9f088008810',
       'internal_failure': False,
@@ -346,7 +347,8 @@ class TaskResultApiTest(TestCase):
     self.assertEqual(expected, result_summary.key.get().to_dict())
     self.assertEqual('foo', result_summary.get_output())
     self.assertEqual(
-        datetime.timedelta(seconds=2), result_summary.duration_total)
+        datetime.timedelta(seconds=2),
+        result_summary.duration_as_seen_by_server)
     self.assertEqual(
         datetime.timedelta(seconds=2),
         result_summary.duration_now(utils.utcnow()))
@@ -456,7 +458,7 @@ class TaskResultApiTest(TestCase):
         started_ts=datetime.datetime(2010, 1, 1, 0, 0, 0),
         completed_ts=datetime.datetime(2010, 1, 1, 0, 2, 0))
     self.assertEqual(
-        datetime.timedelta(seconds=120), run_result.duration_total)
+        datetime.timedelta(seconds=120), run_result.duration_as_seen_by_server)
     self.assertEqual(
         datetime.timedelta(seconds=120),
         run_result.duration_now(utils.utcnow()))
@@ -464,7 +466,7 @@ class TaskResultApiTest(TestCase):
     run_result = task_result.TaskRunResult(
         started_ts=datetime.datetime(2010, 1, 1, 0, 0, 0),
         abandoned_ts=datetime.datetime(2010, 1, 1, 0, 1, 0))
-    self.assertEqual(None, run_result.duration_total)
+    self.assertEqual(None, run_result.duration_as_seen_by_server)
     self.assertEqual(None, run_result.duration_now(utils.utcnow()))
 
   def test_run_result_timeout(self):
@@ -474,6 +476,8 @@ class TaskResultApiTest(TestCase):
     ndb.transaction(result_summary.put)
     run_result = task_result.new_run_result(request, 1, 'localhost', 'abc', {})
     run_result.state = task_result.State.TIMED_OUT
+    run_result.duration = 0.1
+    run_result.exit_code = -1
     run_result.completed_ts = utils.utcnow()
     run_result.modified_ts = utils.utcnow()
     result_summary.set_from_run_result(run_result, request)

appengine/swarming/server/task_scheduler.py
@@ -145,8 +145,9 @@ def _update_stats(run_result, bot_id, request, completed):
   """Updates stats after a bot task update notification."""
   if completed:
     runtime_ms = 0
-    if run_result.duration_total:
-      runtime_ms = _secs_to_ms(run_result.duration_total.total_seconds())
+    if run_result.duration_as_seen_by_server:
+      runtime_ms = _secs_to_ms(
+          run_result.duration_as_seen_by_server.total_seconds())
     pending_ms = 0
     if run_result.started_ts:
       pending_ms = _secs_to_ms(
@@ -263,7 +264,7 @@ def _handle_dead_bot(run_result_key):
   return task_is_retried
 
 
-def _copy_entity(src, dst, skip_list):
+def _copy_summary(src, dst, skip_list):
   """Copies the attributes of entity src into dst.
 
   It doesn't copy the key nor any member in skip_list.
@@ -271,8 +272,7 @@ def _copy_entity(src, dst, skip_list):
   assert type(src) == type(dst), '%s!=%s' % (src.__class__, dst.__class__)
   # Access to a protected member _XX of a client class - pylint: disable=W0212
   kwargs = {
-    k: getattr(src, k) for k, v in src.__class__._properties.iteritems()
-    if not isinstance(v, ndb.ComputedProperty) and k not in skip_list
+    k: getattr(src, k) for k in src._properties_fixed() if k not in skip_list
   }
   dst.populate(**kwargs)
 
@@ -439,7 +439,7 @@ def schedule_request(request):
       # functionality.
       # Setting task.queue_number to None removes it from the scheduling.
       task.queue_number = None
-      _copy_entity(
+      _copy_summary(
           dupe_summary, result_summary, ('created_ts', 'name', 'user', 'tags'))
       result_summary.properties_hash = None
       result_summary.try_number = 0
@@ -567,8 +567,8 @@ def bot_update_task(
   - bot_id: Self advertised bot id to ensure it's the one expected.
   - output: Data to append to this command output.
   - output_chunk_start: Index of output in the stdout stream.
-  - exit_code: Mark that this command is terminated.
-  - duration: Time spent in seconds for this command.
+  - exit_code: Mark that this task completed.
+  - duration: Time spent in seconds for this task.
   - hard_timeout: Bool set if an hard timeout occured.
   - io_timeout: Bool set if an I/O timeout occured.
   - cost_usd: Cost in $USD of this task up to now.
@@ -585,6 +585,10 @@ def bot_update_task(
   assert output is None or isinstance(output, str)
   if cost_usd is not None and cost_usd < 0.:
     raise ValueError('cost_usd must be None or greater or equal than 0')
+  if (duration is None) != (exit_code is None):
+    raise ValueError(
+        'had unexpected duration; expected iff a command completes\n'
+        'duration: %r; exit: %r' % (duration, exit_code))
 
   packed = task_pack.pack_run_result_key(run_result_key)
   logging.debug(
@@ -619,37 +623,29 @@ def bot_update_task(
       return None, None, False, 'TaskRunResult is broken; %s' % (
           run_result.to_dict())
 
-    # This happens as an HTTP request is retried when the DB write succeeded but
-    # it still returned HTTP 500.
-    if run_result.exit_code is not None and exit_code is not None:
-      if run_result.exit_code != exit_code:
-        result_summary_future.wait()
-        return None, None, False, 'got 2 different exit_code; %s then %s' % (
-            run_result.exit_code, exit_code)
-
-    if run_result.durations and duration is not None:
-      if run_result.durations[0] != duration:
-        result_summary_future.wait()
-        return None, None, False, 'got 2 different durations; %s then %s' % (
-            run_result.durations[0], duration)
-
-    if (duration is None) != (exit_code is None):
-      result_summary_future.wait()
-      return None, None, False, (
-          'had unexpected duration; expected iff a command completes\n'
-          'duration: %s vs %s; exit: %s vs %s' % (
-            run_result.durations, duration, run_result.exit_code, exit_code))
-
-    # If the command completed. Check if the value wasn't set already.
-    if duration is not None and not run_result.durations:
-      run_result.durations.append(duration)
-    if exit_code is not None and run_result.exit_code is None:
-      run_result.exit_codes.append(exit_code)
+    # Assumptions:
+    # - duration and exit_code are both set or not set.
+    # - same for run_result.
+    if exit_code is not None:
+      if run_result.exit_code is not None:
+        # This happens as an HTTP request is retried when the DB write succeeded
+        # but it still returned HTTP 500.
+        if run_result.exit_code != exit_code:
+          result_summary_future.wait()
+          return None, None, False, 'got 2 different exit_code; %s then %s' % (
+              run_result.exit_code, exit_code)
+        if run_result.duration != duration:
+          result_summary_future.wait()
+          return None, None, False, 'got 2 different durations; %s then %s' % (
+              run_result.duration, duration)
+      else:
+        run_result.duration = duration
+        run_result.exit_code = exit_code
+    task_completed = run_result.exit_code is not None
 
     if outputs_ref:
       run_result.outputs_ref = task_request.FilesRef(**outputs_ref)
 
-    task_completed = run_result.exit_code is not None
     if run_result.state in task_result.State.STATES_RUNNING:
       if hard_timeout or io_timeout:
         run_result.state = task_result.State.TIMED_OUT

appengine/swarming/server/task_scheduler_test.py
@@ -307,8 +307,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'cost_saved_usd': 0.1,
       'created_ts': new_ts,
       'deduped_from': deduped_from,
-      'durations': [0.1],
-      'exit_codes': [0],
+      'duration': 0.1,
+      'exit_code': 0,
       'failure': False,
       'id': task_id,
       'internal_failure': False,
@@ -510,8 +510,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'cost_saved_usd': None,
       'created_ts': created_ts,
       'deduped_from': None,
-      'durations': [],
-      'exit_codes': [],
+      'duration': None,
+      'exit_code': None,
       'failure': False,
       'id': '1d69b9f088008810',
       'internal_failure': False,
@@ -555,8 +555,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'cost_saved_usd': None,
       'created_ts': created_ts,  # Time the TaskRequest was created.
       'deduped_from': None,
-      'durations': [],
-      'exit_codes': [],
+      'duration': None,
+      'exit_code': None,
       'failure': False,
       'id': '1d69b9f088008810',
       'internal_failure': False,
@@ -587,8 +587,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
         'children_task_ids': [],
         'completed_ts': None,
         'cost_usd': 0.,
-        'durations': [],
-        'exit_codes': [],
+        'duration': None,
+        'exit_code': None,
         'failure': False,
         'id': '1d69b9f088008811',
         'internal_failure': False,
@@ -627,8 +627,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'cost_saved_usd': None,
       'created_ts': created_ts,
       'deduped_from': None,
-      'durations': [0.1],
-      'exit_codes': [0],
+      'duration': 0.1,
+      'exit_code': 0,
       'failure': False,
       'id': '1d69b9f088008810',
       'internal_failure': False,
@@ -659,8 +659,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
         'children_task_ids': [],
         'completed_ts': done_ts,
         'cost_usd': 0.1,
-        'durations': [0.1],
-        'exit_codes': [0],
+        'duration': 0.1,
+        'exit_code': 0,
         'failure': False,
         'id': '1d69b9f088008811',
         'internal_failure': False,
@@ -704,8 +704,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'cost_saved_usd': None,
       'created_ts': self.now,
       'deduped_from': None,
-      'durations': [0.1],
-      'exit_codes': [1],
+      'duration': 0.1,
+      'exit_code': 1,
       'failure': True,
       'id': '1d69b9f088008810',
       'internal_failure': False,
@@ -716,7 +716,6 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'server_versions': [u'v1a'],
       'started_ts': self.now,
       'state': State.COMPLETED,
-      'try_number': 1,
       'tags': [
         u'OS:Windows-3.1.1',
         u'pool:default',
@@ -724,6 +723,7 @@ class TaskSchedulerApiTest(test_case.TestCase):
         u'tag:1',
         u'user:Jesus',
       ],
+      'try_number': 1,
       'user': u'Jesus',
     }
     self.assertEqual(expected, result_summary.to_dict())
@@ -737,8 +737,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
         'children_task_ids': [],
         'completed_ts': self.now,
         'cost_usd': 0.1,
-        'durations': [0.1],
-        'exit_codes': [1],
+        'duration': 0.1,
+        'exit_code': 1,
         'failure': True,
         'id': '1d69b9f088008811',
         'internal_failure': False,
@@ -862,8 +862,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'cost_saved_usd': None,
       'created_ts': self.now,
       'deduped_from': None,
-      'durations': [0.1],
-      'exit_codes': [0],
+      'duration': 0.1,
+      'exit_code': 0,
       'failure': True,
       'id': '1d69b9f088008810',
       'internal_failure': False,
@@ -894,8 +894,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'children_task_ids': [],
       'completed_ts': self.now,
       'cost_usd': 0.1,
-      'durations': [0.1],
-      'exit_codes': [0],
+      'duration': 0.1,
+      'exit_code': 0,
       'failure': True,
       'id': '1d69b9f088008811',
       'internal_failure': False,
@@ -940,8 +940,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'cost_saved_usd': None,
       'created_ts': self.now,
       'deduped_from': None,
-      'durations': [],
-      'exit_codes': [],
+      'duration': None,
+      'exit_code': None,
       'failure': False,
       'id': '1d69b9f088008810',
       'internal_failure': True,
@@ -971,8 +971,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'children_task_ids': [],
       'completed_ts': None,
       'cost_usd': 0.,
-      'durations': [],
-      'exit_codes': [],
+      'duration': None,
+      'exit_code': None,
       'failure': False,
       'id': '1d69b9f088008811',
       'internal_failure': True,
@@ -1063,8 +1063,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'cost_saved_usd': None,
       'created_ts': self.now,
       'deduped_from': None,
-      'durations': [],
-      'exit_codes': [],
+      'duration': None,
+      'exit_code': None,
       'failure': False,
       'id': '1d69b9f088008810',
       'internal_failure': False,
@@ -1136,8 +1136,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'cost_saved_usd': None,
       'created_ts': self.now,
       'deduped_from': None,
-      'durations': [],
-      'exit_codes': [],
+      'duration': None,
+      'exit_code': None,
       'failure': False,
       'id': '1d69b9f088008810',
       'internal_failure': True,
@@ -1200,8 +1200,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'children_task_ids': [],
       'completed_ts': None,
       'cost_usd': 0.,
-      'durations': [],
-      'exit_codes': [],
+      'duration': None,
+      'exit_code': None,
       'failure': False,
       'id': '1d69b9f088008811',
       'internal_failure': True,
@@ -1224,8 +1224,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'cost_saved_usd': None,
       'created_ts': self.now,
       'deduped_from': None,
-      'durations': [],
-      'exit_codes': [],
+      'duration': None,
+      'exit_code': None,
       'failure': False,
       'id': '1d69b9f088008810',
       'internal_failure': False,
@@ -1273,8 +1273,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'cost_saved_usd': None,
       'created_ts': self.now,
       'deduped_from': None,
-      'durations': [0.1],
-      'exit_codes': [0],
+      'duration': 0.1,
+      'exit_code': 0,
       'failure': False,
       'id': '1d69b9f088008810',
       'internal_failure': False,
@@ -1335,8 +1335,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'children_task_ids': [],
       'completed_ts': None,
       'cost_usd': 0.,
-      'durations': [],
-      'exit_codes': [],
+      'duration': None,
+      'exit_code': None,
       'failure': False,
       'id': '1d69b9f088008811',
       'internal_failure': True,
@@ -1359,8 +1359,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'cost_saved_usd': None,
       'created_ts': self.now,
       'deduped_from': None,
-      'durations': [],
-      'exit_codes': [],
+      'duration': None,
+      'exit_code': None,
       'failure': False,
       'id': '1d69b9f088008810',
       'internal_failure': False,
@@ -1435,8 +1435,8 @@ class TaskSchedulerApiTest(test_case.TestCase):
       'cost_saved_usd': None,
       'created_ts': self.now,
       'deduped_from': None,
-      'durations': [],
-      'exit_codes': [],
+      'duration': None,
+      'exit_code': None,
       'failure': False,
       'id': '1d69b9f088008810',
       'internal_failure': True,

commit 46f0ac7d0b0db67be49b506d5bd1a5cabf7de1ad

It is fine to set duration and exit_code in BOT_DIED.
Regression from cbec56be8c05b8bd3d7ff2f081797e3ec3f041bf.

This is extremely rare but this can currently happen when the bot XSRF token
expires while the task is running, it currently expires after 4 hours.

R=vadimsh@chromium.org
BUG=

Review URL: https://codereview.chromium.org/1877933002

appengine/swarming/server/task_result.py
@@ -448,7 +448,8 @@ class _TaskResultCommon(ndb.Model):
         raise datastore_errors.BadValueError(
             'duration and exit_code must be set with state %s' %
             State.to_string(self.state))
-    else:
+    elif self.state != State.BOT_DIED:
+      # With BOT_DIED, it can be either ways.
       if self.duration is not None:
         raise datastore_errors.BadValueError(
             'duration and exit_code must not be set with state %s' %