bf775c6fcaaa324474208dfcaaac75b2a226789f
kjlubick <kjlubick@google.com>
2016-08-09 11:48:20 -0700

    Update new botlist to use dimensions endpoint
    - Use new dimensions endpoint to get full fleet dimensions.  Before, you would only be able to filter based on the things already in the list, which is a problem if the thing you want doesn't show up in the first [BOT_LIST_LIMIT] commits.
    - Dimensions are passed from the client-side filter to the server-side filter.  The items we have will be filtered out while waiting for the server to give us more. The spinner will be active while any of the ajax requests are active.
    - The "devices" dimension was split (server side) into device_type, device_os, and android_devices.  The last one is the number of connected, ready devices.
    - Added new method _attribute, which will look for an attribute in the "dimensions" and fallback to "state".  This allows a more consistent "API" than hodge-podge _devices/_state all over the place.
    - Quarantined bot message actually show up (pulled from state).
    - Dead bot message augmented with human friendly "last seen" message.
    - Disk space column shown up, which is pulled exclusively from state, not a dimension.  It is displayed in a human friendly manner.
    - Remove "auto download" of demo json from Makefile
    - Sinon was updated to bring in a bug fix.
    
    BUG=631047
    
    Review-Url: https://codereview.chromium.org/2211163003

--- appengine/swarming/elements/res/js/common.js
+++ appengine/swarming/elements/res/js/common.js
@@ -2,7 +2,8 @@
 // Use of this source code is governed under the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
-window.swarming = window.swarming || function() {
+this.swarming = this.swarming || function() {
+
   var swarming = {};
 
   swarming.stableSort = function(arr, comp) {
@@ -43,10 +44,71 @@ window.swarming = window.swarming || function() {
     // Try numeric, aka "natural" sort and use it if ns is not NaN.
     // Javascript will try to corece these to numbers or return NaN.
     var ns = a - b;
-    if (ns) {
+    if (!isNaN(ns)) {
       return ns;
     }
     return a.localeCompare(b);
+  };
+
+
+  var TIME_DELTAS = [
+    { units: "w", delta: 7*24*60*60 },
+    { units: "d", delta:   24*60*60 },
+    { units: "h", delta:      60*60 },
+    { units: "m", delta:         60 },
+    { units: "s", delta:          1 },
+  ];
+
+  /**
+   * Returns the difference between the specified time and 's' as a string in a
+   * human friendly format.
+   * If left unspecified, "now" defaults to Date.now()
+   * If 's' is a number it is assumed to contain the time in milliseconds
+   * otherwise it is assumed to contain a time string.
+   *
+   * For example, a difference of 123 seconds between 's' and the current time
+   * would return "2m".
+   */
+  swarming.diffDate = function(s, now) {
+    var ms = (typeof(s) == "number") ? s : Date.parse(s);
+    now = now || Date.now();
+    var diff = (ms - now)/1000;
+    if (diff < 0) {
+      diff = -1.0 * diff;
+    }
+    return humanize(diff, TIME_DELTAS);
+  };
+
+  swarming.KB = 1024;
+  swarming.MB = swarming.KB * 1024;
+  swarming.GB = swarming.MB * 1024
+
+  var BYTES_DELTAS = [
+    { units: " TB", delta: 1024*swarming.GB},
+    { units: " GB", delta:      swarming.GB},
+    { units: " MB", delta:      swarming.MB},
+    { units: " KB", delta:      swarming.KB},
+    { units: " B",  delta:                1},
+  ];
+
+  swarming.humanBytes = function(b, unit) {
+    if (Number.isInteger(unit)) {
+      b = b * unit;
+    }
+    return humanize(b, BYTES_DELTAS);
+  }
+
+  function humanize(n, deltas) {
+    for (var i=0; i<deltas.length-1; i++) {
+      // If n would round to '60s', return '1m' instead.
+      var nextDeltaRounded =
+          Math.round(n/deltas[i+1].delta)*deltas[i+1].delta;
+      if (nextDeltaRounded/deltas[i].delta >= 1) {
+        return Math.round(n/deltas[i].delta)+deltas[i].units;
+      }
+    }
+    var i = DELTAS.length-1;
+    return Math.round(n/DELTAS[i].delta)+DELTAS[i].units;
   }
 
   return swarming;
