85983d4936b082a6371bc3f95b691f08e20b73b7
ryanmartens <ryanmartens@google.com>
2016-09-07 12:28:52 -0700

    Refactors authentictors into separate file.
    R=vadimsh@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2320553003

--- appengine/swarming/server/bot_archive.py
+++ appengine/swarming/server/bot_archive.py
@@ -175,6 +175,7 @@ FILES = (
     'third_party/rsa/rsa/varblock.py',
     'third_party/six/__init__.py',
     'utils/__init__.py',
+    'utils/authenticators.py',
     'utils/cacert.pem',
     'utils/file_path.py',
     'utils/fs.py',
