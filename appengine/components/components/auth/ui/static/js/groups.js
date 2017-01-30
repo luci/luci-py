c0ba470ac1b6b24286d8d416b52cedff8d391048
vadimsh <vadimsh@chromium.org>
2017-01-30 15:56:20 -0800

    Allow '@' in group names in UI too.
    R=nodir@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2667833002

--- appengine/components/components/auth/ui/static/js/groups.js
+++ appengine/components/components/auth/ui/static/js/groups.js
@@ -652,7 +652,7 @@ var waitForResult = function(defer, groupChooser, form) {
 // Sets up jQuery.validate validators for group form fields.
 var registerFormValidators = function() {
   // Regular expressions for form fields.
-  var groupRe = /^[0-9a-zA-Z_\-\.\/]{3,80}$/;
+  var groupRe = /^[0-9a-zA-Z_\-\.\/@]{3,80}$/;
   var membersRe = /^((user|bot|service|anonymous)\:)?[\w\-\+\%\.\@\*\[\]]+$/;
 
   // Splits |value| on lines boundary and checks that each line matches 're'.
