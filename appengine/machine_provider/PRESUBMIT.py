f564811e531f7174981f7ac6cf07ff28a70e9d54
smut <smut@google.com>
2017-01-31 16:21:52 -0800

    Add Machine Provider tests to presubmit, run_coverage.py to MP and GCE Backend
    BUG=682457
    
    Review-Url: https://codereview.chromium.org/2664343002

---
+++ appengine/machine_provider/PRESUBMIT.py
@@ -0,0 +1,26 @@
+# Copyright 2017 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+"""Presubmit for Machine Provider.
+
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts for
+details on the presubmit API built into git cl.
+"""
+
+def CommonChecks(input_api, output_api):
+  tests = input_api.canned_checks.GetUnitTestsInDirectory(
+      input_api,
+      output_api,
+      input_api.PresubmitLocalPath(),
+      whitelist=[r'.+_test\.py$'],
+  )
+  return input_api.RunTests(tests, parallel=True)
+
+
+def CheckChangeOnUpload(input_api, output_api):
+  return CommonChecks(input_api, output_api)
+
+
+def CheckChangeOnCommit(input_api, output_api):
+  return CommonChecks(input_api, output_api)
