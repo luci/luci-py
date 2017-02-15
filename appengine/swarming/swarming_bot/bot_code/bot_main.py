luci/luci-py

commit 17e1b0c97e5e7d64e67caa1f2787af8cffd6b03b

swarming_bot: Make Bot thread safe.
- Clearly separate what property is mutable and what is not.
- Do a deep copy of self._attributes, as dimensions and state are dict of dict
  or list, which are references to the original data. Better be safe than sorry.

R=vadimsh@chromium.org
BUG=

Review-Url: https://codereview.chromium.org/2694053002

appengine/swarming/swarming_bot/bot_code/bot_main.py
@@ -675,8 +675,9 @@ def _run_bot(arg_error):
     # have fully initialize bot.Bot object. Note that 'get_dimensions' and
     # 'get_state' may depend on actions done by 'on_bot_startup' hook, that's
     # why we do it here and not in 'get_bot'.
-    botobj._update_dimensions(_get_dimensions(botobj))
-    botobj._update_state(_get_state(botobj, 0))
+    with botobj._lock:
+      botobj._update_dimensions(_get_dimensions(botobj))
+      botobj._update_state(_get_state(botobj, 0))
 
     if quit_bit.is_set():
       logging.info('Early quit 3')
@@ -706,8 +707,9 @@ def _run_bot(arg_error):
     last_action = time.time()
     while not quit_bit.is_set():
       try:
-        botobj._update_dimensions(_get_dimensions(botobj))
-        botobj._update_state(_get_state(botobj, consecutive_sleeps))
+        with botobj._lock:
+          botobj._update_dimensions(_get_dimensions(botobj))
+          botobj._update_state(_get_state(botobj, consecutive_sleeps))
         did_something = _poll_server(botobj, quit_bit, last_action)
         if did_something:
           last_action = time.time()

appengine/swarming/swarming_bot/api/bot.py
@@ -4,10 +4,12 @@
 
 """Bot interface used in bot_config.py."""
 
+import copy
 import inspect
 import logging
 import os
 import sys
+import threading
 import time
 
 import os_utilities
@@ -76,14 +78,18 @@ class Bot(object):
       shutdown_hook):
     # Do not expose attributes for now, as attributes may be refactored.
     assert server is None or not server.endswith('/'), server
-    self._attributes = attributes or {}
+    # Immutable.
     self._base_dir = base_dir
-    self._bot_group_cfg_ver = None
     self._remote = remote
     self._server = server
-    self._server_side_dimensions = {}
     self._server_version = server_version
     self._shutdown_hook = shutdown_hook
+
+    # Mutable.
+    self._lock = threading.Lock()
+    self._attributes = attributes or {}
+    self._bot_group_cfg_ver = None
+    self._server_side_dimensions = {}
 
   @property
   def base_dir(self):
@@ -119,12 +125,14 @@ class Bot(object):
     itself to a different pool. By forcefully overriding dimensions on the
     server side we can use them as security boundaries.
     """
-    return self._attributes.get('dimensions', {}).copy()
+    with self._lock:
+      return copy.deepcopy(self._attributes.get('dimensions', {}))
 
   @property
   def id(self):
     """Returns the bot's ID."""
-    return self.dimensions.get('id', ['unknown'])[0]
+    with self._lock:
+      return self._attributes.get('dimensions', {}).get('id', ['unknown'])[0]
 
   @property
   def remote(self):
@@ -164,7 +172,8 @@ class Bot(object):
 
     The state may change often, but it can't be used in scheduling decisions.
     """
-    return self._attributes.get('state', {}).copy()
+    with self._lock:
+      return copy.deepcopy(self._attributes.get('state', {}))
 
   @property
   def swarming_bot_zip(self):
@@ -181,7 +190,9 @@ class Bot(object):
 
   def post_event(self, event_type, message):
     """Posts an event to the server."""
-    self._remote.post_bot_event(event_type, message, self._attributes)
+    with self._lock:
+      attr = copy.deepcopy(self._attributes)
+    self._remote.post_bot_event(event_type, message, attr)
 
   def post_error(self, message):
     """Posts given string as a failure.
@@ -191,7 +202,7 @@ class Bot(object):
     Include a full stack trace, because sometimes the error is not sufficient
     by itself.
     """
-    logging.error('Error: %s\n%s', self._attributes, message)
+    logging.error('post_error(%s)', message)
     stack = '\nCalling stack:\n%s' % _make_stack()
     try:
       self.post_event('bot_error', '%s%s' % (message.rstrip(), stack))
@@ -239,11 +250,12 @@ class Bot(object):
 
     See docs for '/handshake' call for the format of 'cfg' dict.
     """
-    self._bot_group_cfg_ver = cfg_version
-    self._server_side_dimensions = (cfg or {}).get('dimensions')
-    # Apply changes to 'self._attributes'.
-    self._update_dimensions(self._attributes.get('dimensions') or {})
-    self._update_state(self._attributes.get('state') or {})
+    with self._lock:
+      self._bot_group_cfg_ver = cfg_version
+      self._server_side_dimensions = (cfg or {}).get('dimensions')
+      # Apply changes to 'self._attributes'.
+      self._update_dimensions(self._attributes.get('dimensions') or {})
+      self._update_state(self._attributes.get('state') or {})
 
   def _update_dimensions(self, new_dimensions):
     """Called internally to update Bot.dimensions."""

commit d8a3c8d29a0780bcbd93a8eab25f60180234cfeb

swarming_bot: Fix deadlock introduced in 17e1b0c97e5e7d.
This caused any hook in get_dimensions() or get_state() to call back into the
bot.Bot instance to deadlock.

TBR=vadimsh@chromium.org
BUG=

Review-Url: https://codereview.chromium.org/2696023003

appengine/swarming/swarming_bot/bot_code/bot_main.py
@@ -675,9 +675,11 @@ def _run_bot(arg_error):
     # have fully initialize bot.Bot object. Note that 'get_dimensions' and
     # 'get_state' may depend on actions done by 'on_bot_startup' hook, that's
     # why we do it here and not in 'get_bot'.
+    dims = _get_dimensions(botobj)
+    states = _get_state(botobj, 0)
     with botobj._lock:
-      botobj._update_dimensions(_get_dimensions(botobj))
-      botobj._update_state(_get_state(botobj, 0))
+      botobj._update_dimensions(dims)
+      botobj._update_state(states)
 
     if quit_bit.is_set():
       logging.info('Early quit 3')
@@ -707,9 +709,11 @@ def _run_bot(arg_error):
     last_action = time.time()
     while not quit_bit.is_set():
       try:
+        dims = _get_dimensions(botobj)
+        states = _get_state(botobj, consecutive_sleeps)
         with botobj._lock:
-          botobj._update_dimensions(_get_dimensions(botobj))
-          botobj._update_state(_get_state(botobj, consecutive_sleeps))
+          botobj._update_dimensions(dims)
+          botobj._update_state(states)
         did_something = _poll_server(botobj, quit_bit, last_action)
         if did_something:
           last_action = time.time()