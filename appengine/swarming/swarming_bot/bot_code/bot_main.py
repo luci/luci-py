luci/luci-py

commit 46c91ccebf963cd871f87b94c2260690e7ff532b

swarming: switch ts_mon metrics from seconds to milliseconds
The problem isn't the data but the bucketting; they get bucketed at second
resolution. Since most hooks runs in a negligible amount of time, this leads to
unactionable data.

R=sergeyberezin@chromium.org
BUG=

Review-Url: https://codereview.chromium.org/2713513005

appengine/swarming/swarming_bot/bot_code/bot_main.py
@@ -105,13 +105,13 @@ DEFAULT_SETTINGS = {
 ### Monitoring
 
 
-_bucketer = ts_mon.GeometricBucketer(growth_factor=10**0.05,
+_bucketer = ts_mon.GeometricBucketer(growth_factor=10**0.07,
                                      num_finite_buckets=100)
 
 hooks_durations = ts_mon.CumulativeDistributionMetric(
     'swarming/bots/hooks/durations', bucketer=_bucketer,
-    description='Duration of bot hook calls.',
-    units=ts_mon.MetricsDataUnits.SECONDS)
+    description='Duration of bot hook calls in ms',
+    units=ts_mon.MetricsDataUnits.MILLISECONDS)
 
 
 def _flatten_dimensions(dimensions):
@@ -129,7 +129,7 @@ def monitor_call(func):
     try:
       return func(chained, botobj, name, *args, **kwargs)
     finally:
-      duration = max(0, time.time() - start)
+      duration = max(0, (time.time() - start) * 1000)
       if botobj and botobj.dimensions:
         flat_dims = _flatten_dimensions(botobj.dimensions)
         if flat_dims:

commit 37a1ef32412c606c26a535551acb2aa4fa25cd8b

swarming: fix hilarious log entry
duration is now in ms, not seconds, but it's easier to still log in seconds. The
logging regression was introduced in 46c91ccebf963cd.

R=sergeyberezin@chromium.org
BUG=

Review-Url: https://codereview.chromium.org/2714553003

appengine/swarming/swarming_bot/bot_code/bot_main.py
@@ -135,7 +135,7 @@ def monitor_call(func):
         if flat_dims:
           hooks_durations.add(
               duration, fields={'hookname': name, 'pool': flat_dims})
-      logging.info('%s(): %gs', name, round(duration, 3))
+      logging.info('%s(): %gs', name, round(duration/1000., 3))
   return hook
 
 