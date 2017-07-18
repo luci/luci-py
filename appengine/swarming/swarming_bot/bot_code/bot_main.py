3eb44d2179038db9f1cb33fd852f4918e9fe2b2c
vadimsh <vadimsh@chromium.org>
2017-07-18 06:38:49 -0700

    Fix misleading 'Isolate will use gRPC proxy' logging.
    grpc_proxy is '', not None. So we print the log line, and then continue not
    using grpc proxy.
    
    R=maruel@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2978153002

--- appengine/swarming/swarming_bot/bot_code/bot_main.py
+++ appengine/swarming/swarming_bot/bot_code/bot_main.py
@@ -587,7 +587,7 @@ def _run_isolated_flags(botobj):
   # override.
   grpc_proxy = get_config().get('isolate_grpc_proxy')
   grpc_proxy = os.environ.get('ISOLATE_GRPC_PROXY', grpc_proxy)
-  if grpc_proxy is not None:
+  if grpc_proxy:
     logging.info('Isolate will use gRPC proxy %s', grpc_proxy)
     args.extend(['--grpc-proxy', grpc_proxy])
   return args
