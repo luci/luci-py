f59af786b5b9f1571698333c0b064231ca07d144
maruel <maruel@chromium.org>
2017-07-24 09:30:17 -0700

    Bump number of outputs limit to 4096.
    This safe because even if Cloud DB's entity size limit is '1Mb-4',
    TaskProperties is stored as an ndb.LocalStructuredProperty(compressed=True).
    This means the embedded struct is compressed via python's zlib.compress() and
    the data is quite compressible, so the expectation is that TaskProperties can
    safely be significantly larger 1Mb.
    
    TBR=aludiwn@google.com
    BUG=
    
    Review-Url: https://codereview.chromium.org/2951853003

--- appengine/swarming/server/task_request.py
+++ appengine/swarming/server/task_request.py
@@ -610,9 +610,9 @@ class TaskProperties(ndb.Model):
               'an idempotent task cannot have unpinned packages; '
               'use tags or instance IDs as package versions')
 
-    if len(self.outputs) > 2048:
+    if len(self.outputs) > 4096:
       raise datastore_errors.BadValueError(
-          'Up to 2048 outputs can be listed for a task')
+          'Up to 4096 outputs can be listed for a task')
 
 
 class TaskRequest(ndb.Model):
