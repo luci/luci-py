939316c1373320ae305492632d7fc91a0f53367e
Marc-Antoine Ruel <maruel@chromium.org>
2018-03-05 21:06:40 -0500

    swarming_bot: android state 'host_dimensions' to use the default dimensions
    It's to reduce the copy pasted code and will give a clearer picture.
    
    Bug:
    Change-Id: I8df9376b8eb7eed542fbb7ee9c23802242a69969
    Reviewed-on: https://chromium-review.googlesource.com/854554
    Commit-Queue: Marc-Antoine Ruel <maruel@chromium.org>
    Reviewed-by: Benjamin Pastene <bpastene@chromium.org>

--- appengine/swarming/swarming_bot/api/os_utilities.py
+++ appengine/swarming/swarming_bot/api/os_utilities.py
@@ -906,15 +906,9 @@ def get_state_all_devices_android(devices):
     return state
 
   # Add a few values that were popped from dimensions.
-  state[u'host_dimensions'] = {
-    u'cpu': get_cpu_dimensions(),
-    u'cores': [unicode(get_num_processors())],
-    u'gpu': get_gpu()[0],
-  }
-  machine_type = get_machine_type()
-  if machine_type:
-    state[u'host_dimensions'][u'machine_type'] = [machine_type]
-
+  state[u'host_dimensions'] = get_dimensions()
+  # The default value is irrelevant.
+  state[u'host_dimensions'].pop(u'pool', None)
   state.update(platforms.android.get_state(devices))
   return state
 
