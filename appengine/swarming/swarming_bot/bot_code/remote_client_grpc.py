11dda251b37e0b0b34e3fd52c895b00fd31e33b8
Wei Huang <huangwe@google.com>
2018-03-06 16:10:11 -0500

    Send final stdout chunk before aborting tasks.
    We shouldn't send stdout after marking a task as aborted.
    
    b/73906742
    
    Change-Id: Ic75291f9a2107fc9172b26d1b7b68503c6a68168
    Reviewed-on: https://chromium-review.googlesource.com/952098
    Commit-Queue: Wei Huang <huangwe@google.com>
    Reviewed-by: Adrian Ludwin <aludwin@google.com>

--- appengine/swarming/swarming_bot/bot_code/remote_client_grpc.py
+++ appengine/swarming/swarming_bot/bot_code/remote_client_grpc.py
@@ -239,6 +239,10 @@ class RemoteClientGrpc(object):
     return should_continue
 
   def post_task_error(self, task_id, bot_id, message):
+    # Send final stdout chunk before aborting the task. This is to satisfy the
+    # Remote Worker API semantics.
+    self._send_stdout_chunk(bot_id, task_id, None, True)
+
     req = tasks_pb2.UpdateTaskResultRequest()
     req.name = task_id + '/result'
     req.source = bot_id
@@ -247,7 +251,6 @@ class RemoteClientGrpc(object):
     req.result.status.code = code_pb2.ABORTED
     req.result.status.message = message
     self._proxy_tasks.call_unary('UpdateTaskResult', req)
-    self._send_stdout_chunk(bot_id, task_id, None, True)
 
   def get_bot_code(self, new_zip_fn, bot_version, _bot_id):
     with open(new_zip_fn, 'w') as zf:
