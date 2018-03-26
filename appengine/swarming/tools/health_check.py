c4cb71761baac10facb4bb411f2d5fe3d39d48ba
Peter Ward <flowblok@chromium.org>
2018-03-26 12:16:31 +1100

    Automatically select the best pool for the health check task.
    Bug: 812965
    Change-Id: I1306464225ddb813100170b9d65114673d01cb19
    Reviewed-on: https://chromium-review.googlesource.com/948202
    Commit-Queue: flowblok <flowblok@chromium.org>
    Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>

--- appengine/swarming/tools/health_check.py
+++ appengine/swarming/tools/health_check.py
@@ -6,19 +6,83 @@
 """Check the health of a Swarming version."""
 
 import argparse
+import collections
+import functools
+import json
 import os
 import subprocess
 import sys
+import time
 
 HERE = os.path.dirname(__file__)
 SWARMING_TOOL = os.path.join(HERE, '..', '..', '..', 'client', 'swarming.py')
 
-# TODO(flowblok): remove this hard-coded pool
-POOL = 'ChromeOS'
+
+def retry_exception(exc_type, max_attempts, delay):
+  """Decorator to retry a function on failure with linear backoff.
+
+  Args:
+    exc_type: The type of exception raised by the function to retry.
+    max_attempts: Maximum number of times to call the function before reraising
+                  the exception.
+    delay: Time to sleep between attempts, in seconds.
+
+  Returns:
+    A decorator to be applied to the function.
+  """
+  def deco(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+      for _ in range(max_attempts):
+        try:
+          return fn(*args, **kwargs)
+        except exc_type:
+          time.sleep(delay)
+    return wrapper
+  return deco
+
+
+@retry_exception(ValueError, 1, 10)
+def pick_best_pool(url, server_version):
+  """Pick the best pool to run the health check task on.
+
+  Asks the specified swarming server for a list of all bots, filters those
+  running the specified server version, and returns the pool with the most bots
+  in it.
+
+  Args:
+    url: The swarming server to query.
+    server_version: Which server version to filter bots by.
+
+  Returns:
+    A string indicating the best pool to run the health check task on.
+  """
+  output = subprocess.check_output([
+      SWARMING_TOOL, 'query',
+      '-S', url,
+      '--limit', '0',
+      'bots/list?dimensions=server_version:%s' % server_version,
+  ])
+  data = json.loads(output)
+  bots = data.get('items', [])
+
+  pool_counts = collections.Counter()
+  for bot in bots:
+    for dimension in bot.get('dimensions', []):
+      if dimension['key'] == 'pool':
+        pool_counts.update(dimension['value'])
+
+  if not pool_counts:
+    raise ValueError('No bots are running server_version=%s' % server_version)
+
+  return pool_counts.most_common(1)[0][0]
 
 
 def main():
   parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--pool',
+      help='Pool to schedule a task on. If unspecified, this is autodetected.')
   parser.add_argument('application')
   parser.add_argument('server_version')
   args = parser.parse_args()
@@ -26,15 +90,20 @@ def main():
   url = 'https://{server_version}-dot-{application}.appspot.com'.format(
       application=args.application,
       server_version=args.server_version)
+  print 'Swarming server:', url
 
-  print 'Scheduling no-op task on', url
+  pool = args.pool
+  if not pool:
+    print 'Finding best pool to use'
+    pool = pick_best_pool(url, args.server_version)
 
+  print 'Scheduling no-op task'
   rv = subprocess.call([
       SWARMING_TOOL, 'run',
       '-S', url,
       '--expiration', '120',
       '--hard-timeout', '120',
-      '-d', 'pool', POOL,
+      '-d', 'pool', pool,
       '-d', 'server_version', args.server_version,
       '--raw-cmd', '--', 'python', '-c', 'pass'])
   if rv != 0:
