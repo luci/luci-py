67c85ab36fa78866fc257cf8411a3e77b2631259
Nodir Turakulov <nodir@google.com>
2018-05-31 08:25:57 -0700

    [prpc] Include pRPC tests in PRESUBMIT
    R=vadimsh@chromium.org
    
    Change-Id: I80d7a79139d63066aca84d7699b646eb4a45cb26
    Reviewed-on: https://chromium-review.googlesource.com/1080282
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>

--- appengine/components/PRESUBMIT.py
+++ appengine/components/PRESUBMIT.py
@@ -67,6 +67,7 @@ def CommonChecks(input_api, output_api):
     join('components', 'config'),
     join('components', 'datastore_utils'),
     join('components', 'ereporter2'),
+    join('components', 'prpc'),
     join('tests'),
   ]
   tests = []
