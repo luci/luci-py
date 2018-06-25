65de3aef5845afd951e64077df8c50d11cc9a2b1
Nodir Turakulov <nodir@google.com>
2018-06-25 23:11:58 +0000

    [prpc] Add ServicerContext.clone
    Usage: https://chromium-review.googlesource.com/c/infra/infra/+/1114186
    
    Bug: 854218
    Change-Id: If9ae582f5b08d58ebc4ff8e598a49332213fffde
    Reviewed-on: https://chromium-review.googlesource.com/1114242
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>

--- appengine/components/components/prpc/context.py
+++ appengine/components/components/prpc/context.py
@@ -113,3 +113,14 @@ class ServicerContext(object):
   def response_encoding(self):
     """Returns prpc.Encoding of the response."""
     return self._response_encoding
+
+  def clone(self):
+    """Returns a shallow copy of self.
+
+    This may be useful for running parallel handlers that each may return
+    different results and may have different timeouts.
+    """
+    ret = ServicerContext()
+    # pylint: disable=attribute-defined-outside-init
+    ret.__dict__ = self.__dict__.copy()
+    return ret
