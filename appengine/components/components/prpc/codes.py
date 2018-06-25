cedeb9791b002f75e47cc017707c57aa0d4c4672
Nodir Turakulov <nodir@google.com>
2018-06-25 23:11:08 +0000

    [prpc] Make RPC codes a named tuple
    Expose error code value and name as an attribute of a status code object.
    
    Bug: 854218
    Change-Id: I78fbcfcadadf1234115f377e29f693eb98d0a6a2
    Reviewed-on: https://chromium-review.googlesource.com/1114245
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>

--- appengine/components/components/prpc/codes.py
+++ appengine/components/components/prpc/codes.py
@@ -4,31 +4,37 @@
 
 """Definition of possible RPC response status codes."""
 
+import collections
 
-class StatusCode(object):
+StatusCodeBase = collections.namedtuple('StatusCodeBase', ['value', 'name'])
+
+
+class StatusCode(StatusCodeBase):
   """Mirrors grpc.StatusCode in the gRPC Core.
 
   See https://grpc.io/grpc/python/grpc.html?highlight=status#grpc.StatusCode
   """
-  OK                  = (0, 'ok')
-  CANCELLED           = (1, 'cancelled')
-  UNKNOWN             = (2, 'unknown')
-  INVALID_ARGUMENT    = (3, 'invalid argument')
-  DEADLINE_EXCEEDED   = (4, 'deadline exceeded')
-  NOT_FOUND           = (5, 'not found')
-  ALREADY_EXISTS      = (6, 'already exists')
-  PERMISSION_DENIED   = (7, 'permission denied')
-  RESOURCE_EXHAUSTED  = (8, 'resource exhausted')
-  FAILED_PRECONDITION = (9, 'failed precondition')
-  ABORTED             = (10, 'aborted')
-  OUT_OF_RANGE        = (11, 'out of range')
-  UNIMPLEMENTED       = (12, 'unimplemented')
-  INTERNAL            = (13, 'internal error')
-  UNAVAILABLE         = (14, 'unavailable')
-  DATA_LOSS           = (15, 'data loss')
-  UNAUTHENTICATED     = (16, 'unauthenticated')
+  OK                  = StatusCodeBase(0, 'ok')
+  CANCELLED           = StatusCodeBase(1, 'cancelled')
+  UNKNOWN             = StatusCodeBase(2, 'unknown')
+  INVALID_ARGUMENT    = StatusCodeBase(3, 'invalid argument')
+  DEADLINE_EXCEEDED   = StatusCodeBase(4, 'deadline exceeded')
+  NOT_FOUND           = StatusCodeBase(5, 'not found')
+  ALREADY_EXISTS      = StatusCodeBase(6, 'already exists')
+  PERMISSION_DENIED   = StatusCodeBase(7, 'permission denied')
+  RESOURCE_EXHAUSTED  = StatusCodeBase(8, 'resource exhausted')
+  FAILED_PRECONDITION = StatusCodeBase(9, 'failed precondition')
+  ABORTED             = StatusCodeBase(10, 'aborted')
+  OUT_OF_RANGE        = StatusCodeBase(11, 'out of range')
+  UNIMPLEMENTED       = StatusCodeBase(12, 'unimplemented')
+  INTERNAL            = StatusCodeBase(13, 'internal error')
+  UNAVAILABLE         = StatusCodeBase(14, 'unavailable')
+  DATA_LOSS           = StatusCodeBase(15, 'data loss')
+  UNAUTHENTICATED     = StatusCodeBase(16, 'unauthenticated')
 
 
 # Used in ServicerContext.set_code to assert that the code is known.
 ALL_CODES = frozenset(
-    getattr(StatusCode, k) for k in dir(StatusCode) if not k.startswith('_'))
+    getattr(StatusCode, k)
+    for k in dir(StatusCode)
+    if isinstance(getattr(StatusCode, k), StatusCodeBase))
