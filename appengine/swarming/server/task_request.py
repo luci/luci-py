f1bd1fa7ed6bb91f908bfcfa425b9db843fa1535
Marc-Antoine Ruel <maruel@chromium.org>
2019-01-17 20:19:42 +0000

    [swarming]: fix TaskRequest.to_proto with broken request and BQ table
    - TaskProperties.env_paths comments was more than 1024 characters, so
      BigQuery rejected it. Trim the comments to be under this limit.
    - Tweak the wording of other comments.
    - Fix an issue where some stored TaskProperties.env and env_prefixes is
      None, causing a crash when converting to proto.
    
    Bug: 765435
    Change-Id: Ie195b55ddc9bd939ded069141c114d521a978462
    Reviewed-on: https://chromium-review.googlesource.com/c/1416856
    Commit-Queue: Marc-Antoine Ruel <maruel@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>

--- appengine/swarming/server/task_request.py
+++ appengine/swarming/server/task_request.py
@@ -759,15 +759,14 @@ class TaskProperties(ndb.Model):
       v = out.dimensions.add()
       v.key = key
       v.values.extend(sorted(values))
-    for key, value in sorted(self.env.iteritems()):
+    for key, value in sorted((self.env or {}).iteritems()):
       v = out.env.add()
       v.key = key
       v.value = value
-    if self.env_prefixes:
-      for key, values in sorted(self.env_prefixes.iteritems()):
-        v = out.env_paths.add()
-        v.key = key
-        v.values.extend(sorted(values))
+    for key, values in sorted((self.env_prefixes or {}).iteritems()):
+      v = out.env_paths.add()
+      v.key = key
+      v.values.extend(sorted(values))
     # TODO(maruel): Define containment; https://crbug.com/808836
     if self.execution_timeout_secs:
       out.execution_timeout.seconds = self.execution_timeout_secs
