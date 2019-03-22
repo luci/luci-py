9fa3b2089ce50cb6fadf0a17c13d50f0e40656e0
Marc-Antoine Ruel <maruel@chromium.org>
2019-03-22 01:13:06 +0000

    [swarming] Use a slightly les hackish sleep
    Use standard ndb.Future.wait_any() and a list instead of a set of
    ndb.Future. This should help with the deadlock observed on prod. Add a
    log entry to confirm that the log is happening before or after, to see
    if it helped or not.
    
    Bug: 900042
    Change-Id: I264e6002bfdfcf28e23611cfe5902fb839e5979d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1534883
    Commit-Queue: Jao-ke Chin-Lee <jchinlee@chromium.org>
    Auto-Submit: Marc-Antoine Ruel <maruel@chromium.org>
    Reviewed-by: Jao-ke Chin-Lee <jchinlee@chromium.org>

--- appengine/swarming/server/task_queues.py
+++ appengine/swarming/server/task_queues.py
@@ -313,17 +313,15 @@ def _cap_futures(futures):
   """
   out = []
   while len(futures) > 50:
-    # Give the event loop some time to run tasks.
-    r = ndb.eventloop.run0()
-    if r is not None:
-      time.sleep(r)
-    done = []
+    ndb.Future.wait_any(futures)
+    tmp = []
     for f in futures:
       if f.done():
         out.append(f.get_result())
-        done.append(f)
-    futures.difference_update(done)
-  return out
+      else:
+        tmp.append(tmp)
+    futures = tmp
+  return out, futures
 
 
 def _flush_futures(futures):
@@ -893,15 +891,17 @@ def rebuild_task_cache(payload):
   updated = 0
   viable = 0
   try:
-    pending = set()
+    pending = []
     for bot_task_key in _yield_BotTaskDimensions_keys(
         dimensions_hash, dimensions_flat):
       viable += 1
       future = _refresh_BotTaskDimensions(
           bot_task_key, dimensions_flat, now, valid_until_ts)
-      pending.add(future)
-      updated += sum(1 for i in _cap_futures(pending) if i)
+      pending.append(future)
+      done, pending = _cap_futures(pending)
+      updated += sum(1 for i in done if i)
     updated += sum(1 for i in _flush_futures(pending) if i)
+    logging.debug('updated %d; getting reading for transaction', updated)
 
     # Done updating, now store the entity. Must use a transaction as there could
     # be other dimensions set in the entity.
