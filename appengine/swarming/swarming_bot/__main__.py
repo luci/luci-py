96e160e4a0ac74dbe5019b16dd38eb7dc472aaea
Lei Lei <leilei@chromium.org>
2019-06-14 20:53:53 +0000

    Reland "[client] Rename third_party/pyasn1-modules to third_party/pyasn1_modules"
    This reverts commit bf6abb217a664f0a0d93b25639a1dc48e1c78d0c.
    
    Reason for revert: Reland the changes to rename pyasn1-modules to pyasn1_modules, and will fix the issue, which caused severe outage.
    
    Original change's description:
    > Revert "[client] Rename third_party/pyasn1-modules to third_party/pyasn1_modules"
    >
    > This reverts commit 1341b4f0cad1d767ed7b51c9380072c50cd1949f.
    >
    > Reason for revert: caused severe outage
    >
    > I suspect it happened to work on the CI because pyasn1_modules
    > happens to be installed on the machine.
    >
    > Bug: 970342
    >
    > Original change's description:
    > > [client] Rename third_party/pyasn1-modules to third_party/pyasn1_modules
    > >
    > > There is a compile error when '-' in file path on python3.
    > >
    > > Bug: 942720
    > > Change-Id: Id6ddedcc1469db8014499520015c28ebc9a0945a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1637363
    > > Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    > > Commit-Queue: Marc-Antoine Ruel <maruel@chromium.org>
    >
    > TBR=maruel@chromium.org,leilei@chromium.org
    >
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    >
    > Bug: 942720
    > Change-Id: I0c57dbafe81d68c7d283d517f0a8afc41c1348a1
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1642224
    > Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    > Commit-Queue: Marc-Antoine Ruel <maruel@chromium.org>
    
    TBR=maruel@chromium.org,leilei@chromium.org
    
    Bug: 970342, 942720
    Change-Id: I2c34f0da62cebc990dd83f161bb9527f49880830
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1656512
    Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    Commit-Queue: Lei Lei <leilei@chromium.org>

--- appengine/swarming/swarming_bot/__main__.py
+++ appengine/swarming/swarming_bot/__main__.py
@@ -34,7 +34,7 @@ sys.path.insert(0, os.path.join(THIS_FILE, 'third_party'))
 sys.path.insert(0, os.path.join(
     THIS_FILE, 'third_party', 'httplib2', 'python%d' % sys.version_info.major))
 sys.path.insert(0, os.path.join(THIS_FILE, 'third_party', 'pyasn1'))
-sys.path.insert(0, os.path.join(THIS_FILE, 'third_party', 'pyasn1-modules'))
+sys.path.insert(0, os.path.join(THIS_FILE, 'third_party', 'pyasn1_modules'))
 sys.path.insert(0, os.path.join(THIS_FILE, 'third_party', 'rsa'))
 
 
