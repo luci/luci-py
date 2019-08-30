84d687ec678f578e2a0acfc9cf4797b5514bc1de
Marc-Antoine Ruel <maruel@chromium.org>
2019-08-30 15:18:18 +0000

    [swarming] Do not log error for KILLED tasks ignored in eventmon
    We are eventually going to turn down eventmon. This is just creation
    noise at error level, which makes diagnosing other issues harder.
    
    R=tikuta@chromium.org
    
    Change-Id: Ic4504a55b8cc2a00cb19b84eabec7cf0ae500b99
    Bug: 795745
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1775292
    Commit-Queue: Marc-Antoine Ruel <maruel@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>

--- appengine/swarming/event_mon_metrics.py
+++ appengine/swarming/event_mon_metrics.py
@@ -117,10 +117,10 @@ def _task_summary_to_proto(summary, event):
     event.proto.swarming_task_event.state = state_enum['TIMED_OUT'].number
   elif summary.state == task_result.State.EXPIRED:
     event.proto.swarming_task_event.state = state_enum['EXPIRED'].number
-  # TODO(maruel): Report KILLED tasks.
-  # https://crbug.com/754390
-  #elif summary.state == task_result.State.KILLED:
-  #  event.proto.swarming_task_event.state = state_enum['KILLED'].number
+  elif summary.state == task_result.State.KILLED:
+    # This state is not defined in the schema, and we are turning it down
+    # anyway. https://crbug.com/795745
+    pass
   else:
     logging.error('Unhandled task state %r', summary.state)
 
