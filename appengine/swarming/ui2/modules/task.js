c51c800261d4a77296cc2928fc2e6899bc97a803
Junji Watanabe <jwata@google.com>
2019-09-19 01:37:21 +0000

    add license header to modules/task.js in swarming-ui
    Bug: 826709
    Change-Id: I7adb55563d373ea16852de3df5e2a6d1126a110b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1808993
    Auto-Submit: Junji Watanabe <jwata@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>

--- appengine/swarming/ui2/modules/task.js
+++ appengine/swarming/ui2/modules/task.js
@@ -1,3 +1,7 @@
+// Copyright 2019 The LUCI Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 export const ONGOING_STATES = new Set([
   'PENDING',
   'RUNNING'
