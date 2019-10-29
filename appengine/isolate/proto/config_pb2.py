fdeba1645ae943c93050c0d4eda2bf8fe01186cf
smut <smut@google.com>
2019-10-29 22:09:28 +0000

    [isolate] Add client monitoring config
    Bug: 1015684
    Change-Id: I4a3e332d8576e14706640a97f6e72af15a93b8f6
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1880517
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: smut <smut@google.com>

--- appengine/isolate/proto/config_pb2.py
+++ appengine/isolate/proto/config_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='',
   syntax='proto2',
   serialized_options=None,
-  serialized_pb=_b('\n\x0c\x63onfig.proto\"\xdd\x01\n\x0bSettingsCfg\x12\x18\n\x10google_analytics\x18\x01 \x01(\t\x12\x1a\n\x12\x64\x65\x66\x61ult_expiration\x18\x02 \x01(\x05\x12\x18\n\x10sharding_letters\x18\x03 \x01(\x05\x12\x11\n\tgs_bucket\x18\x04 \x01(\t\x12\x1a\n\x12gs_client_id_email\x18\x05 \x01(\t\x12\x1c\n\x14\x65nable_ts_monitoring\x18\x06 \x01(\x08\x12\x1b\n\x04\x61uth\x18\x07 \x01(\x0b\x32\r.AuthSettings\x12\x14\n\x0cui_client_id\x18\x08 \x01(\t\"H\n\x0c\x41uthSettings\x12\x19\n\x11\x66ull_access_group\x18\x01 \x01(\t\x12\x1d\n\x15readonly_access_group\x18\x02 \x01(\t')
+  serialized_pb=_b('\n\x0c\x63onfig.proto\"\x98\x02\n\x0bSettingsCfg\x12\x18\n\x10google_analytics\x18\x01 \x01(\t\x12\x1a\n\x12\x64\x65\x66\x61ult_expiration\x18\x02 \x01(\x05\x12\x18\n\x10sharding_letters\x18\x03 \x01(\x05\x12\x11\n\tgs_bucket\x18\x04 \x01(\t\x12\x1a\n\x12gs_client_id_email\x18\x05 \x01(\t\x12\x1c\n\x14\x65nable_ts_monitoring\x18\x06 \x01(\x08\x12\x1b\n\x04\x61uth\x18\x07 \x01(\x0b\x32\r.AuthSettings\x12\x14\n\x0cui_client_id\x18\x08 \x01(\t\x12\x39\n\x18\x63lient_monitoring_config\x18\t \x03(\x0b\x32\x17.ClientMonitoringConfig\"H\n\x0c\x41uthSettings\x12\x19\n\x11\x66ull_access_group\x18\x01 \x01(\t\x12\x1d\n\x15readonly_access_group\x18\x02 \x01(\t\"=\n\x16\x43lientMonitoringConfig\x12\x14\n\x0cip_whitelist\x18\x01 \x01(\t\x12\r\n\x05label\x18\x02 \x01(\t')
 )
 
 
@@ -88,6 +88,13 @@ _SETTINGSCFG = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='client_monitoring_config', full_name='SettingsCfg.client_monitoring_config', index=8,
+      number=9, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -101,7 +108,7 @@ _SETTINGSCFG = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=17,
-  serialized_end=238,
+  serialized_end=297,
 )
 
 
@@ -138,13 +145,53 @@ _AUTHSETTINGS = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=240,
-  serialized_end=312,
+  serialized_start=299,
+  serialized_end=371,
+)
+
+
+_CLIENTMONITORINGCONFIG = _descriptor.Descriptor(
+  name='ClientMonitoringConfig',
+  full_name='ClientMonitoringConfig',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='ip_whitelist', full_name='ClientMonitoringConfig.ip_whitelist', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='label', full_name='ClientMonitoringConfig.label', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto2',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=373,
+  serialized_end=434,
 )
 
 _SETTINGSCFG.fields_by_name['auth'].message_type = _AUTHSETTINGS
+_SETTINGSCFG.fields_by_name['client_monitoring_config'].message_type = _CLIENTMONITORINGCONFIG
 DESCRIPTOR.message_types_by_name['SettingsCfg'] = _SETTINGSCFG
 DESCRIPTOR.message_types_by_name['AuthSettings'] = _AUTHSETTINGS
+DESCRIPTOR.message_types_by_name['ClientMonitoringConfig'] = _CLIENTMONITORINGCONFIG
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 SettingsCfg = _reflection.GeneratedProtocolMessageType('SettingsCfg', (_message.Message,), dict(
@@ -160,6 +207,13 @@ AuthSettings = _reflection.GeneratedProtocolMessageType('AuthSettings', (_messag
   # @@protoc_insertion_point(class_scope:AuthSettings)
   ))
 _sym_db.RegisterMessage(AuthSettings)
+
+ClientMonitoringConfig = _reflection.GeneratedProtocolMessageType('ClientMonitoringConfig', (_message.Message,), dict(
+  DESCRIPTOR = _CLIENTMONITORINGCONFIG,
+  __module__ = 'config_pb2'
+  # @@protoc_insertion_point(class_scope:ClientMonitoringConfig)
+  ))
+_sym_db.RegisterMessage(ClientMonitoringConfig)
 
 
 # @@protoc_insertion_point(module_scope)
