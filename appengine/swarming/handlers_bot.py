f6d98f9d3b5ae0fc03b39120836bc703888f6d4b
Junji Watanabe <jwata@google.com>
2019-12-05 10:53:24 +0000

    [swarming] move assert_bot to poll handler
    This is just a refactoring. No functionalities will change.
    
    assert_bot is called only at /poll
    No need to be in _BotBaseHandler._process() anymore.
    
    Bug: 801679
    Change-Id: I9717d4216f35ed224b282d608d10fa4d81e7ddaf
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1948022
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Junji Watanabe <jwata@google.com>

--- appengine/swarming/handlers_bot.py
+++ appengine/swarming/handlers_bot.py
@@ -313,7 +313,7 @@ class _BotBaseHandler(_BotApiHandler):
   EXPECTED_KEYS = {u'dimensions', u'state', u'version'}
   REQUIRED_STATE_KEYS = {u'running_time', u'sleep_streak'}
 
-  def _process(self, assert_bot):
+  def _process(self):
     """Fetches bot info and settings, does authorization and quarantine checks.
 
     Returns:
@@ -429,12 +429,6 @@ class _BotBaseHandler(_BotApiHandler):
       result.quarantined_msg = 'Quarantined by admin'
       return result
 
-    if assert_bot:
-      # TODO(jwata): Parallelise assert_bot by removing get_result(),
-      # Use @ndb.toplevel on the handler not to forget calling ndb.Future
-      bot_root_key = bot_management.get_root_key(bot_id)
-      task_queues.assert_bot_async(bot_root_key, dimensions).get_result()
-
     return result
 
 
@@ -461,7 +455,7 @@ class BotHandshakeHandler(_BotBaseHandler):
   """
   @auth.public  # auth happens in self._process()
   def post(self):
-    res = self._process(assert_bot=False)
+    res = self._process()
     bot_management.bot_event(
         event_type='bot_connected', bot_id=res.bot_id,
         external_ip=self.request.remote_addr,
@@ -517,7 +511,8 @@ class BotPollHandler(_BotBaseHandler):
       self._cmd_sleep(1000, True)
       return
 
-    res = self._process(assert_bot=True)
+    res = self._process()
+
     sleep_streak = res.state.get('sleep_streak', 0)
     quarantined = bool(res.quarantined_msg)
 
@@ -577,7 +572,21 @@ class BotPollHandler(_BotBaseHandler):
       self._cmd_sleep(sleep_streak, True)
       return
 
-    # The bot is in good shape. Try to grab a task.
+    # The bot is in good shape.
+
+    # Prepare BotTaskDimensions
+    # TODO(crbug.com/1027407): get_result() takes longer than 60 sec which ends
+    # up with "DeadlineExceededError".
+    # Approaches would be to
+    # - Move the assert_bot task to background rather than waiting at polling.
+    # - Execute assert_bot_async here (before bot_reap_task), and get_result()
+    # after reaping task.
+    # See discussion:
+    # https://crrev.com/c/1948022/2#message-15c7ac534cdc49794fcb66cd209e5d2272ea22a5
+    bot_root_key = bot_management.get_root_key(res.bot_id)
+    task_queues.assert_bot_async(bot_root_key, res.dimensions).get_result()
+
+    # Try to grab a task.
     try:
       try:
         # This is a fairly complex function call, exceptions are expected.
@@ -725,7 +734,7 @@ class BotEventHandler(_BotBaseHandler):
 
   @auth.public  # auth happens in self._process()
   def post(self):
-    res = self._process(assert_bot=False)
+    res = self._process()
     event = res.request.get('event')
     if event not in self.ALLOWED_EVENTS:
       logging.error('Unexpected event type')
