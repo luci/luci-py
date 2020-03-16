ad01f666fa3964ce3cc3f297ac2111821e822df6
Junji Watanabe <jwata@google.com>
2020-03-16 22:45:43 +0000

    [auth] fix import errors in test.py
    Fix "ImportError: No module named ModuleImportFailure"
    https://logs.chromium.org/logs/infra/buildbucket/cr-buildbucket.appspot.com/8885907509484740160/+/steps/auth_service/0/steps/run_tests_python2/0/stdout#L4731_0
    
    Sadly, it's hard to see the actual error, which you can confirm with './test.py your_test' like
    
    ```
    ❯ ./test.py realms.validation_test
    E
    ======================================================================
    ERROR: realms.validation_test (nose2.loader.LoadTestsFailure)
    ----------------------------------------------------------------------
    Traceback (most recent call last):
      File "/usr/local/google/home/jwata/chrome_infra_internal/infra/luci/appengine/auth_service/realms/__init__.py", line 6, in <module>
        from .config import refetch_config
      File "/usr/local/google/home/jwata/chrome_infra_internal/infra/luci/appengine/auth_service/realms/config.py", line 22, in <module>
        from google.appengine.api import datastore_errors
    ImportError: No module named appengine.api
    ```
    
    I added a new test.py dedicated to auth_service to execute test_env.setup_test_env() for auth service tests.
    
    Run result:
    https://logs.chromium.org/logs/infra/buildbucket/cr-buildbucket.appspot.com/8885689873673929952/+/steps/auth_service/0/steps/run_tests_python2/0/stdout
    
    The exit code bug will be fixed by another CL crrev.com/c/2104732.
    
    Bug: 1061596
    Change-Id: Ie93297fe22119ebd83f722230882732520611c15
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2104733
    Auto-Submit: Junji Watanabe <jwata@google.com>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@google.com>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>

--- appengine/auth_service/test.py
+++ appengine/auth_service/test.py
@@ -1,1 +1,21 @@
-test_support/parallel_test_runner.py
+#!/usr/bin/env vpython
+# Copyright 2020 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+import sys
+
+import six
+
+import test_env
+test_env.setup_test_env()
+
+from test_support import parallel_test_runner
+
+
+def main():
+  return parallel_test_runner.run_tests(python3=six.PY3)
+
+
+if __name__ == '__main__':
+  sys.exit(main())
