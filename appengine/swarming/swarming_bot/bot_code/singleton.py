304e54af0880019f8ec00781453657cc378aeea9
Takuto Ikuta <tikuta@chromium.org>
2020-03-26 08:23:02 +0000

    swarming: add log to see what happens around singleton
    This is to increase verbosity of log like
    crbug.com/1061531#c5
    
    Bug: 1061531
    Change-Id: I72a30005caf6223ed00867cfd3bf9686ed1a949b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2120668
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Ye Kuang <yekuang@google.com>

--- appengine/swarming/swarming_bot/bot_code/singleton.py
+++ appengine/swarming/swarming_bot/bot_code/singleton.py
@@ -60,8 +60,9 @@ class Singleton(object):
       try:
         fcntl.flock(self.handle, fcntl.LOCK_EX | fcntl.LOCK_NB)
       except IOError:
-        # There's a small race conditon where it could report a previous pid.
-        logging.error('Singleton held by %s', self.handle.read())
+        # There's a small race condition where it could report a previous pid.
+        logging.exception('Singleton "%s" is held by "%s"', self.key,
+                          self.handle.read())
         self.handle.close()
         self.handle = None
         return False
