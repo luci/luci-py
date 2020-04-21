b676e5428a2d25733db9e525c8c8afe1ab5a2e4a
Takuto Ikuta <tikuta@chromium.org>
2020-04-21 12:44:12 +0000

    client: send exception from run_isolated to error reporting
    Bug: 997818
    Change-Id: Ibd3b428d87d5a73d9dac05d69fd9e84492c8af1f
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2159170
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Ye Kuang <yekuang@google.com>
    Reviewed-by: Ye Kuang <yekuang@google.com>

--- client/run_isolated.py
+++ client/run_isolated.py
@@ -1561,6 +1561,7 @@ def main(args):
       containment=containment)
   try:
     if options.isolate_server:
+      on_error.report_on_exception_exit(options.isolate_server)
       server_ref = isolate_storage.ServerRef(
           options.isolate_server, options.namespace)
       storage = isolateserver.get_storage(server_ref)
