4c5e8a88b747f91de6d894a8ea232d651a02b086
Takuto Ikuta <tikuta@chromium.org>
2020-04-30 09:15:23 +0000

    client/third_party: add license header in urllib3/update.sh
    I forgot to add license header in crrev.com/c/2174001
    
    Bug: 1076637
    Change-Id: Ibff067653b73d9ffa286a4dc019d68285513be7d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2174016
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>

--- client/third_party/urllib3/update.sh
+++ client/third_party/urllib3/update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright 2020 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
 
 cd $(dirname $0)
 
