ed2b895bc4b6baa35b1e9dd474679330a8f43ea8
Ye Kuang <yekuang@google.com>
2020-05-08 13:08:56 +0000

    client: update isolated client
    https://chromium.googlesource.com/infra/infra/+log/607e08228204224129d898082ac5b87dfc2f8e65..f097334d4f2503ba7693d2cf068c958895282993/DEPS
    
    f097334d4f Roll infra/go/src/go.chromium.org/luci a0d231294de7..4d2a6af1f9bb (1 commits)
    1a83b5afa3 Roll infra/go/src/go.chromium.org/luci b02eaec38b54..a0d231294de7 (1 commits)
    8dbb9b2d02 Roll infra/go/src/go.chromium.org/luci a52d43eb03fb..b02eaec38b54 (1 commits)
    bc5dacc3d1 Roll infra/go/src/go.chromium.org/luci 1894ebc4959a..a52d43eb03fb (1 commits)
    0ccad25e19 Roll infra/go/src/go.chromium.org/luci 3b56bc16c5b2..1894ebc4959a (1 commits)
    ef3e2cbe12 Roll infra/go/src/go.chromium.org/luci ea7ecf6bb218..3b56bc16c5b2 (1 commits)
    b0a50c74c3 Roll infra/go/src/go.chromium.org/luci d92e37c72a39..ea7ecf6bb218 (1 commits)
    9fd2920e99 Roll infra/go/src/go.chromium.org/luci f9713342cf19..d92e37c72a39 (1 commits)
    f38c9200a8 Roll infra/go/src/go.chromium.org/luci f31862d2067b..f9713342cf19 (1 commits)
    1e812bbb59 Roll infra/go/src/go.chromium.org/luci 999297ce3762..f31862d2067b (1 commits)
    de1c2f0ea8 Roll infra/go/src/go.chromium.org/luci 4602c6ca4c15..999297ce3762 (1 commits)
    386adf750f Roll infra/go/src/go.chromium.org/luci 8ac794278f42..4602c6ca4c15 (1 commits)
    b301ef9f93 Roll infra/go/src/go.chromium.org/luci b466476f1b1b..8ac794278f42 (1 commits)
    b080c67f92 Roll infra/go/src/go.chromium.org/luci e8cec3dc9c6c..b466476f1b1b (1 commits)
    e276a7bbb7 Roll infra/go/src/go.chromium.org/luci 1f8feca32c60..e8cec3dc9c6c (1 commits)
    6635684c24 Roll infra/go/src/go.chromium.org/luci 940aa4b93ed6..1f8feca32c60 (1 commits)
    cd2ec62aa5 Roll infra/go/src/go.chromium.org/luci 50a525026835..940aa4b93ed6 (1 commits)
    3178cf5c46 Roll infra/go/src/go.chromium.org/luci 145e07071713..50a525026835 (1 commits)
    6eb5554a18 Roll infra/go/src/go.chromium.org/luci 5590202bedf1..145e07071713 (1 commits)
    ac1db05f1a Roll infra/go/src/go.chromium.org/luci 38db91022c52..5590202bedf1 (1 commits)
    
    Bug: 1076468
    Change-Id: Ieac829df658f38e5d05e50305396b72c310a32e0
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2190108
    Auto-Submit: Ye Kuang <yekuang@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>

--- client/run_isolated.py
+++ client/run_isolated.py
@@ -116,7 +116,7 @@ ISOLATED_CLIENT_DIR = u'ic'
 # Or take revision from
 # https://ci.chromium.org/p/infra-internal/g/infra-packagers/console
 ISOLATED_PACKAGE = 'infra/tools/luci/isolated/${platform}'
-ISOLATED_REVISION = 'git_revision:607e08228204224129d898082ac5b87dfc2f8e65'
+ISOLATED_REVISION = 'git_revision:f097334d4f2503ba7693d2cf068c958895282993'
 
 # Keep synced with task_request.py
 CACHE_NAME_RE = re.compile(r'^[a-z0-9_]{1,4096}$')
