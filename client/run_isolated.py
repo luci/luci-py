82754d612a372e8865c468c17cf4e49b63860642
Ye Kuang <yekuang@google.com>
2020-05-13 00:51:03 +0000

    client: update isolated client
    https://chromium.googlesource.com/infra/infra/+log/f097334d4f2503ba7693d2cf068c958895282993..85c15375298a6b56ff5347a929b84fda28497a01/DEPS
    
    85c1537529 Roll infra/go/src/go.chromium.org/luci 63a74cd4f664..d4bc79d85e4a (1 commits)
    05b5895bef Roll infra/go/src/go.chromium.org/luci 2bde1bd75383..63a74cd4f664 (1 commits)
    a34374cb70 Roll infra/go/src/go.chromium.org/luci 6544ee9df9fa..2bde1bd75383 (1 commits)
    5b53098fc3 Roll infra/go/src/go.chromium.org/luci 57d83f30e386..6544ee9df9fa (1 commits)
    54fae470a3 Roll infra/go/src/go.chromium.org/luci bdec0307469b..57d83f30e386 (1 commits)
    a1000a6f4b Roll infra/go/src/go.chromium.org/luci ec7b4aa1e2a3..bdec0307469b (1 commits)
    0832fde1be Roll infra/go/src/go.chromium.org/luci 41d8efcb517f..ec7b4aa1e2a3 (1 commits)
    1d2312ec2d Roll infra/go/src/go.chromium.org/luci e4468c71811e..41d8efcb517f (1 commits)
    2392405503 Roll infra/go/src/go.chromium.org/luci b8556869d4fb..e4468c71811e (1 commits)
    8eb8d7c13a Roll infra/go/src/go.chromium.org/luci 4748aed019ac..b8556869d4fb (1 commits)
    e6385e055e Roll infra/go/src/go.chromium.org/luci 30a8e14ed9ab..4748aed019ac (1 commits)
    b5ec326b6c Roll infra/go/src/go.chromium.org/luci df843057a87a..30a8e14ed9ab (1 commits)
    736e795833 Roll infra/go/src/go.chromium.org/luci 1719e4e808f9..df843057a87a (1 commits)
    fb6d30081c Roll infra/go/src/go.chromium.org/luci 86049c282195..1719e4e808f9 (1 commits)
    9e4b4fc989 Roll infra/go/src/go.chromium.org/luci 91cd7a152265..86049c282195 (1 commits)
    e46f63b317 Roll infra/go/src/go.chromium.org/luci 228893df7bed..91cd7a152265 (1 commits)
    e9d65fb3fe Roll infra/go/src/go.chromium.org/luci 4d2a6af1f9bb..228893df7bed (1 commits)
    
    Bug: 1080471
    Change-Id: Ib0a8b06d2b3d3005ad3abd3d844cc0eb64aad89b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2196012
    Commit-Queue: Ye Kuang <yekuang@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Ye Kuang <yekuang@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>

--- client/run_isolated.py
+++ client/run_isolated.py
@@ -116,7 +116,7 @@ ISOLATED_CLIENT_DIR = u'ic'
 # Or take revision from
 # https://ci.chromium.org/p/infra-internal/g/infra-packagers/console
 ISOLATED_PACKAGE = 'infra/tools/luci/isolated/${platform}'
-ISOLATED_REVISION = 'git_revision:f097334d4f2503ba7693d2cf068c958895282993'
+ISOLATED_REVISION = 'git_revision:85c15375298a6b56ff5347a929b84fda28497a01'
 
 # Keep synced with task_request.py
 CACHE_NAME_RE = re.compile(r'^[a-z0-9_]{1,4096}$')
