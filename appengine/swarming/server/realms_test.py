a0418596da55f915f7c1c6a0ed5c3d337299cf8d
Junji Watanabe <jwata@google.com>
2020-05-19 06:51:07 +0000

    [swarming] refactor mocking in realms_test.py
    derived from
    https://crrev.com/c/2208511/3/appengine/swarming/server/realms_test.py#90
    
    Bug: 1066839
    Change-Id: Ibf6f1cf007b9e393eb03c71f2a45e53da359c1b0
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2208514
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Auto-Submit: Junji Watanabe <jwata@google.com>

--- appengine/swarming/server/realms_test.py
+++ appengine/swarming/server/realms_test.py
@@ -30,7 +30,10 @@ class RealmsTest(test_case.TestCase):
 
   def setUp(self):
     super(RealmsTest, self).setUp()
-    utils.clear_cache(config.settings)
+    self._has_permission_mock = mock.Mock()
+    self._has_permission_dryrun_mock = mock.Mock()
+    self.mock(auth, 'has_permission', self._has_permission_mock)
+    self.mock(auth, 'has_permission_dryrun', self._has_permission_dryrun_mock)
 
   def tearDown(self):
     super(RealmsTest, self).tearDown()
@@ -73,81 +76,64 @@ class RealmsTest(test_case.TestCase):
     self.assertEqual(expected, realms.is_enforced_permission(
         realms_pb2.REALM_PERMISSION_POOLS_CREATE_TASK, pool_cfg))
 
-  def _mock_for_check_pools_create_task(self,
-                                        pool_realm='test:pool',
-                                        is_enforced=None,
-                                        is_allowed_legacy=None,
-                                        is_allowed_realms=None):
+  def _mock_for_check_pools_create_task_legacy(self,
+                                               is_allowed_legacy,
+                                               pool_realm='test:pool'):
+    self.mock(realms, 'is_enforced_permission', lambda *_: False)
     self.mock(pools_config,
               'get_pool_config', lambda _: pools_pb2.Pool(realm=pool_realm))
-    self.mock(realms, 'is_enforced_permission', lambda *_: is_enforced)
     self.mock(task_scheduler,
               '_is_allowed_to_schedule', lambda _: is_allowed_legacy)
-    if is_enforced:
-      has_permission_mock = mock.Mock(return_value=is_allowed_realms)
-      self.mock(auth, 'has_permission', has_permission_mock)
-    else:
-      has_permission_mock = mock.Mock()
-      self.mock(auth, 'has_permission_dryrun', has_permission_mock)
-    return has_permission_mock
+
+  def _mock_for_check_pools_create_task(self, pool_realm='test:pool'):
+    self.mock(realms, 'is_enforced_permission', lambda *_: True)
+    self.mock(pools_config,
+              'get_pool_config', lambda _: pools_pb2.Pool(realm=pool_realm))
 
   def test_check_pools_create_task_legacy_allowed(self):
-    has_permission_mock = self._mock_for_check_pools_create_task(
-        is_enforced=False, is_allowed_legacy=True)
-
+    self._mock_for_check_pools_create_task_legacy(is_allowed_legacy=True)
     self.assertIsNone(realms.check_pools_create_task('test_pool'))
-
-    has_permission_mock.assert_called_once_with(
+    self._has_permission_dryrun_mock.assert_called_once_with(
         'swarming.pools.createTask', [u'test:pool'],
         True,
         tracking_bug='crbug.com/1066839')
 
   def test_check_pools_create_task_legacy_allowed_no_pool_realm(self):
-    has_permission_mock = self._mock_for_check_pools_create_task(
-        pool_realm=None, is_enforced=False, is_allowed_legacy=True)
-
+    self._mock_for_check_pools_create_task_legacy(
+        is_allowed_legacy=True, pool_realm=None)
     self.assertIsNone(realms.check_pools_create_task('test_pool'))
-
-    has_permission_mock.assert_not_called()
+    self._has_permission_dryrun_mock.assert_not_called()
 
   def test_check_pools_create_task_legacy_not_allowed(self):
-    has_permission_mock = self._mock_for_check_pools_create_task(
-        is_enforced=False, is_allowed_legacy=False)
-
+    self._mock_for_check_pools_create_task_legacy(is_allowed_legacy=False)
     with self.assertRaises(auth.AuthorizationError):
       realms.check_pools_create_task('test_pool')
-
-    has_permission_mock.assert_called_once_with(
+    self._has_permission_dryrun_mock.assert_called_once_with(
         'swarming.pools.createTask', [u'test:pool'],
         False,
         tracking_bug='crbug.com/1066839')
 
   def test_check_pools_create_task_legacy_not_allowed_no_pool_realm(self):
-    has_permission_mock = self._mock_for_check_pools_create_task(
-        pool_realm=None, is_enforced=False, is_allowed_legacy=False)
-
+    self._mock_for_check_pools_create_task_legacy(
+        is_allowed_legacy=False, pool_realm=None)
     with self.assertRaises(auth.AuthorizationError):
       realms.check_pools_create_task('test_pool')
-
-    has_permission_mock.assert_not_called()
+    self._has_permission_dryrun_mock.assert_not_called()
 
   def test_check_pools_create_task_enforced_allowed(self):
-    has_permission_mock = self._mock_for_check_pools_create_task(
-        is_enforced=True, is_allowed_realms=True)
-
+    self._mock_for_check_pools_create_task()
+    self._has_permission_mock.return_value = True
     self.assertIsNone(realms.check_pools_create_task('test_pool'))
-
-    has_permission_mock.assert_called_once_with('swarming.pools.createTask',
-                                                [u'test:pool'])
+    self._has_permission_mock.assert_called_once_with(
+        'swarming.pools.createTask', [u'test:pool'])
 
   def test_check_pools_create_task_enforced_not_allowed(self):
-    has_permission_mock = self._mock_for_check_pools_create_task(
-        is_enforced=True, is_allowed_realms=False)
-
+    self._mock_for_check_pools_create_task()
+    self._has_permission_mock.return_value = False
     with self.assertRaises(auth.AuthorizationError):
       realms.check_pools_create_task('test_pool')
-    has_permission_mock.assert_called_once_with('swarming.pools.createTask',
-                                                [u'test:pool'])
+    self._has_permission_mock.assert_called_once_with(
+        'swarming.pools.createTask', [u'test:pool'])
 
 
 if __name__ == '__main__':
