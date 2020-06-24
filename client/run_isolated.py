801e681351f8083df15f28516a8343136bbf796c
Takuto Ikuta <tikuta@chromium.org>
2020-06-24 07:43:05 +0000

    client: update isolated client
    https://chromium.googlesource.com/infra/infra/+log/2a7df3559f991935cd89b0ed040531476a976467..58b9c29f4c1aedddc1da81481be3a5e08b18234e/DEPS
    
        2020-06-24 weiweilin@google.com [resultui] increase the height of variant entry headers
        2020-06-24 tandrii@google.com ttq: set up structure and bare bones public interface.
        2020-06-24 nqmtuan@google.com [Milo] Return 404 instead of 500 when Not Found
        2020-06-24 tikuta@chromium.org cache: implement workaround for access denied error
        2020-06-24 yekuang@google.com Fix error message when Hardlink fails
        2020-06-24 weiweilin@google.com [milo] add link to test results page
        2020-06-24 tandrii@google.com milo: switch to faster binary encoding for Buildbucket responses.
        2020-06-24 iannucci@chromium.org Rename common/proto/milo.Link to avoid conflicts.
        2020-06-24 yiwzhang@google.com Regenerate expectation due to protoc-gen-go upgrade
        2020-06-23 weiweilin@google.com [resultui] add feature description and Q&A
        2020-06-23 weiweilin@google.com [resultui] add image-diff artifact entry
        2020-06-23 weiweilin@google.com [resultui] add standalone image-diff artifact page
    
    This commit is generated by update_isolated.sh.
    
    Change-Id: I5e563417fd7e6275a8fcdea690be0f4f5ef9fd07
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2262084
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Ye Kuang <yekuang@google.com>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Ye Kuang <yekuang@google.com>

--- client/run_isolated.py
+++ client/run_isolated.py
@@ -116,7 +116,7 @@ ISOLATED_CLIENT_DIR = u'ic'
 # Or take revision from
 # https://ci.chromium.org/p/infra-internal/g/infra-packagers/console
 ISOLATED_PACKAGE = 'infra/tools/luci/isolated/${platform}'
-ISOLATED_REVISION = 'git_revision:2a7df3559f991935cd89b0ed040531476a976467'
+ISOLATED_REVISION = 'git_revision:58b9c29f4c1aedddc1da81481be3a5e08b18234e'
 
 # Keep synced with task_request.py
 CACHE_NAME_RE = re.compile(r'^[a-z0-9_]{1,4096}$')
