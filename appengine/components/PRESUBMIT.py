907ad5b166603eb480a039ba49cbcf6c2e46ec8f
Junji Watanabe <jwata@google.com>
2020-07-01 06:15:17 +0000

    [components] remove unit test check from PRESUBMIT.py
    `luci-py-try-xenial-64` runs unit tests in components.
    https://logs.chromium.org/logs/infra/buildbucket/cr-buildbucket.appspot.com/8876001145356115872/+/u/components/run_tests_python2/stdout
    
    See components/test.py for the test runner setup.
    
    Change-Id: I2f19e27b954de5a6700d9e8bcb51eaa8399462d0
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2277542
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Junji Watanabe <jwata@google.com>

--- appengine/components/PRESUBMIT.py
+++ appengine/components/PRESUBMIT.py
@@ -10,10 +10,6 @@ details on the presubmit API built into gclient.
 
 
 def CommonChecks(input_api, output_api):
-  output = []
-  def join(*args):
-    return input_api.os_path.join(input_api.PresubmitLocalPath(), *args)
-
   block_list = list(input_api.DEFAULT_BLOCK_LIST) + [
     r'.*_pb2\.py$',
   ]
@@ -23,35 +19,11 @@ def CommonChecks(input_api, output_api):
     'cyclic-import',
     'relative-import',
   ]
-  output.extend(input_api.canned_checks.RunPylint(
-      input_api, output_api,
+  return input_api.canned_checks.RunPylint(
+      input_api,
+      output_api,
       block_list=block_list,
-      disabled_warnings=disabled_warnings))
-
-  test_directories = [
-    join('components'),
-    join('components', 'auth'),
-    join('components', 'auth', 'ui'),
-    join('components', 'config'),
-    join('components', 'datastore_utils'),
-    join('components', 'endpoints_webapp2'),
-    join('components', 'ereporter2'),
-    join('components', 'protoutil'),
-    join('components', 'prpc'),
-    join('components', 'prpc', 'discovery'),
-    join('components', 'stats_framework'),
-    join('tests'),
-  ]
-  tests = []
-  for directory in test_directories:
-    tests.extend(
-        input_api.canned_checks.GetUnitTestsInDirectory(
-            input_api, output_api,
-            directory,
-            whitelist=[r'.+_test\.py$'],
-            blacklist=[]))
-  output.extend(input_api.RunTests(tests, parallel=True))
-  return output
+      disabled_warnings=disabled_warnings)
 
 
 # pylint: disable=unused-argument
