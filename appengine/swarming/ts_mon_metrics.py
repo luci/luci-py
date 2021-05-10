e4cfa81283ca3b5fcaff6cd796b29762cfa43e39
Junji Watanabe <jwata@google.com>
2021-05-10 02:29:12 +0000

    Revert "swarming: remove invalid priority check"
    This reverts commit f9496155a375d9cf1ed7d453e924dd12a4388795.
    
    Reason for revert: the error started happening for a deployment issue.  https://console.cloud.google.com/errors/CLig9ffo-IOCgQE?time=P30D&project=chromium-swarm-dev
    
    
    Original change's description:
    > swarming: remove invalid priority check
    >
    > This shouldn't be necessary now.
    >
    > Fixed: 1200915
    > Change-Id: I2be3669d7750b39dabff6da06a48d8ed79a5cb12
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2874820
    > Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    > Commit-Queue: Junji Watanabe <jwata@google.com>
    > Reviewed-by: Junji Watanabe <jwata@google.com>
    
    Change-Id: I709101b33b39a6db6d4a518931a7a951ef67b783
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2882237
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>

--- appengine/swarming/ts_mon_metrics.py
+++ appengine/swarming/ts_mon_metrics.py
@@ -463,7 +463,11 @@ def on_task_completed(summary):
   """When a task is stopped from being processed."""
   fields = _extract_job_fields(_tags_to_dict(summary.tags))
   if summary.state == task_result.State.EXPIRED:
-    fields['priority'] = summary.priority
+    if summary.priority is None:
+      # TODO(crbug.com/1200915): remove this after 7 days from deploy.
+      fields['priority'] = -1
+    else:
+      fields['priority'] = summary.priority
     _tasks_expired.increment(fields=fields)
     return
 
