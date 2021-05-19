57adc459ed160ee803506eb640a0353b0391dabe
Takuto Ikuta <tikuta@chromium.org>
2021-05-19 04:14:18 +0000

    client: update go clients
    https://chromium.googlesource.com/infra/infra/+log/e305f03e52874471d5627b43c6344b2215698f8a..debf803364166dda805c12f69b60ba980858d73b/DEPS
    
        2021-05-19 weiweilin@google.com [resultui] display duration since build start in the timeline tab
        2021-05-19 weiweilin@google.com [resultui] display build start/end time in the timeline tab
        2021-05-19 weiweilin@google.com [resultui] Enable source map in prod.
        2021-05-19 weiweilin@google.com [resultui] update import statements to improve tree shaking
        2021-05-19 weiweilin@google.com [resultui] add tests for the new timeline tab
        2021-05-19 weiweilin@google.com [resultui] add loading indicator for the new timeline tab
        2021-05-19 weiweilin@google.com [resultui] implement dynamic time interval
        2021-05-19 weiweilin@google.com [resultui] implement the timeline for the new timeline tab
        2021-05-19 weiweilin@google.com [resultui] implement side-panel for the new timeline tab
        2021-05-19 tikuta@chromium.org cas: log profiler dump time
        2021-05-19 weiweilin@google.com [resultui] setup grid lines and borders for the new timeline tab
        2021-05-19 weiweilin@google.com [resultui] implement axes for the new timeline tab
        2021-05-19 yiwzhang@google.com cv: pass tree client dependency explicitly instead of putting into context
        2021-05-19 weiweilin@google.com [resultui] setup basic layout for the new timeline tab
        2021-05-19 weiweilin@google.com [resultui] add step index to StepExt
        2021-05-18 yiwzhang@google.com cv: implement onSubmissionCompleted handler
        2021-05-18 yiwzhang@google.com cv: move acquiring submit queue and tree check logic to OnReadyForSubmission
        2021-05-18 yiwzhang@google.com cv: make not holding submit queue a permanent failure
        2021-05-18 qyearsley@chromium.org cv: Followup to CL group key for BQ export
        2021-05-18 yiwzhang@google.com cv: remove tree check in submitter
        2021-05-18 sdefresne@chromium.org [prpc] Expose `-use-id-token` and `audience` flags
        2021-05-18 tikuta@chromium.org cas: fix flag name in error
        2021-05-18 momosasaki@google.com Rename 'deleteBots' to 'deleteBot'
    
    This commit is generated by update_go_clients.sh.
    
    Bug: 1172879
    Change-Id: Idd3129a56ad772413630a80da2a07a0410b781e6
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2903476
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Junji Watanabe <jwata@google.com>

--- client/run_isolated.py
+++ client/run_isolated.py
@@ -115,7 +115,7 @@ _CAS_CLIENT_DIR = u'cc'
 # https://ci.chromium.org/p/infra-internal/g/infra-packagers/console
 ISOLATED_PACKAGE = 'infra/tools/luci/isolated/${platform}'
 _CAS_PACKAGE = 'infra/tools/luci/cas/${platform}'
-_LUCI_GO_REVISION = 'git_revision:e305f03e52874471d5627b43c6344b2215698f8a'
+_LUCI_GO_REVISION = 'git_revision:debf803364166dda805c12f69b60ba980858d73b'
 
 # Keep synced with task_request.py
 CACHE_NAME_RE = re.compile(r'^[a-z0-9_]{1,4096}$')
