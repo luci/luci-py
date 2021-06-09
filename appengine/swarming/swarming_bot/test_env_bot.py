d0d9e6bcd6eda5a96fe4470af9272d1c55880bf5
Momo Sasaki <momosasaki@google.com>
2021-06-09 08:29:28 +0000

    [swarming] exit program if symlink is disabled
    Support for symbolic links may be disabled in git for Windows.
    Therefore exit the program and give a error message if symbolic link is disabled.
    
    Bug: 1203081, 1017545
    Change-Id: I57e05f6139be6c8814304829488eb93b409c4f39
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2853853
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Momo Sasaki <momosasaki@google.com>

--- appengine/swarming/swarming_bot/test_env_bot.py
+++ appengine/swarming/swarming_bot/test_env_bot.py
@@ -10,32 +10,8 @@ import sys
 BOT_DIR = os.path.dirname(os.path.realpath(os.path.abspath(__file__)))
 
 
-def init_symlinks(root):
-  """Adds support for symlink-as-file on Windows.
-
-  Manually resolves symlinks in path for directory and add them to sys.path.
-  """
-  if sys.platform != 'win32':
-    return
-  for i in os.listdir(root):
-    if '.' in i:
-      continue
-    path = os.path.join(root, i)
-    if os.path.isfile(path):
-      # Found a file instead of a symlink to a directory. Adjust sys.path
-      # accordingly to where the symlink points.
-      with open(path) as f:
-        link = f.read()
-      if '\n' in link:
-        continue
-      dest = os.path.normpath(os.path.join(root, link))
-      # This is not exactly right but close enough.
-      sys.path.insert(0, os.path.dirname(dest))
-
-
 def setup_test_env():
   """Sets up the environment for bot tests."""
-  init_symlinks(BOT_DIR)
   client = os.path.normpath(os.path.join(BOT_DIR, '..', '..', '..', 'client'))
   client_tests = os.path.join(client, 'tests')
   sys.path.insert(0, client_tests)
@@ -44,8 +20,11 @@ def setup_test_env():
   if os.path.isfile(tp):
     # check if third_party is symlink or regular existing file
     # because support for symlink in git for Windows may be disabled
-    with open(tp, 'r') as f:
-      tp = os.path.join(BOT_DIR, f.read())
+    sys.stderr.write(
+      'Please enable symlink. '
+      'Run \'git config --global core.symlinks true\' '
+      'and fetch the repository again')
+    exit(1)
   sys.path.insert(0, tp)
 
   # libusb1 expects to be directly in sys.path.
