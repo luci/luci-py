80f50430adbfc09aeb1703ba8a97439dead6edee
Takuto Ikuta <tikuta@chromium.org>
2021-10-28 09:22:41 +0000

    swarming: remove init_symlinks
    This is not necessary anymore as we enabled symlink on Windows bots.
    
    Fixed: 1018060
    Change-Id: I1b61b29165148b68d756ed94ec95898f006994c4
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/3250134
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Reviewed-by: Junji Watanabe <jwata@google.com>

--- appengine/swarming/swarming_test_env.py
+++ appengine/swarming/swarming_test_env.py
@@ -9,34 +9,10 @@ import sys
 APP_DIR = os.path.dirname(os.path.realpath(os.path.abspath(__file__)))
 
 
-def init_symlinks(root):
-  """Adds support for symlink-as-file on Windows.
-
-  Manually resolves symlinks in path for directory and add them to sys.path.
-  """
-  if sys.platform != 'win32':
-    return
-  for i in os.listdir(root):
-    if '.' in i:
-      continue
-    path = os.path.join(root, i)
-    if os.path.isfile(path):
-      # Found a file instead of a symlink to a directory. Adjust sys.path
-      # accordingly to where the symlink points.
-      with open(path) as f:
-        link = f.read()
-      if '\n' in link:
-        continue
-      dest = os.path.normpath(os.path.join(root, link))
-      # This is not exactly right but close enough.
-      sys.path.insert(0, os.path.dirname(dest))
-
-
 def setup_test_env():
   """Sets up App Engine test environment."""
   # For application modules.
   sys.path.insert(0, APP_DIR)
-  init_symlinks(APP_DIR)
   # TODO(maruel): Remove.
   sys.path.insert(0, os.path.join(APP_DIR, 'components', 'third_party'))
 
