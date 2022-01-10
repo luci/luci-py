fd0bb126c4accc4babaa35eed0184259db5e3d6d
Vadim Shtayura <vadimsh@chromium.org>
2022-01-10 21:58:25 +0000

    [realms] Support conditions when merging realms_pb2.Realms.
    This concludes chrome-infra-auth portion of this feature. It now
    produces AuthDB snapshots with conditional bindings.
    
    R=iannucci@chromium.org
    BUG=1280442
    
    Change-Id: I066f1ec03843b0d5f0b96c18447d4be4530c050c
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/3352667
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

--- appengine/components/components/auth/realms.py
+++ appengine/components/components/auth/realms.py
@@ -33,18 +33,29 @@ def merge(permissions, realms, out=None):
   out.api_version = API_VERSION
   out.permissions.extend(permissions)
 
+  # Sorted list of pairs (project_id, realms_pb2.Realms), we'll visit it twice.
+  sorted_realms = sorted(realms.items())
+
+  # Merge the set of all conditions across all projects.
+  conds = ConditionsSet()
+  for _, proj_realms in sorted_realms:
+    for cond in proj_realms.conditions:
+      conds.add(cond)
+  out.conditions.extend(conds.flat)
+
   # Permission name => its index in the merged realms_pb2.Realms.
   perm_index = {p.name: idx for idx, p in enumerate(permissions)}
 
   # Visit in order of project IDs.
-  for proj_id, proj_realms in sorted(realms.items()):
+  for proj_id, proj_realms in sorted_realms:
     # Calculate a mapping from the permission index in `proj_realms` to
     # the index in the final merged proto (or None if undefined).
     old_to_new = [perm_index.get(p.name) for p in proj_realms.permissions]
 
     # Visit all bindings in all realms.
     for old_realm in proj_realms.realms:
-      # Relabel permission indexes, drop empty bindings that may appear.
+      # Relabel permission and condition indexes, drop empty bindings that may
+      # appear due to unknown permissions.
       bindings = []
       for b in old_realm.bindings:
         perms = sorted(
@@ -53,17 +64,52 @@ def merge(permissions, realms, out=None):
             if old_to_new[idx] is not None
         )
         if perms:
-          bindings.append((perms, b.principals))
+          bindings.append((
+              perms,
+              conds.relabel(proj_realms.conditions, b.conditions),
+              b.principals,
+          ))
 
       # Add the relabeled realm to the output.
       assert old_realm.name.startswith(proj_id+':'), old_realm.name
       new_realm = out.realms.add()
       new_realm.name = old_realm.name
       new_realm.bindings.extend(
-          realms_pb2.Binding(permissions=perms, principals=principals)
-          for perms, principals in sorted(bindings, key=lambda x: x[0])
+          realms_pb2.Binding(
+              permissions=perms,
+              principals=principals,
+              conditions=conds,
+          )
+          for perms, conds, principals in sorted(bindings)
       )
       if old_realm.HasField('data'):
         new_realm.data.CopyFrom(old_realm.data)
 
   return out
+
+
+class ConditionsSet(object):
+  """Dedups identical conditions, maps them to integer indexes.
+
+  Most often identical conditions appear from implicit root bindings that are
+  similar across all projects.
+
+  Assumes all incoming realms_pb2.Condition are immutable and already
+  normalized. Retains the order in which they were added.
+  """
+
+  def __init__(self):
+    self.flat = []  # the final list of dedupped realms_pb2.Condition
+    self._mapping = {}  # serialized realms_pb2.Condition => its index
+    self._id_mapping = {}  # id(realms_pb2.Condition) => its index
+    self._retain = []  # list of all conditions ever passed to `add`
+
+  def add(self, cond):
+    idx = self._mapping.setdefault(cond.SerializeToString(), len(self.flat))
+    if idx == len(self.flat):
+      self.flat.append(cond)
+    self._id_mapping[id(cond)] = idx
+    self._retain.append(cond)  # keep the pointer alive to pin id(cond)
+
+  def relabel(self, conds, indexes):
+    return sorted(self._id_mapping[id(conds[idx])] for idx in indexes)
--- appengine/components/components/auth/realms_test.py
+++ appengine/components/components/auth/realms_test.py
@@ -18,6 +18,17 @@ def perms(*names):
   return [realms_pb2.Permission(name=name) for name in names]
 
 
+def conds(*names):
+  return [
+      realms_pb2.Condition(
+          restrict=realms_pb2.Condition.AttributeRestriction(
+              attribute=name,
+              values=['x', 'y', 'z'],
+          ),
+      ) for name in names
+  ]
+
+
 class MergeTest(test_case.TestCase):
   def test_empty(self):
     self.assertEqual(
@@ -66,6 +77,92 @@ class MergeTest(test_case.TestCase):
                     {
                         'permissions': [0, 1],  # p1, p2
                         'principals': ['group:gr1'],
+                    },
+                ],
+            },
+        ],
+    ))
+
+  def test_remaps_conditions(self):
+    out = realms.merge(
+        perms('luci.dev.p1'),
+        {
+            'proj1': realms_pb2.Realms(
+                permissions=perms('luci.dev.p1'),
+                conditions=conds('a', 'b'),
+                realms=[
+                    {
+                        'name': 'proj1:@root',
+                        'bindings': [
+                            {
+                                'permissions': [0],
+                                'conditions': [0, 1],
+                                'principals': ['group:gr1'],
+                            },
+                        ],
+                    },
+                ],
+            ),
+            'proj2': realms_pb2.Realms(
+                permissions=perms('luci.dev.p1'),
+                conditions=conds('c', 'a', 'b'),
+                realms=[
+                    {
+                        'name': 'proj2:@root',
+                        'bindings': [
+                            {
+                                'permissions': [0],
+                                'conditions': [0],  # c
+                                'principals': ['group:gr2'],
+                            },
+                            {
+                                'permissions': [0],
+                                'conditions': [1],  # a
+                                'principals': ['group:gr3'],
+                            },
+                            {
+                                'permissions': [0],
+                                'conditions': [0, 1, 2],  # c a b
+                                'principals': ['group:gr4'],
+                            },
+                        ],
+                    },
+                ],
+            ),
+        },
+    )
+    self.assertEqual(out, realms_pb2.Realms(
+        api_version=realms.API_VERSION,
+        permissions=perms('luci.dev.p1'),
+        conditions=conds('a', 'b', 'c'),
+        realms=[
+            {
+                'name': 'proj1:@root',
+                'bindings': [
+                    {
+                        'permissions': [0],
+                        'conditions': [0, 1],
+                        'principals': ['group:gr1'],
+                    },
+                ],
+            },
+            {
+                'name': 'proj2:@root',
+                'bindings': [
+                    {
+                        'permissions': [0],
+                        'conditions': [0],  # a
+                        'principals': ['group:gr3'],
+                    },
+                    {
+                        'permissions': [0],
+                        'conditions': [0, 1, 2],  # a b c == c a b
+                        'principals': ['group:gr4'],
+                    },
+                    {
+                        'permissions': [0],
+                        'conditions': [2],  # c
+                        'principals': ['group:gr2'],
                     },
                 ],
             },
