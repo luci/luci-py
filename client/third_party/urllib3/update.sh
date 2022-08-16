a5c33d9bca35dfd633c496c85d210090a5478f9e
Rahul Bangar <rahulbn@google.com>
2022-08-16 22:34:41 +0000

    Ignore AttributeError when enabling post-handshake authentication
    This allows Python urllib3 code to work with BoringSSL.
    
    Change-Id: I472c24f342486048c5f2114bf16c7895fec2ea81
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/3828138
    Auto-Submit: Rahul Bangar <rahulbn@google.com>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Rahul Bangar <rahulbn@google.com>

--- client/third_party/urllib3/update.sh
+++ client/third_party/urllib3/update.sh
@@ -14,6 +14,8 @@ fi
 version="${1}"
 
 find . | fgrep '/' | fgrep -v './update.sh' | fgrep -v 'README.swarming' | \
-    sort -r | xargs rm -r
+    fgrep -v './post_handshake_auth.patch' | sort -r | xargs rm -r
 curl -sL https://github.com/urllib3/urllib3/archive/${version}.tar.gz | \
     tar xvz --strip-components 3 urllib3-${version}/src/urllib3
+
+patch -p4 < ./post_handshake_auth.patch
--- client/third_party/urllib3/util/ssl_.py
+++ client/third_party/urllib3/util/ssl_.py
@@ -281,7 +281,12 @@ def create_urllib3_context(
     if (cert_reqs == ssl.CERT_REQUIRED or sys.version_info >= (3, 7, 4)) and getattr(
         context, "post_handshake_auth", None
     ) is not None:
-        context.post_handshake_auth = True
+        try:
+            # TODO(b/117801554): in google3 this attribute is read-only
+            # remove this patch once BoringSSL supports post_handshake
+            context.post_handshake_auth = True
+        except AttributeError:
+            pass
 
     context.verify_mode = cert_reqs
     if (
