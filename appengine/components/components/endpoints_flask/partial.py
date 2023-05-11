cfb2b6db8fbc2d5816e3a0a30ad98e605c98630d
Dave Tu <dtu@google.com>
2023-05-11 17:24:04 +0000

    [endpoints_flask] Use raw string for docstring literal.
    Change-Id: I24e3ffb81b34481e20bf875d586745068d4fe6b7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/4522051
    Auto-Submit: Dave Tu <dtu@chromium.org>
    Reviewed-by: Andrew Chang <andrewjc@google.com>
    Commit-Queue: Andrew Chang <andrewjc@google.com>

--- appengine/components/components/endpoints_flask/partial.py
+++ appengine/components/components/endpoints_flask/partial.py
@@ -1,7 +1,7 @@
 # Copyright 2018 The LUCI Authors. All rights reserved.
 # Use of this source code is governed under the Apache License, Version 2.0
 # that can be found in the LICENSE file.
-"""Partial response utilities for an Endpoints v1 over webapp2 service.
+r"""Partial response utilities for an Endpoints v1 over webapp2 service.
 
 Grammar of a fields partial response string:
    fields: selector [,selector]*
