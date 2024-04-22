4e4e527cb508f52c6353833b94fe2809faed7250
Anne Redulla <aredulla@google.com>
2024-04-22 04:05:37 +0000

    [auth-service] Switch to Auth Service v2 for changelog generation
    Bug: b:321019030
    
    Change-Id: Ic874b6f0e4f58b6539493853c8c70fe0805e0b3e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/5464726
    Commit-Queue: Anne Redulla <aredulla@google.com>
    Reviewed-by: Joey Scarr <jsca@google.com>

--- appengine/auth_service/appengine_config.py
+++ appengine/auth_service/appengine_config.py
@@ -7,13 +7,21 @@
 https://developers.google.com/appengine/docs/python/tools/appengineconfig
 """
 
+from components import utils
+
+utils.import_third_party()
+utils.fix_protobuf_package()
+
+import change_log
+
 # Auth component UI is tweaked manually, see handlers_frontend.py.
 components_auth_UI_CUSTOM_CONFIG = True
+
+# Use a custom change task enqueuer so changelog generation is handled by
+# Auth Service v2.
+components_auth_CUSTOM_PROCESS_CHANGE_TASK_ENQUEUER = \
+    change_log.enqueue_v2_changelog_task
 
 # Use backend module and dedicated task queue for change log generation.
 components_auth_BACKEND_MODULE = 'backend'
 components_auth_PROCESS_CHANGE_TASK_QUEUE = 'process-auth-db-change'
-
-from components import utils
-utils.import_third_party()
-utils.fix_protobuf_package()
