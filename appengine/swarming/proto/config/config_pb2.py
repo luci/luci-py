295adfa0c7da5643c8503a20b5c25ffd6b10959b
Vadim Shtayura <vadimsh@chromium.org>
2024-06-06 01:32:23 +0000

    [swarming] Add config for Python => Go RPC traffic migration.
    It will be consumed by the Go portion. The Go portion will
    be responsible for either executing requests itself, or
    proxying them to the Python version, based on this config.
    
    R=chanli@chromium.org
    
    Change-Id: I1b1ea7053b944e3059fc9a883724fa3db3215135
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/5599082
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Chan Li <chanli@chromium.org>

--- appengine/swarming/proto/config/config_pb2.py
+++ appengine/swarming/proto/config/config_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'Z3go.chromium.org/luci/swarming/proto/config;configpb',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x19proto/config/config.proto\x12\x0fswarming.config\x1a\x19proto/config/realms.proto\"\xe5\x04\n\x0bSettingsCfg\x12\x18\n\x10google_analytics\x18\x01 \x01(\t\x12\x1e\n\x16reusable_task_age_secs\x18\x02 \x01(\x05\x12\x1e\n\x16\x62ot_death_timeout_secs\x18\x03 \x01(\x05\x12\x1c\n\x14\x65nable_ts_monitoring\x18\x04 \x01(\x08\x12+\n\x04\x63ipd\x18\x06 \x01(\x0b\x32\x1d.swarming.config.CipdSettings\x12,\n$force_bots_to_sleep_and_not_run_task\x18\x08 \x01(\x08\x12\x14\n\x0cui_client_id\x18\t \x01(\t\x12#\n\x1b\x64isplay_server_url_template\x18\x0b \x01(\t\x12\x1a\n\x12max_bot_sleep_time\x18\x0c \x01(\x05\x12+\n\x04\x61uth\x18\r \x01(\x0b\x32\x1d.swarming.config.AuthSettings\x12\x1e\n\x16\x62ot_isolate_grpc_proxy\x18\x0e \x01(\t\x12\x1f\n\x17\x62ot_swarming_grpc_proxy\x18\x0f \x01(\t\x12\x1f\n\x17\x65xtra_child_src_csp_url\x18\x10 \x03(\t\x12%\n\x1d\x65nable_batch_es_notifications\x18\x12 \x01(\x08\x12\x33\n\x08resultdb\x18\x13 \x01(\x0b\x32!.swarming.config.ResultDBSettings\x12)\n\x03\x63\x61s\x18\x14 \x01(\x0b\x32\x1c.swarming.config.CASSettingsJ\x04\x08\x05\x10\x06J\x04\x08\x07\x10\x08J\x04\x08\n\x10\x0bJ\x04\x08\x11\x10\x12\"4\n\x0b\x43ipdPackage\x12\x14\n\x0cpackage_name\x18\x01 \x01(\t\x12\x0f\n\x07version\x18\x02 \x01(\t\"d\n\x0c\x43ipdSettings\x12\x16\n\x0e\x64\x65\x66\x61ult_server\x18\x01 \x01(\t\x12<\n\x16\x64\x65\x66\x61ult_client_package\x18\x02 \x01(\x0b\x32\x1c.swarming.config.CipdPackage\"\xf7\x01\n\x0c\x41uthSettings\x12\x14\n\x0c\x61\x64mins_group\x18\x01 \x01(\t\x12\x1b\n\x13\x62ot_bootstrap_group\x18\x02 \x01(\t\x12\x1e\n\x16privileged_users_group\x18\x03 \x01(\t\x12\x13\n\x0busers_group\x18\x04 \x01(\t\x12\x1b\n\x13view_all_bots_group\x18\x05 \x01(\t\x12\x1c\n\x14view_all_tasks_group\x18\x06 \x01(\t\x12\x44\n\x1a\x65nforced_realm_permissions\x18\x07 \x03(\x0e\x32 .swarming.config.RealmPermission\"\"\n\x10ResultDBSettings\x12\x0e\n\x06server\x18\x01 \x01(\t\"$\n\x0b\x43\x41SSettings\x12\x15\n\rviewer_server\x18\x01 \x01(\tB5Z3go.chromium.org/luci/swarming/proto/config;configpbb\x06proto3'
+  serialized_pb=b'\n\x19proto/config/config.proto\x12\x0fswarming.config\x1a\x19proto/config/realms.proto\"\xa3\x05\n\x0bSettingsCfg\x12\x18\n\x10google_analytics\x18\x01 \x01(\t\x12\x1e\n\x16reusable_task_age_secs\x18\x02 \x01(\x05\x12\x1e\n\x16\x62ot_death_timeout_secs\x18\x03 \x01(\x05\x12\x1c\n\x14\x65nable_ts_monitoring\x18\x04 \x01(\x08\x12+\n\x04\x63ipd\x18\x06 \x01(\x0b\x32\x1d.swarming.config.CipdSettings\x12,\n$force_bots_to_sleep_and_not_run_task\x18\x08 \x01(\x08\x12\x14\n\x0cui_client_id\x18\t \x01(\t\x12#\n\x1b\x64isplay_server_url_template\x18\x0b \x01(\t\x12\x1a\n\x12max_bot_sleep_time\x18\x0c \x01(\x05\x12+\n\x04\x61uth\x18\r \x01(\x0b\x32\x1d.swarming.config.AuthSettings\x12\x1e\n\x16\x62ot_isolate_grpc_proxy\x18\x0e \x01(\t\x12\x1f\n\x17\x62ot_swarming_grpc_proxy\x18\x0f \x01(\t\x12\x1f\n\x17\x65xtra_child_src_csp_url\x18\x10 \x03(\t\x12%\n\x1d\x65nable_batch_es_notifications\x18\x12 \x01(\x08\x12\x33\n\x08resultdb\x18\x13 \x01(\x0b\x32!.swarming.config.ResultDBSettings\x12)\n\x03\x63\x61s\x18\x14 \x01(\x0b\x32\x1c.swarming.config.CASSettings\x12<\n\x11traffic_migration\x18\x15 \x01(\x0b\x32!.swarming.config.TrafficMigrationJ\x04\x08\x05\x10\x06J\x04\x08\x07\x10\x08J\x04\x08\n\x10\x0bJ\x04\x08\x11\x10\x12\"4\n\x0b\x43ipdPackage\x12\x14\n\x0cpackage_name\x18\x01 \x01(\t\x12\x0f\n\x07version\x18\x02 \x01(\t\"d\n\x0c\x43ipdSettings\x12\x16\n\x0e\x64\x65\x66\x61ult_server\x18\x01 \x01(\t\x12<\n\x16\x64\x65\x66\x61ult_client_package\x18\x02 \x01(\x0b\x32\x1c.swarming.config.CipdPackage\"\xf7\x01\n\x0c\x41uthSettings\x12\x14\n\x0c\x61\x64mins_group\x18\x01 \x01(\t\x12\x1b\n\x13\x62ot_bootstrap_group\x18\x02 \x01(\t\x12\x1e\n\x16privileged_users_group\x18\x03 \x01(\t\x12\x13\n\x0busers_group\x18\x04 \x01(\t\x12\x1b\n\x13view_all_bots_group\x18\x05 \x01(\t\x12\x1c\n\x14view_all_tasks_group\x18\x06 \x01(\t\x12\x44\n\x1a\x65nforced_realm_permissions\x18\x07 \x03(\x0e\x32 .swarming.config.RealmPermission\"\"\n\x10ResultDBSettings\x12\x0e\n\x06server\x18\x01 \x01(\t\"$\n\x0b\x43\x41SSettings\x12\x15\n\rviewer_server\x18\x01 \x01(\t\"\x7f\n\x10TrafficMigration\x12\x37\n\x06routes\x18\x01 \x03(\x0b\x32\'.swarming.config.TrafficMigration.Route\x1a\x32\n\x05Route\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x1b\n\x13route_to_go_percent\x18\x02 \x01(\x05\x42\x35Z3go.chromium.org/luci/swarming/proto/config;configpbb\x06proto3'
   ,
   dependencies=[proto_dot_config_dot_realms__pb2.DESCRIPTOR,])
 
@@ -147,6 +147,13 @@ _SETTINGSCFG = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='traffic_migration', full_name='swarming.config.SettingsCfg.traffic_migration', index=16,
+      number=21, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -160,7 +167,7 @@ _SETTINGSCFG = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=74,
-  serialized_end=687,
+  serialized_end=749,
 )
 
 
@@ -198,8 +205,8 @@ _CIPDPACKAGE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=689,
-  serialized_end=741,
+  serialized_start=751,
+  serialized_end=803,
 )
 
 
@@ -237,8 +244,8 @@ _CIPDSETTINGS = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=743,
-  serialized_end=843,
+  serialized_start=805,
+  serialized_end=905,
 )
 
 
@@ -311,8 +318,8 @@ _AUTHSETTINGS = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=846,
-  serialized_end=1093,
+  serialized_start=908,
+  serialized_end=1155,
 )
 
 
@@ -343,8 +350,8 @@ _RESULTDBSETTINGS = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1095,
-  serialized_end=1129,
+  serialized_start=1157,
+  serialized_end=1191,
 )
 
 
@@ -375,22 +382,96 @@ _CASSETTINGS = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1131,
-  serialized_end=1167,
+  serialized_start=1193,
+  serialized_end=1229,
+)
+
+
+_TRAFFICMIGRATION_ROUTE = _descriptor.Descriptor(
+  name='Route',
+  full_name='swarming.config.TrafficMigration.Route',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='name', full_name='swarming.config.TrafficMigration.Route.name', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='route_to_go_percent', full_name='swarming.config.TrafficMigration.Route.route_to_go_percent', index=1,
+      number=2, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=1308,
+  serialized_end=1358,
+)
+
+_TRAFFICMIGRATION = _descriptor.Descriptor(
+  name='TrafficMigration',
+  full_name='swarming.config.TrafficMigration',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='routes', full_name='swarming.config.TrafficMigration.routes', index=0,
+      number=1, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[_TRAFFICMIGRATION_ROUTE, ],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=1231,
+  serialized_end=1358,
 )
 
 _SETTINGSCFG.fields_by_name['cipd'].message_type = _CIPDSETTINGS
 _SETTINGSCFG.fields_by_name['auth'].message_type = _AUTHSETTINGS
 _SETTINGSCFG.fields_by_name['resultdb'].message_type = _RESULTDBSETTINGS
 _SETTINGSCFG.fields_by_name['cas'].message_type = _CASSETTINGS
+_SETTINGSCFG.fields_by_name['traffic_migration'].message_type = _TRAFFICMIGRATION
 _CIPDSETTINGS.fields_by_name['default_client_package'].message_type = _CIPDPACKAGE
 _AUTHSETTINGS.fields_by_name['enforced_realm_permissions'].enum_type = proto_dot_config_dot_realms__pb2._REALMPERMISSION
+_TRAFFICMIGRATION_ROUTE.containing_type = _TRAFFICMIGRATION
+_TRAFFICMIGRATION.fields_by_name['routes'].message_type = _TRAFFICMIGRATION_ROUTE
 DESCRIPTOR.message_types_by_name['SettingsCfg'] = _SETTINGSCFG
 DESCRIPTOR.message_types_by_name['CipdPackage'] = _CIPDPACKAGE
 DESCRIPTOR.message_types_by_name['CipdSettings'] = _CIPDSETTINGS
 DESCRIPTOR.message_types_by_name['AuthSettings'] = _AUTHSETTINGS
 DESCRIPTOR.message_types_by_name['ResultDBSettings'] = _RESULTDBSETTINGS
 DESCRIPTOR.message_types_by_name['CASSettings'] = _CASSETTINGS
+DESCRIPTOR.message_types_by_name['TrafficMigration'] = _TRAFFICMIGRATION
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 SettingsCfg = _reflection.GeneratedProtocolMessageType('SettingsCfg', (_message.Message,), {
@@ -434,6 +515,21 @@ CASSettings = _reflection.GeneratedProtocolMessageType('CASSettings', (_message.
   # @@protoc_insertion_point(class_scope:swarming.config.CASSettings)
   })
 _sym_db.RegisterMessage(CASSettings)
+
+TrafficMigration = _reflection.GeneratedProtocolMessageType('TrafficMigration', (_message.Message,), {
+
+  'Route' : _reflection.GeneratedProtocolMessageType('Route', (_message.Message,), {
+    'DESCRIPTOR' : _TRAFFICMIGRATION_ROUTE,
+    '__module__' : 'proto.config.config_pb2'
+    # @@protoc_insertion_point(class_scope:swarming.config.TrafficMigration.Route)
+    })
+  ,
+  'DESCRIPTOR' : _TRAFFICMIGRATION,
+  '__module__' : 'proto.config.config_pb2'
+  # @@protoc_insertion_point(class_scope:swarming.config.TrafficMigration)
+  })
+_sym_db.RegisterMessage(TrafficMigration)
+_sym_db.RegisterMessage(TrafficMigration.Route)
 
 
 DESCRIPTOR._options = None
