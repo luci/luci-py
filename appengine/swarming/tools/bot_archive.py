6549985cfcca8357d87e77dc1b50cf518afe629d
Vadim Shtayura <vadimsh@chromium.org>
2024-08-16 22:50:40 +0000

    Teach bot_archive.py script to emit bot code into a directory.
    This directory then will be packaged into a CIPD package by
    a builder.
    
    Note that packaging the bot directly from the source checkout
    is problematic because it has symlinks that CIPD client doesn't
    like. bot_archive.py resolves them. Also bot_archive.py uses
    the exact same code the Swarming server is using, reducing chances
    of incompatible packages.
    
    R=chanli@chromium.org
    BUG=b/355013257
    
    Change-Id: I5084ac9db7a80d5aee12a158e6f7ae3ab3308ea0
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/5787503
    Reviewed-by: Chan Li <chanli@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

--- appengine/swarming/tools/bot_archive.py
+++ appengine/swarming/tools/bot_archive.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2015 The LUCI Authors. All rights reserved.
 # Use of this source code is governed under the Apache License, Version 2.0
 # that can be found in the LICENSE file.
@@ -31,18 +31,44 @@ def get_swarming_bot_zip():
       additionals, config['enable_ts_monitoring'])
 
 
+def yield_swarming_bot_files():
+  config = read_config()
+  bot_config_path = os.path.join(ROOT_DIR, 'swarming_bot', 'config',
+                                 'bot_config.py')
+  with open(bot_config_path, 'rb') as f:
+    additionals = {'config/bot_config.py': f.read()}
+  from server import bot_archive
+  return bot_archive.yield_swarming_bot_files(
+      os.path.join(ROOT_DIR, 'swarming_bot'), config['server'], '1',
+      additionals, config['enable_ts_monitoring'])
+
+
 def main():
   parser = argparse.ArgumentParser(description=__doc__)
   parser.add_argument(
       '--output',
       help='Where to drop the bot archive (default: ./swarming_bot.zip).',
       default='swarming_bot.zip')
+  parser.add_argument(
+      '--output-dir',
+      help='If set, copy bot files into this directory instead of a zip file',
+      default=None)
   args = parser.parse_args()
 
   sys.path.insert(0, ROOT_DIR)
-  content, _ = get_swarming_bot_zip()
-  with open(args.output, 'wb') as f:
-    f.write(content)
+
+  if args.output_dir:
+    for name, content in yield_swarming_bot_files():
+      assert not name.endswith('/'), name  # no empty directories
+      path = os.path.join(args.output_dir, name)
+      os.makedirs(os.path.dirname(path), exist_ok=True)
+      with open(path, 'wb') as f:
+        f.write(content)
+  else:
+    content, _ = get_swarming_bot_zip()
+    with open(args.output, 'wb') as f:
+      f.write(content)
+
   return 0
 
 
